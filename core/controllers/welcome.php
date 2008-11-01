146f34dc07c7c47505d8a6e4bdf9e535fcddfbb5
Bharat Mediratta <bharat@menalto.com>
2008-11-01 03:58:08 +0000

    Improve the welcome page to be a bit more explanatory about the product.

--- core/controllers/welcome.php
+++ core/controllers/welcome.php
@@ -1,6 +1,31 @@
 <?php
+/*
+ * Gallery - a web based photo album viewer and editor
+ * Copyright (C) 2000-2008 Bharat Mediratta
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+/**
+ * Enter Description Here
+ * @package
+ * @subpackage
+ * @author Tim
+ */
 class Welcome_Controller extends Controller {
-  public function index() {
-    print "<img src=http://www.gallery2.org/gallery2.png>";
+  function Index() {
+    print new View('welcome.html');
   }
 }
