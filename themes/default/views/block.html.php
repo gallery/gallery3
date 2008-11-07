70e96fbaf0e4f8110f8366934b0d62318a5b57b0
Bharat Mediratta <bharat@menalto.com>
2008-11-07 08:54:30 +0000

    add gMinimize to the block header

--- themes/default/views/block.html.php
+++ themes/default/views/block.html.php
@@ -1,6 +1,6 @@
 <? defined("SYSPATH") or die("No direct script access."); ?>
 <div id="<?= $id ?>" class="gBlock">
-  <div class="gBlockHeader">
+  <div class="gBlockHeader gMinimize">
     <h2><?= $title ?></h2>
   </div>
   <div class="gBlockContent">
