7329cb0dde260f022178c7965f6240660d08da28
Chad Kieffer <chad@2tbsp.com>
2008-11-07 05:03:00 +0000

    Added YUI 2.6.0 CSS collection to lib (reset-gonts-grids and base) and call these rather than CDN versions, for now. Renamed default theme's style sheet to screen.css, recommend that CSS files be named for the primary media they support. Removed unused YUI JS component calls from the default theme.

--- themes/default/views/page.html.php
+++ themes/default/views/page.html.php
@@ -1,24 +1,19 @@
 <? defined("SYSPATH") or die("No direct script access."); ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Tranisitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Tranisitional//EN" 
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
     <title><?= _("Browse Photos") ?> :: <?= $item->title ?></title>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-    <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.5.2/build/reset-fonts-grids/reset-fonts-grids.css" />
-    <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.5.2/build/base/base-min.css" />
-    <link rel="stylesheet" type="text/css" href="<?= $theme->url("css/styles.css") ?>" media="screen,projection" />
-
-    <!-- CSS -->
-    <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.6.0/build/container/assets/container.css" />
-    <script type="text/javascript" src="http://yui.yahooapis.com/2.6.0/build/yahoo-dom-event/yahoo-dom-event.js"></script>
-    <script type="text/javascript" src="http://yui.yahooapis.com/2.6.0/build/animation/animation-min.js"></script>
-    <script type="text/javascript" src="http://yui.yahooapis.com/2.6.0/build/container/container-min.js"></script>
-
-    <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.6.0/build/treeview/assets/skins/sam/treeview.css" />
-    <script type="text/javascript" src="http://yui.yahooapis.com/2.6.0/build/treeview/treeview-min.js"></script>
+    <link rel="stylesheet" type="text/css" href="<?= url::base() . 
+	  "lib/yui/reset-fonts-grids.css" ?>" media="screen,print,projection" />
+    <link rel="stylesheet" type="text/css" href="<?= url::base() . 
+	  "lib/yui/base-min.css" ?>" media="screen,print,projection" />
+    <link rel="stylesheet" type="text/css" href="<?= 
+      $theme->url("css/screen.css") ?>" media="screen,print,projection" />
   </head>
 
-  <body class="yui-skin-sam">
+  <body>
     <div id="doc2" class="yui-t5 gView">
       <?= $theme->display("header.html") ?>
       <div id="bd">
@@ -27,7 +22,6 @@
             <?= $content ?>
           </div>
         </div>
-
         <?= $theme->display("sidebar.html") ?>
       </div>
       <?= $theme->display("footer.html") ?>
