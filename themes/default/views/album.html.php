50a9848f383c988b720c99945fbb91f371e02fa0
Chad Kieffer <chad@2tbsp.com>
2008-11-07 07:31:58 +0000

    Updated CSS organization and headings, removed unused styles, added placeholders for a few styles, including gThumbnail. Simplified album grid loop, the thumbnail container markup for albums and items was almost identical, no need to repeat all of it. Switched album grid markup from <div> to <ul>. Updated element nesting in photo template.

--- themes/default/views/album.html.php
+++ themes/default/views/album.html.php
@@ -1,39 +1,32 @@
 <? defined("SYSPATH") or die("No direct script access."); ?>
-<div id="gAlbumGrid" class="gAlbumView">
   <div id="gAlbumGridHeader">
     <h1><?= $item->title ?></h1>
     <span class="understate"><?= $item->description ?></span>
     <a href="#" id="gSlideshowLink" class="buttonlink">Slideshow</a>
   </div>
 
+  <ul id="gAlbumGrid">
   <? foreach ($children as $i => $child): ?>
-  <? if ($child->is_album()): ?>
-  <div class="gAlbumContainer gAlbum <?= text::alternate("first", "", "") ?>">
-    <a href="<?= url::site("album/{$child->id}") ?>">
-      <img id="photo-id-<?= $child->id ?>" class="photo"
-           alt="photo" src="<?= $child->thumbnail_url() ?>"
-           width="<?= $child->thumbnail_width ?>"
-           height="<?= $child->thumbnail_height ?>" />
-    </a>
-    <h2>Album title</h2>
-    <ul class="gMetadata">
-      <li>Views: 321</li>
-      <li>By: <a href="#">username</a></li>
-    </ul>
-  </div>
-  <? else: ?>
-  <div class="gItemContainer <?= text::alternate("first", "", "") ?>">
-    <a href="<?= url::site("photo/{$child->id}") ?>">
-      <img id="photo-id-<?= $child->id ?>" class="photo"
-           alt="photo" src="<?= $child->thumbnail_url() ?>"
-           width="<?= $child->thumbnail_width ?>"
-           height="<?= $child->thumbnail_height ?>" />
-    </a>
-    <h2><?= $child->title ?></h2>
-  </div>
-  <? endif ?>
+    <? $album_class = ""; ?>
+    <? if ($child->is_album()): ?>
+      <? $album_class = "gAlbum "; ?>
+    <? endif ?>
+    <li class="gItem <?= $album_class . text::alternate("first", "", "") ?>">
+      <a href="<?= url::site("photo/{$child->id}") ?>">
+        <img id="gPhotoID-<?= $child->id ?>" class="gThumbnail"
+             alt="photo" src="<?= $child->thumbnail_url() ?>"
+             width="<?= $child->thumbnail_width ?>"
+             height="<?= $child->thumbnail_height ?>" />
+      </a>
+      <h2><?= $child->title ?></h2>
+      <ul class="gMetadata">
+        <li>Views: 321</li>
+        <li>By: <a href="#">username</a></li>
+      </ul>
+    </li>  
   <? endforeach ?>
-
+  </ul>
+  
   <div id="gPagination">
     Items 1-10 of 34
     <span class="first_inactive">first</span>
@@ -41,4 +34,3 @@
     <a href="#" class="next">next</a>
     <a href="#" class="last">last</a>
   </div>
-</div>
