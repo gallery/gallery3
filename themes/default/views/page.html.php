6a1ef44c8842d007dd59574cc76ba767519d124f
Bharat Mediratta <bharat@menalto.com>
2008-11-07 06:44:36 +0000

    convert tabs -> spaces

--- themes/default/views/page.html.php
+++ themes/default/views/page.html.php
@@ -1,15 +1,15 @@
 <? defined("SYSPATH") or die("No direct script access."); ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Tranisitional//EN" 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Tranisitional//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
     <title><?= _("Browse Photos") ?> :: <?= $item->title ?></title>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-    <link rel="stylesheet" type="text/css" href="<?= url::base() . 
-	  "lib/yui/reset-fonts-grids.css" ?>" media="screen,print,projection" />
-    <link rel="stylesheet" type="text/css" href="<?= url::base() . 
-	  "lib/yui/base-min.css" ?>" media="screen,print,projection" />
-    <link rel="stylesheet" type="text/css" href="<?= 
+    <link rel="stylesheet" type="text/css" href="<?= url::base() .
+          "lib/yui/reset-fonts-grids.css" ?>" media="screen,print,projection" />
+    <link rel="stylesheet" type="text/css" href="<?= url::base() .
+          "lib/yui/base-min.css" ?>" media="screen,print,projection" />
+    <link rel="stylesheet" type="text/css" href="<?=
       $theme->url("css/screen.css") ?>" media="screen,print,projection" />
   </head>
 
