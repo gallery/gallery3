085ba7cd46c3fcc5bcb7a3f4c98ee580e67b6800
Bharat Mediratta <bharat@menalto.com>
2008-11-10 00:16:41 +0000

    Tests for Item_Controller

---
+++ core/tests/Item_Controller_Test.php
@@ -0,0 +1,43 @@
+<?php defined("SYSPATH") or die("No direct script access.");
+/**
+ * Gallery - a web based photo album viewer and editor
+ * Copyright (C) 2000-2008 Bharat Mediratta
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+class Item_Controller_Test extends Unit_Test_Case {
+  public function change_item_test() {
+    $controller = new Item_Controller();
+    $album = album::create(1, "test", "test");
+    $_POST["title"] = "new title";
+    $_POST["description"] = "new description";
+
+    $controller->post($album);
+    $this->assert_equal("new title", $album->title);
+    $this->assert_equal("new description", $album->description);
+  }
+
+  public function change_item_test_with_return() {
+    $controller = new Item_Controller();
+    $album = album::create(1, "test", "test");
+    $_POST["title"] = "item_title";
+    $_POST["description"] = "item_description";
+    $_POST["__return"] = "item_description";
+
+    $tihs->assert_equal("item_description", $controller->post($album));
+    $this->assert_equal("item_title", $album->title);
+    $this->assert_equal("item_description", $album->description);
+  }
+}
