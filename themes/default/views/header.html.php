5e385398f00cd77ced7b91892c02c8c97db0848e
Chad Kieffer <chad@2tbsp.com>
2008-11-10 02:16:09 +0000

    IE fixes. Default theme now looks roughly the same in IE and Firefox. We need to avoid using absolute and relative positioning for our primary layout containers (i.e. gHeader, gFooter, etc.).

--- themes/default/views/header.html.php
+++ themes/default/views/header.html.php
@@ -4,7 +4,7 @@
 <h1><?= $item->title_edit ?></h1>
 
 <div id="gLoginMenu">
-  <a href="#"><?= _("Register") ?></a> |
+  <a href="#"><?= _("Register") ?></a> | 
   <a href="#"><?= _("Login") ?></a>
 </div>
 
@@ -16,14 +16,14 @@
   <li><a href="#"><?= _("ADMIN") ?></a></li>
 </ul>
 
+<form id="gSearchForm">
+  <input type="text" class="text" value="<?= _("Search Gallery ...") ?>"/>
+  <input type="submit" class="submit" value="search" />
+</form>
+
 <ul id="gBreadcrumbs">
   <? foreach ($parents as $parent): ?>
   <li><a href="<?= url::site("album/{$parent->id}") ?>"><?= $parent->title_edit ?></a></li>
   <? endforeach ?>
   <li class="active"><?= $item->title_edit ?></li>
 </ul>
-
-<form id="gSearchForm">
-  <input type="text" class="text" value="<?= _("Search Gallery ...") ?>"/>
-  <input type="submit" class="submit" value="search" />
-</form>
