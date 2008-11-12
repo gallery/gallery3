91b149adbf42f71b1892cef4b90f62c1147c8d06
Tim Almdal <tnalmdal@shaw.ca>
2008-11-12 17:48:04 +0000

    Generate the login view using fieldsets and an un order list

--- modules/user/views/login.html.php
+++ modules/user/views/login.html.php
@@ -1,13 +1,23 @@
 <? defined("SYSPATH") or die("No direct script access."); ?>
-<form id="gLogin">
-  <label for="username">Username</label>
-  <input type="text" class="text" id="username" />
-  <label for="password">Password</label>
-  <input type="password" class="password" id="password" />
-  <input type="submit" class="submit" value="<?= _("Login")?>" />
-  <? if (!empty($error_message)): ?>
-    <p class="error">
-      <?= $error_message ?>
-    </p>
-  <? endif;?>
-</form>
+<fieldset id="gLogin">
+  <ul>
+    <li>
+      <label for="username">Username</label>
+      <input type="text" class="text" id="username" />
+    </li>
+    <li>
+      <label for="password">Password</label>
+      <input type="password" class="password" id="password" />
+    </li>
+    <li>
+      <input type="submit" class="submit" value="<?= _("Login")?>" />
+    </li>
+    <? if (!empty($error_message)): ?>
+      <li>
+        <p class="error" id="login_message">
+          <?= $error_message ?>
+       </p>
+      </li>
+    <? endif;?>
+  </ul>
+</fieldset>
