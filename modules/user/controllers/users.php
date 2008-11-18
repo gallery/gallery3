59dbd1dc833a974f55f5cc66fa1c3204fa30980e
Bharat Mediratta <bharat@menalto.com>
2008-11-18 08:10:14 +0000

    Move User_Controller to Users_Controller per jozefs request.

--- modules/user/controllers/users.php
+++ modules/user/controllers/users.php
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
-class User_Controller extends REST_Controller {
+class Users_Controller extends REST_Controller {
   protected $resource_type = "user";
 
   /**
