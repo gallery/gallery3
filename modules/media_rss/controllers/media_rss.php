a197c057c83534b0fa59da692fa63bcd49c16a26
Bharat Mediratta <bharat@menalto.com>
2008-11-19 03:23:40 +0000

    rename views/feed.media_rss.php to views/feed.mrss.php because mrss is a more standard abbreviation for MediaRSS.  Sigh.

--- modules/media_rss/controllers/media_rss.php
+++ modules/media_rss/controllers/media_rss.php
@@ -24,7 +24,7 @@ class Media_RSS_Controller extends Controller {
       return Kohana::show_404();
     }
 
-    $view = new View("feed.media_rss");
+    $view = new View("feed.mrss");
     $view->item = $item;
 
     // This should probably be a method in ORM_MPTT
