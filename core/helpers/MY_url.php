9ddc8c8aabfc5a6697ff636900297f94ecff4d8f
Bharat Mediratta <bharat@menalto.com>
2008-11-19 23:36:50 +0000

    Fix typo.

--- core/helpers/MY_url.php
+++ core/helpers/MY_url.php
@@ -17,7 +17,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
-<?php
 class url extends url_Core {
   /**
    * Just like url::file() except that it returns an absolute URI
