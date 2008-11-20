b18a1f477830274e2487fd0564d795dbbf386eb7
Chad Kieffer <chad@2tbsp.com>
2008-11-20 05:07:35 +0000

    Content type and character encoding should be defined before <title>

--- themes/default/views/page.html.php
+++ themes/default/views/page.html.php
@@ -3,8 +3,8 @@
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
-    <title><?= _("Browse Photos") ?> :: <?= $item->title ?></title>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <title><?= _("Browse Photos") ?> :: <?= $item->title ?></title>
     <link rel="stylesheet" type="text/css" href="<?= url::file("lib/yui/reset-fonts-grids.css") ?>"
           media="screen,print,projection" />
     <link rel="stylesheet" type="text/css" href="<?= url::file("lib/yui/base-min.css") ?>"
