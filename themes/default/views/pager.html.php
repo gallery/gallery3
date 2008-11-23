a43b8d03a176faf0092da60b019f38d65c5e90ea
Chad Kieffer <chad@2tbsp.com>
2008-11-23 18:15:54 +0000

    This should have been checked in with r18815.

--- themes/default/views/pager.html.php
+++ themes/default/views/pager.html.php
@@ -1,28 +1,25 @@
 <? defined("SYSPATH") or die("No direct script access."); ?>
 <? // See http://docs.kohanaphp.com/libraries/pagination ?>
-<div id="gPagination">
-  <?= sprintf(_("Photos %d - %d of %d"), $current_first_item, $current_last_item, $total_items) ?>
+<ul id="gPager">
+  <li><?= sprintf(_("Items %d - %d of %d"), $current_first_item, $current_last_item, $total_items) ?></li>
   <? if ($first_page): ?>
-  <a href="<?= str_replace('{page}', 1, $url) ?>"><?= _("first") ?></a>
+  <li class="first"><a href="<?= str_replace('{page}', 1, $url) ?>"><?= _("first") ?></a></li>
   <? else: ?>
-  <span class="first_inactive"><?= _("first") ?></span>
+  <li class="first_inactive"><?= _("first") ?></li>
   <? endif ?>
-
   <? if ($previous_page): ?>
-  <a href="<?= str_replace('{page}', $previous_page, $url) ?>"><?= _("previous") ?></a>
+  <li class="previous"><a href="<?= str_replace('{page}', $previous_page, $url) ?>"><?= _("previous") ?></a></li>
   <? else: ?>
-  <span class="previous_inactive"><?= _("previous") ?></span>
+  <li class="previous_inactive"><?= _("previous") ?></li>
   <? endif ?>
-
   <? if ($next_page): ?>
-  <a href="<?= str_replace('{page}', $next_page, $url) ?>"><?= _("next") ?></a>
+  <li class="next"><a href="<?= str_replace('{page}', $next_page, $url) ?>"><?= _("next") ?></a></li>
   <? else: ?>
-  <span class="next_inactive"><?= _("next") ?></span>
+  <li class="next_inactive"><?= _("next") ?></li>
   <? endif ?>
-
   <? if ($last_page): ?>
-  <a href="<?= str_replace('{page}', $last_page, $url) ?>"><?= _("last") ?></a>
+  <li class="last"><a href="<?= str_replace('{page}', $last_page, $url) ?>"><?= _("last") ?></a></li>
   <? else: ?>
-  <span class="last_inactive"><?= _("last") ?></span>
+  <li class="last_inactive"><?= _("last") ?></li>
   <? endif ?>
-</div>
+</ul>
