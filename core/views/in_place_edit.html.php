85bdb606bf56a71b4b02fa353129b6c2cfe2b527
Bharat Mediratta <bharat@menalto.com>
2008-11-25 02:16:00 +0000

    Escape CDATA blocks to avoid JS errors

--- core/views/in_place_edit.html.php
+++ core/views/in_place_edit.html.php
@@ -1,6 +1,6 @@
 <? defined("SYSPATH") or die("No direct script access."); ?>
 <script type="text/javascript">
-<![CDATA[
+// <![CDATA[
 $(document).ready(function() {
   ajax_update = function(className, id) {
     return function(value, settings) {
@@ -38,5 +38,5 @@ $(document).ready(function() {
     }
   }
 });
-]]>
+// ]]>
 </script>
