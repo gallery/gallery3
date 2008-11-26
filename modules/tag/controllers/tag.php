aa19a79e067c95f3e1c8b3e35fc051b4fa0eddbf
Bharat Mediratta <bharat@menalto.com>
2008-11-26 20:33:06 +0000

    Clean up unnecessary comments, remove stray references to Comment_Controller

--- modules/tag/controllers/tag.php
+++ modules/tag/controllers/tag.php
@@ -24,21 +24,21 @@ class Tag_Controller extends REST_Controller {
    *  @see Rest_Controller::_index()
    */
   public function _index() {
-    throw new Exception("@todo Comment_Controller::_form NOT IMPLEMENTED");
+    throw new Exception("@todo Tag_Controller::_index NOT IMPLEMENTED");
   }
 
   /**
    *  @see Rest_Controller::_form_add($parameters)
    */
   public function _form_add($parameters) {
-    throw new Exception("@todo Comment_Controller::_form NOT IMPLEMENTED");
+    throw new Exception("@todo Tag_Controller::_form NOT IMPLEMENTED");
   }
 
   /**
    *  @see Rest_Controller::_form_edit($resource)
    */
   public function _form_edit($tag) {
-    throw new Exception("@todo Comment_Controller::_form NOT IMPLEMENTED");
+    throw new Exception("@todo Tag_Controller::_form NOT IMPLEMENTED");
   }
 
   public function _show($tag) {
@@ -46,28 +46,14 @@ class Tag_Controller extends REST_Controller {
   }
 
   public function _create($tag) {
-    // @todo Productionize this code
-    // 1) Add security checks
     throw new Exception("@todo Tag_Controller::_create NOT IMPLEMENTED");
-      }
+  }
 
   public function _delete($tag) {
-    // @todo Production this code
-    // 1) Add security checks
     throw new Exception("@todo Tag_Controller::_delete NOT IMPLEMENTED");
   }
 
   public function _update($tag) {
-    // @todo Productionize this
-    // 1) Figure out how to do the right validation here.  Validate the form input and apply it to
-    //    the model as appropriate.
-    // 2) Figure out how to dispatch according to the needs of the client.  Ajax requests from
-    //    jeditable will want the changed field back, and possibly the whole item in json.
-    //
-    // For now let's establish a simple protocol where the client passes in a __return parameter
-    // that specifies which field it wants back from the item.  Later on we can expand that to
-    // include a data format, etc.
-
     throw new Exception("@todo Tag_Controller::_update NOT IMPLEMENTED");
   }
 }
