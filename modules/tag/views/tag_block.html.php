9f37aaa1c29b9b17bee514597ec316130d7531c7
Bharat Mediratta <bharat@menalto.com>
2008-11-26 20:40:04 +0000

    Move Tag_Controller -> Tags_Controller to fit our REST pattern.

--- modules/tag/views/tag_block.html.php
+++ modules/tag/views/tag_block.html.php
@@ -3,7 +3,7 @@
   <? foreach ($tags as $tag): ?>
   <li class="size<?=(int)(($tag->count / $max_count) * 7) ?>">
     <span><?= $tag->count ?> photos are tagged with </span>
-    <a href="<?=url::site("tag/$tag->id") ?>"><?= $tag->name ?></a>
+    <a href="<?=url::site("tags/$tag->id") ?>"><?= $tag->name ?></a>
   </li>
   <? endforeach ?>
 </ul>
