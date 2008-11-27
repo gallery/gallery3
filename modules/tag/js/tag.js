480abae4010c036bdd7df75147614f6c3e596ded
Bharat Mediratta <bharat@menalto.com>
2008-11-27 04:31:12 +0000

    Fix indentation.

--- modules/tag/js/tag.js
+++ modules/tag/js/tag.js
@@ -9,11 +9,11 @@ $("document").ready(function() {
 });
 
 function get_tag_block(url) {
-    $.post(url, function(data) {
-      $('#gTagFormContainer').html(data);
-      $("#gAddTag").submit(function(event){
-         get_tag_block($("#gAddTag").attr("action"));
-        return false;
-      });
-   });
+  $.post(url, function(data) {
+    $('#gTagFormContainer').html(data);
+    $("#gAddTag").submit(function(event){
+      get_tag_block($("#gAddTag").attr("action"));
+      return false;
+    });
+  });
 }
