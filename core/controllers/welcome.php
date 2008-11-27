d909fc10706776f5396d032e655659c06989763c
Bharat Mediratta <bharat@menalto.com>
2008-11-27 10:25:50 +0000

    Make tags look a little better.

--- core/controllers/welcome.php
+++ core/controllers/welcome.php
@@ -238,21 +238,21 @@ class Welcome_Controller extends Template_Controller {
     // Words from lorem2.com
     $words = explode(
       " ",
-      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat " .
-      "mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, " .
-      "semper suscipit, posuere a, pede.  Donec nec justo eget felis facilisis " .
-      "fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque " .
-      "felis. Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, " .
-      "pharetra a, ultricies in, diam. Sed arcu. Cras consequat. Praesent dapibus, neque " .
-      "id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu " .
-      "erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, " .
-      "facilisis luctus, metus. Phasellus ultrices nulla quis nibh. Quisque a " .
-      "lectus. Donec consectetuer ligula vulputate sem tristique cursus. Nam nulla quam, " .
-      "gravida non, commodo a, sodales sit amet, nisi. Pellentesque fermentum " .
-      "dolor. Aliquam quam lectus, facilisis auctor, ultrices ut, elementum vulputate, " .
-      "nunc. Sed adipiscing ornare risus. Morbi est est, blandit sit amet, sagittis vel, " .
-      "euismod vel, velit. Pellentesque egestas sem. Suspendisse commodo ullamcorper " .
-      "magna.");
+      "Lorem ipsum dolor sit amet consectetuer adipiscing elit Donec odio Quisque volutpat " .
+      "mattis eros Nullam malesuada erat ut turpis Suspendisse urna nibh viverra non " .
+      "semper suscipit posuere a pede  Donec nec justo eget felis facilisis " .
+      "fermentum Aliquam porttitor mauris sit amet orci Aenean dignissim pellentesque " .
+      "felis Morbi in sem quis dui placerat ornare Pellentesque odio nisi euismod in " .
+      "pharetra a ultricies in diam Sed arcu Cras consequat Praesent dapibus neque " .
+      "id cursus faucibus tortor neque egestas augue eu vulputate magna eros eu " .
+      "erat Aliquam erat volutpat Nam dui mi tincidunt quis accumsan porttitor " .
+      "facilisis luctus metus Phasellus ultrices nulla quis nibh Quisque a " .
+      "lectus Donec consectetuer ligula vulputate sem tristique cursus Nam nulla quam " .
+      "gravida non commodo a sodales sit amet nisi Pellentesque fermentum " .
+      "dolor Aliquam quam lectus facilisis auctor ultrices ut elementum vulputate " .
+      "nunc Sed adipiscing ornare risus Morbi est est blandit sit amet sagittis vel " .
+      "euismod vel velit Pellentesque egestas sem Suspendisse commodo ullamcorper " .
+      "magna");
 
     while ($number--) {
       $results[] = $words[array_rand($words, 1)];
