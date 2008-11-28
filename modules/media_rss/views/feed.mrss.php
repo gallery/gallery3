0a6be1bf5584f9b755ac89f7be45542345498fe1
Bharat Mediratta <bharat@menalto.com>
2008-11-28 02:44:02 +0000

    Fix some minor issues with the feed format

--- modules/media_rss/views/feed.mrss.php
+++ modules/media_rss/views/feed.mrss.php
@@ -1,13 +1,13 @@
 <? defined("SYSPATH") or die("No direct script access."); ?>
 <? echo "<?xml version=\"1.0\" ?>" ?>
 <rss version="2.0" xmlns:media="http://search.yahoo.com/mrss/"
-                   xmlns:atom="http://www.w3.org/2005/Atom">
+     xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
     <title><? $title ?></title>
     <link><?= $link ?></link>
     <description><?= $description ?></description>
     <language>en-us</language>
-    <atom:link rel="self" href="<?= $feed_link ?>" type="application/rss+xmlml" />
+    <atom:link rel="self" href="<?= $feed_link ?>" type="application/rss+xml" />
     <? if (!empty($previous_page_link)): ?>
     <atom:link rel="previous" href="<?= $previous_page_link ?>" type="application/rss+xml" />
     <? endif ?>
@@ -17,29 +17,29 @@
     <pubDate><?= $pub_date ?></pubDate>
     <lastBuildDate><?= $pub_date ?></lastBuildDate>
     <? foreach ($children as $child): ?>
-      <item>
-        <title><?= $child->title ?></title>
-        <link><?= url::abs_site("photos/$child->id") ?></link>
-        <guid isPermaLink="false"><?= $child->id ?></guid>
-        <description><?= $child->description ?></description>
-        <media:group>
-          <media:thumbnail  url="<?= $child->thumbnail_url(true) ?>"
-            height="<?= $child->thumbnail_height ?>"
-            width="<?= $child->thumbnail_width ?>"
-           />
-          <media:content url="<?= $child->resize_url(true) ?>"
-            type="<?= $child->mime_type ?>"
-            height="<?= $child->resize_height ?>"
-            width="<?= $child->resize_width ?>"
-            isDefault="true"
-          />
-          <media:content url="<?= $child->file_url(true) ?>"
-            type="<?= $child->mime_type ?>"
-            height="<?= $child->height ?>"
-            width="<?= $child->width ?>"
-          />
-        </media:group>
-      </item>
+    <item>
+      <title><?= $child->title ?></title>
+      <link><?= url::abs_site("photos/$child->id") ?></link>
+      <guid isPermaLink="true"><?= url::abs_site("photos/$child->id") ?></guid>
+      <description><?= $child->description ?></description>
+      <media:group>
+        <media:thumbnail url="<?= $child->thumbnail_url(true) ?>"
+                         height="<?= $child->thumbnail_height ?>"
+                         width="<?= $child->thumbnail_width ?>"
+                         />
+        <media:content url="<?= $child->resize_url(true) ?>"
+                       type="<?= $child->mime_type ?>"
+                       height="<?= $child->resize_height ?>"
+                       width="<?= $child->resize_width ?>"
+                       isDefault="true"
+                       />
+        <media:content url="<?= $child->file_url(true) ?>"
+                       type="<?= $child->mime_type ?>"
+                       height="<?= $child->height ?>"
+                       width="<?= $child->width ?>"
+                       />
+      </media:group>
+    </item>
     <? endforeach ?>
   </channel>
 </rss>
