6f959deed90750d3f87f26e64fc44beb3f7ba903
Bharat Mediratta <bharat@menalto.com>
2008-12-03 03:23:30 +0000

    Rename generic test.php to gallery_unit_test.php to make navigation to this file a little easier

--- modules/gallery_unit_test/controllers/gallery_unit_test.php
+++ modules/gallery_unit_test/controllers/gallery_unit_test.php
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
-class Test_Controller extends Controller {
+class Gallery_Unit_Test_Controller extends Controller {
   function Index() {
     if (!defined('TEST_MODE')) {
       print Kohana::show_404();
