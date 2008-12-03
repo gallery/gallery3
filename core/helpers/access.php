718ac67a02aa6fea73b6ed4ff3b092e77320798b
Bharat Mediratta <bharat@menalto.com>
2008-12-03 04:17:02 +0000

    get rid of a stray space that was breaking the File_Structure_Test

--- core/helpers/access.php
+++ core/helpers/access.php
@@ -1,4 +1,4 @@
- <?php defined("SYSPATH") or die("No direct script access.");
+<?php defined("SYSPATH") or die("No direct script access.");
 /**
  * Gallery - a web based photo album viewer and editor
  * Copyright (C) 2000-2008 Bharat Mediratta
