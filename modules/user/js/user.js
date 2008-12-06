adfe664d96a168d3474c97c633d75db7dd57a4be
Chad Kieffer <chad@2tbsp.com>
2008-12-06 23:28:26 +0000

    Removed tabs, todo, added description

--- modules/user/js/user.js
+++ modules/user/js/user.js
@@ -1,23 +1,23 @@
 /**
- * @todo preventDefault() not working in IE 6 and 7
+ * Display user login form 
+ * 
  * @todo Close link should be reusable
  */
-
 $("document").ready(function() {
   $("#gLoginLink").click(function() {
     var url = $("#gLoginLink a").attr("href");
     $.get(url, function(data) {
-	    $('#gLoginLink').hide();
-	    $("#gLoginMenu").append('<li><a href="#">X</a></li>');
-	    $("#gLoginMenu li:last").addClass("gClose").show();
-	    $("#gLoginMenu .gClose a").click(function() {
-	      $("#gLoginForm").remove();
-	      $("#gLoginMenu .gClose").remove();
-	      $("#gLoginFormContainer").hide();
-	      $("#gLoginLink").show();
-	      $("input#gUsername").val("");
-	      $("input#gPassword").val("");
-	    });
+      $('#gLoginLink').hide();
+      $("#gLoginMenu").append('<li><a href="#">X</a></li>');
+      $("#gLoginMenu li:last").addClass("gClose").show();
+      $("#gLoginMenu .gClose a").click(function() {
+        $("#gLoginForm").remove();
+        $("#gLoginMenu .gClose").remove();
+        $("#gLoginFormContainer").hide();
+        $("#gLoginLink").show();
+        $("input#gUsername").val("");
+        $("input#gPassword").val("");
+      });
       $("#gLoginFormContainer").html(data).hide().fadeIn();
       ajaxify_login_form();
     });
