b0f39497b4ab4315cba1ba4db9633b34e72ec440
Bharat Mediratta <bharat@menalto.com>
2008-12-08 08:32:44 +0000

    Some minor beautification.

--- modules/gallery_unit_test/views/kohana_error_page.php
+++ modules/gallery_unit_test/views/kohana_error_page.php
@@ -1,13 +1,17 @@
-<?php 
-echo html::specialchars($error) . "\n";
-echo html::specialchars($description) . "\n";
-if ( ! empty($line) AND ! empty($file)) {
-  echo $file . "[" . $line . "]:" . "\n";
+<?php
+echo $error, "\n\n";
+echo wordwrap($description, 80), "\n\n";
+if (!empty($line) && !empty($file)) {
+  echo $file, "[", $line, "]:\n";
 }
-echo $message . "\n";
-if ( ! empty($trace)) {
-  $trace = preg_replace(array('/<li>/', '/<(.*?)>/', '/&gt;/'), array("\t", '', '>'), $trace);
-  echo Kohana::lang('core.stack_trace') . "\n";
-  echo $trace . "\n";
+echo $message, "\n\n";
+
+if (!empty($trace)) {
+  $trace = preg_replace(
+    array('/<li>/', '/<(.*?)>/', '/&gt;/'),
+    array("  ",     '',          '>'),
+    $trace);
+  echo "Stack Trace:\n";
+  echo $trace, "\n";
 }
 
