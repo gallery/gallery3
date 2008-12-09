930fb1f6976e60ec3774d837036b5a73bdbf32a8
Bharat Mediratta <bharat@menalto.com>
2008-12-09 08:29:12 +0000

    Remove tabs, fix indentation.

--- themes/default/views/page.html.php
+++ themes/default/views/page.html.php
@@ -1,20 +1,20 @@
 <? defined("SYSPATH") or die("No direct script access."); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Tranisitional//EN"
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+          "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
     <title>
-    <? if ($page_type == "tag"): ?>
-    <?= _("Browse Tags") ?> :: <?= $tag->name ?>
-    <? else: ?>
-    <?= _("Browse Photos") ?> :: <?= $item->title ?>
-    <? endif ?>
+      <? if ($page_type == "tag"): ?>
+      <?= _("Browse Tags") ?> :: <?= $tag->name ?>
+      <? else: ?>
+      <?= _("Browse Photos") ?> :: <?= $item->title ?>
+      <? endif ?>
     </title>
     <link rel="stylesheet" type="text/css" href="<?= url::file("lib/yui/reset-fonts-grids.css") ?>"
           media="screen,print,projection" />
-    <link rel="stylesheet" type="text/css" href="<?= $theme->url("jquery/superfish.css") ?>" 
-			    media="screen" /> 
+    <link rel="stylesheet" type="text/css" href="<?= $theme->url("jquery/superfish.css") ?>"
+          media="screen" />
     <link rel="stylesheet" type="text/css" href="<?= $theme->url("css/screen.css") ?>"
           media="screen,print,projection" />
     <script src="<?= url::file("lib/jquery.js") ?>" type="text/javascript"></script>
