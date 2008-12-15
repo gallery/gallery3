628058b4ed5aefb543ceb6ca9d3b87828c66bef1
Bharat Mediratta <bharat@menalto.com>
2008-12-15 08:52:32 +0000

    Refresh from kohana svn trunk r3771

---
+++ modules/unit_test/config/unit_test.php
@@ -0,0 +1,15 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+/**
+ * @package  Unit_Test
+ *
+ * Default paths to scan for tests.
+ */
+$config['paths'] = array
+(
+	MODPATH.'unit_test/tests',
+);
+
+/**
+ * Set to TRUE if you want to hide passed tests from the report.
+ */
+$config['hide_passed'] = FALSE;
