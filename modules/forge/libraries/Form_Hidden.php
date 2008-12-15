93977e579cca9a9d0df28099e1fb7596bcc1c825
Bharat Mediratta <bharat@menalto.com>
2008-12-15 09:22:32 +0000

    Refresh of Forge r168 from vendor/forge/modified

---
+++ modules/forge/libraries/Form_Hidden.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * FORGE hidden input library.
+ *
+ * $Id$
+ *
+ * @package    Forge
+ * @author     Kohana Team
+ * @copyright  (c) 2007-2008 Kohana Team
+ * @license    http://kohanaphp.com/license.html
+ */
+class Form_Hidden_Core extends Form_Input {
+
+	protected $data = array
+	(
+		'name'  => '',
+		'value' => '',
+	);
+
+	public function render()
+	{
+		return form::hidden($this->data['name'], $this->data['value']);
+	}
+
+} // End Form Hidden
