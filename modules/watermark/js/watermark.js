15c22471cc2aab3255595497d632a14cdc591544
Tim Almdal <tnalmdal@shaw.ca>
2008-12-15 04:23:57 +0000

    Cleanup watermark.js

--- modules/watermark/js/watermark.js
+++ modules/watermark/js/watermark.js
@@ -1,15 +1,12 @@
 $("document").ready(function() {
-  ajaxify_form();
+  ajaxify_watermark_add_form();
 });
 
-function ajaxify_form() {
+function ajaxify_watermark_add_form() {
   $("#gUploadWatermarkForm").ajaxForm({
     complete:function(xhr, statusText) {
       $("#gUploadWatermarkForm").replaceWith(xhr.responseText);
-      if (xhr.status == 200) {
-        $("#gUploadWatermarkForm").clearForm();
-      }
-      ajaxify_form();
+      ajaxify_watermark_add_form();
     }
   });
 }
