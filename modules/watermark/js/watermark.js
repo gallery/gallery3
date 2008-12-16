c32988a097d6199194ae156fafbac444a0b9b56a
Tim Almdal <tnalmdal@shaw.ca>
2008-12-16 19:12:24 +0000

    Separate the handling of the dialog resizing into the watermark.js, so we don't have to download the watermark dialog initialization with each request.

--- modules/watermark/js/watermark.js
+++ modules/watermark/js/watermark.js
@@ -10,3 +10,19 @@ function ajaxify_watermark_add_form() {
     }
   });
 }
+
+function watermark_dialog_initialize() {
+  var container = $("#gDialog").parent().parent();
+  var container_height = $(container).attr("offsetHeight");
+  var container_width = $(container).attr("offsetWidth");
+
+  var new_height = $("#gDialog").attr("offsetHeight") +
+    container.find("div.ui-dialog-titlebar").attr("offsetHeight") +
+    container.find("div.ui-dialog-buttonpane").attr("offsetHeight");
+  var height = Math.max(new_height, container_height);
+  var width = Math.max($("#gDialog").attr("offsetWidth"), container_width);
+  container.css("height", height + "px");
+  container.css("width", width + "px");
+  container.css("top", ((document.height - height) / 2) + "px");
+  container.css("left", ((document.width - width) / 2) + "px");
+}
--- modules/watermark/views/watermark_position.html.php
+++ modules/watermark/views/watermark_position.html.php
@@ -1,19 +1,5 @@
 <script>
-$("#gDialog").ready(function() {
-  var container = $("#gDialog").parent().parent();
-  var container_height = $(container).attr("offsetHeight");
-  var container_width = $(container).attr("offsetWidth");
-
-  var new_height = $("#gDialog").attr("offsetHeight") +
-    container.find("div.ui-dialog-titlebar").attr("offsetHeight") +
-    container.find("div.ui-dialog-buttonpane").attr("offsetHeight");
-  var height = Math.max(new_height, container_height);
-  var width = Math.max($("#gDialog").attr("offsetWidth"), container_width);
-  container.css("height", height + "px");
-  container.css("width", width + "px");
-  container.css("top", ((document.height - height) / 2) + "px");
-  container.css("left", ((document.width - width) / 2) + "px");
-});
+$("#gDialog").ready(watermark_dialog_initialize());
 </script>
 <div id="gWatermarkAdmin">
   <div id="gTarget" class="droppable">
