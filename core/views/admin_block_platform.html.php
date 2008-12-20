0f10f37c1656d0fa9e4eb90ccb367c6a0293b4e7
Bharat Mediratta <bharat@menalto.com>
2008-12-20 02:06:14 +0000

    Replace placeholders with real data

--- core/views/admin_block_platform.html.php
+++ core/views/admin_block_platform.html.php
@@ -1,34 +1,15 @@
 <? defined("SYSPATH") or die("No direct script access."); ?>
 <ul>
   <li>
-    Platform
-    <ul>
-      <li>
-        Apache 2.0.24
-        <ul>
-          <li>mod_rewrite: active</li>
-        </ul>
-      </li>
-      <li>
-        PHP 5.2.8
-        <ul>
-          <li>Memory: 32MB</li>
-          <li></li>
-          <li></li>
-        </ul>
-      </li>
-      <li>
-        MySQL 5.0.1
-      </li>
-      <li>
-        Graphics Toolkits
-        <ul>
-          <li>ImageMagick 1.6</li>
-          <li>GD</li>
-          <li>FFMPEG</li>
-        </ul>
-      </li>
-    </ul>
+    Operating System: <?= PHP_OS ?>
+  </li>
+  <li>
+    Apache: <?= apache_get_version() ?>
+  </li>
+  <li>
+    PHP <?= phpversion() ?>
+  </li>
+  <li>
+    MySQL: <?= mysql_get_server_info() ?>
   </li>
 </ul>
-<p class="gWarning">^ Display as a tree widget</p>
