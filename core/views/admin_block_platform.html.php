a5a9a79fa6a162d556b27020c3489fc2f27077e1
Bharat Mediratta <bharat@menalto.com>
2008-12-20 02:08:03 +0000

    Internationalize.

--- core/views/admin_block_platform.html.php
+++ core/views/admin_block_platform.html.php
@@ -1,15 +1,15 @@
 <? defined("SYSPATH") or die("No direct script access."); ?>
 <ul>
   <li>
-    Operating System: <?= PHP_OS ?>
+    <? printf(_("Operating System: %s"), PHP_OS) ?>
   </li>
   <li>
-    Apache: <?= apache_get_version() ?>
+    <? printf(_("Apache: %s"), apache_get_version()) ?>
   </li>
   <li>
-    PHP <?= phpversion() ?>
+    <? printf(_("PHP %s"), phpversion()) ?>
   </li>
   <li>
-    MySQL: <?= mysql_get_server_info() ?>
+    <? printf(_("MySQL: %s"), mysql_get_server_info()) ?>
   </li>
 </ul>
