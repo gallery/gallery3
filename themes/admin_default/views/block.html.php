30d541cb26b1754afdf0f5130dcf75af40457251
Bharat Mediratta <bharat@menalto.com>
2008-12-20 00:51:15 +0000

    Copy this from the default theme and change <h3> to <h2>

---
+++ themes/admin_default/views/block.html.php
@@ -0,0 +1,7 @@
+<? defined("SYSPATH") or die("No direct script access."); ?>
+<div id="<?= $id ?>" class="gBlock">
+  <h2><?= $title ?></h2>
+  <div class="gBlockContent">
+    <?= $content ?>
+  </div>
+</div>
