37d24e863c7a1356fb66d374810411537af80841
Bharat Mediratta <bharat@menalto.com>
2008-12-22 19:46:40 +0000

    Internationalize welcome block.

--- core/views/admin_block_welcome.html.php
+++ core/views/admin_block_welcome.html.php
@@ -1,12 +1,15 @@
 <? defined("SYSPATH") or die("No direct script access."); ?>
 <p>
-  This is your administration dashboard and it provides a quick overview of status messages,
-  recent updates, and frequently used options. Add or remove blocks and rearrange them
-  to tailor to your needs. The admin menu provides quick access to all of Gallery 3's options
-  and settings. Here are a few of the most used options to get you started.
+  <?= _("This is your administration dashboard and it provides a quick overview of status messages, recent updates, and frequently used options. Add or remove blocks and rearrange them to tailor to your needs. The admin menu provides quick access to all of Gallery 3's options and settings. Here are a few of the most used options to get you started.") ?>
 </p>
 <ul>
-  <li><a href="#">General Settings</a> - General configuation options for your Gallery.</li>
-  <li><a href="#">Modules</a> - Manage available and installed modules.</li>
-  <li><a href="#">Presentation</a> - Choose a theme, set image sizes.</li>
+  <li>
+    <?= sprintf(_("%sGeneral Settings%s - General configuation options for your Gallery."), "<a href=\"#\">", "</a>") ?>
+  </li>
+  <li>
+    <?= sprintf(_("%sModules%s - Manage available and installed modules."), "<a href=\"" . url::site("admin/modules") . "\">", "</a>") ?>
+  </li>
+  <li>
+    <?= sprintf(_("%sPresentation%s - Choose a theme, set image sizes."), "<a href=\"#\">", "</a>") ?>
+  </li>
 </ul>
