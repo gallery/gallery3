95ec6a8f571c8dbc93cbdcb871f763fdcf074d72
Bharat Mediratta <bharat@menalto.com>
2008-12-25 02:47:17 +0000

    Only admins can delete users.

--- modules/user/controllers/users.php
+++ modules/user/controllers/users.php
@@ -82,7 +82,7 @@ class Users_Controller extends REST_Controller {
    *  @see REST_Controller::_delete($resource)
    */
   public function _delete($user) {
-    if (!user::active()->admin && ($user->guest || $user->id != user::active()->id)) {
+    if (!user::active()->admin) {
       access::forbidden();
     }
     // Prevent CSRF
