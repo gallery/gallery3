1161cefada4644908145a6111ec2bec19887a0f2
Bharat Mediratta <bharat@menalto.com>
2008-12-28 10:14:12 +0000

    Javascript wrapper to ping tasks

---
+++ core/views/admin_maintenance_task.html.php
@@ -0,0 +1,23 @@
+<? defined("SYSPATH") or die("No direct script access."); ?>
+<script src="<?= url::file("lib/jquery.js") ?>" type="text/javascript"></script>
+<script type="text/javascript">
+  update = function() {
+    $.ajax({
+      url: "<?= url::site("admin/maintenance/run/$task->id") ?>",
+      dataType: "json",
+      success: function(data) {
+        $("#gStatus").html("" + data.task.status);
+        $("#gPercentComplete").html("" + data.task.percent_complete);
+        if (!data.task.done) {
+          setTimeout(update, 100);
+        }
+      }
+    });
+  }
+  update();
+</script>
+<div id="gProgressBar">
+  status: <span id="gStatus"></span>
+  <br/>
+  percent_complete: <span id="gPercentComplete"></span>
+</div>
