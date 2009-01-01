e8d10cab9d3391f2b5adf64b4a0745c49848f606
Chad Kieffer <chad@2tbsp.com>
2009-01-01 22:56:57 +0000

    Applied #gPager to next/last nav, moved up above photo and moved photo title back to below the photo.

--- themes/default/views/photo.html.php
+++ themes/default/views/photo.html.php
@@ -1,26 +1,27 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
-<div id="gItemHeader">
-  <?= $theme->photo_top() ?>
-  <h1><?= $item->title ?></h1>
-</div>
-
 <div id="gItem">
-  <img id="gPhotoId-<?= $item->id ?>" alt="<?= $item->title ?>" src="<?= $item->resize_url() ?>"
-       width="<?= $item->resize_width ?>"
-       height="<?= $item->resize_height ?>" />
-  <div><?= $item->description ?></div>
+  <?= $theme->photo_top() ?>
+  
+  <ul id="gPager">
+    <li><?= sprintf(_("%d of %d"), $position, $sibling_count) ?></li>
+    <? if ($previous_item): ?>
+    <li class="previous"><a href="<?= $previous_item->url() ?>"><?= _("previous") ?></a></li>
+    <? endif ?>
+    <? if ($next_item): ?>
+    <li class="next"><a href="<?= $next_item->url() ?>"><?= _("next") ?></a></li>
+    <? endif ?>
+  </ul>
+
+  <img id="gPhotoId-<?= $item->id ?>" 
+      src="<?= $item->resize_url() ?>"
+      alt="<?= $item->title ?>" 
+      width="<?= $item->resize_width ?>"
+      height="<?= $item->resize_height ?>" />
+
+  <div id="gInfo">
+    <h1><?= $item->title ?></h1>
+    <div><?= $item->description ?></div>
+  </div>
 
   <?= $theme->photo_bottom() ?>
 </div>
-
-<? if ($previous_item): ?>
-<a href="<?= $previous_item->url() ?>"><?= _("previous") ?></a>
-<? endif ?>
-
-<?= sprintf(_("Viewing photo %d of %d"), $position, $sibling_count) ?>
-
-<? if ($next_item): ?>
-<a href="<?= $next_item->url() ?>"><?= _("next") ?></a>
-<? endif ?>
-
-
