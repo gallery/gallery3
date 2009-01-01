7452a18938dc84393bf4009acdb713fe60c0b507
Chad Kieffer <chad@2tbsp.com>
2009-01-01 22:54:35 +0000

    Update the CSS item ID and class application for items and albums in albums. Miscellaneous CSS updates.

--- themes/default/views/album.html.php
+++ themes/default/views/album.html.php
@@ -1,5 +1,5 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
-<div id="gAlbumHeader">
+<div id="gInfo">
   <?= $theme->album_top() ?>
   <h1><?= $item->title ?></h1>
   <div class="gDescription"><?= $item->description ?></div>
@@ -7,20 +7,21 @@
 
 <ul id="gAlbumGrid">
   <? foreach ($children as $i => $child): ?>
-  <? $album_class = ""; ?>
-  <? if ($child->is_album()): ?>
-  <? $album_class = "gAlbum "; ?>
-  <? endif ?>
-  <li id="g<?= $child->id ?>" class="gItem <?= $album_class ?>">
+    <? $item_class = "gPhoto"; ?>
+    <? if ($child->is_album()): ?>
+      <? $item_class = "gAlbum"; ?>
+    <? endif ?>
+  <li id="gItemId-<?= $child->id ?>" class="gItem <?= $item_class ?>">
     <?= $theme->thumb_top($child) ?>
     <a href="<?= $child->url() ?>">
-      <img id="gPhotoId-<?= $child->id ?>" class="gThumbnail"
-           alt="photo" src="<?= $child->thumb_url() ?>"
+      <img class="gThumbnail"
+           src="<?= $child->thumb_url() ?>"
+           alt="<?= $child->title ?>" 
            width="<?= $child->thumb_width ?>"
            height="<?= $child->thumb_height ?>" />
     </a>
-    <h2><a href="<?= $child->url() ?>"><?= $child->title ?></a></h2>
     <?= $theme->thumb_bottom($child) ?>
+    <h2><a href="<?= $child->url() ?>"><?= $child->title ?></a></h2>
     <ul class="gMetadata">
       <?= $theme->thumb_info($child) ?>
     </ul>
