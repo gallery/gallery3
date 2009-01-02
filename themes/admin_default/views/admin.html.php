1ce19a994dfcc8c0d79e6a2f9492dcbee8599d86
Chad Kieffer <chad@2tbsp.com>
2009-01-02 20:09:28 +0000

    Set the admin layout to 100% of the window. Add IE fix stylesheet for admin theme, reduce height of admin header.

--- themes/admin_default/views/admin.html.php
+++ themes/admin_default/views/admin.html.php
@@ -9,30 +9,30 @@
         type="text/css" media="screen,projection">
     <link rel="stylesheet" href="<?= url::file("themes/default/css/screen.css") ?>"
         type="text/css" media="screen,projection">
-    <link rel="stylesheet" href="<?= $theme->url("css/screen.css") ?>"
-        type="text/css" media="screen,projection">
-    <link rel="stylesheet" href="<?= $theme->url("css/superfish.css") ?>"
+    <link rel="stylesheet" href="<?= url::file("themes/default/css/superfish.css") ?>"
         type="text/css" media="screen,projection">
     <link rel="stylesheet" href="<?= $theme->url("css/superfish-navbar.css") ?>"
+        type="text/css" media="screen,projection">
+    <link rel="stylesheet" href="<?= $theme->url("css/screen.css") ?>"
         type="text/css" media="screen,projection">
     <script src="<?= url::file("lib/jquery.js") ?>" type="text/javascript"></script>
     <script src="<?= url::file("lib/jquery.form.js") ?>" type="text/javascript"></script>
     <script src="<?= url::file("lib/jquery-ui.js") ?>" type="text/javascript"></script>
     <script src="<?= url::file("lib/gallery.dialog.js") ?>" type="text/javascript"></script>
-    <script src="<?= $theme->url("js/superfish.js") ?>" type="text/javascript"></script>
+    <script src="<?= url::file("themes/default/js/superfish.js") ?>" type="text/javascript"></script>
     <script src="<?= $theme->url("js/ui.init.js") ?>" type="text/javascript"></script>
     <?= $theme->admin_head() ?>
   </head>
 
   <body>
     <?= $theme->admin_page_top() ?>
-    <div id="doc4" class="yui-t5 gView">
+    <div id="doc3" class="yui-t5 gView">
       <div id="hd">
         <div id="gHeader">
           <?= $theme->site_status() ?>
           <?= $theme->admin_header_top() ?>
           <ul id="gLoginMenu">
-            <li><?= html::anchor("albums/1", "Browse Gallery") ?></li>
+            <li><?= html::anchor("albums/1", "Browse the Gallery") ?></li>
             <li id="gLogoutLink"><a href="<?= url::site("logout?continue=albums/1") ?>">Logout</a></li>
           </ul>
           <img src="<?= $theme->url("images/logo.png") ?>" id="gLogo" alt="<?= _("Gallery 3: Your Photos on Your Web Site") ?>" />
