e63966b49063ceebdb948e7992af6504c6dcf477
Bharat Mediratta <bharat@menalto.com>
2009-01-02 01:20:06 +0000

    Tweak indentation

--- core/views/admin_themes.html.php
+++ core/views/admin_themes.html.php
@@ -1,13 +1,17 @@
 <? defined("SYSPATH") or die("No direct script access.") ?>
 <div id="gThemes">
   <h1><?= _("Theme Administration") ?></h2>
-    <p><?= _("These are the themes in your system") ?></p>
+    <p>
+      <?= _("These are the themes in your system") ?>
+    </p>
     <form method="post" action="<?= url::site("admin/themes/save") ?>">
-    <?= access::csrf_form_field() ?>
-    <? foreach ($themes as $theme): ?>
-    <input type="radio" name="theme" value="<?= $theme ?>"
-    <? if ($theme == $active): ?> checked="checked" <? endif ?> /><?= $theme ?>
-    <? endforeach ?>
-    <input type="submit" value="<?= _("Save") ?>"/>
+      <?= access::csrf_form_field() ?>
+      <? foreach ($themes as $theme): ?>
+      <input type="radio" name="theme" value="<?= $theme ?>"
+             <? if ($theme == $active): ?> checked="checked" <? endif ?>
+             />
+      <?= $theme ?>
+      <? endforeach ?>
+      <input type="submit" value="<?= _("Save") ?>"/>
     </form>
 </div>
