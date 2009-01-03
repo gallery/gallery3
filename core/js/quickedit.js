8ed5200101ac6feb48795b2fd8b2fb4a3462d45e
Bharat Mediratta <bharat@menalto.com>
2009-01-03 20:13:08 +0000

    Add a loading dialog when we quickedit.

--- core/js/quickedit.js
+++ core/js/quickedit.js
@@ -3,9 +3,10 @@ $(document).ready(function() {
 });
 
 var show_quickedit = function() {
+  var quick_edit = $(this);
   $("#gQuickEditPane").remove();
-  $(this).append("<div id=\"gQuickEditPane\"></div>");
-  var img = $(this).find("img");
+  quick_edit.append("<div id=\"gQuickEditPane\"></div>");
+  var img = quick_edit.find("img");
   var pos = img.position();
   $("#gQuickEditPane").css({
     "position": "absolute",
@@ -14,29 +15,33 @@ var show_quickedit = function() {
     "width": img.innerWidth() + 1,
     "height": 32
   });
-  $(this).hover(function() { }, hide_quickedit);
+  quick_edit.hover(function() { }, hide_quickedit);
   $.get(
-    $(this).attr("quickedit_link"),
+    quick_edit.attr("quickedit_link"),
     {},
     function(data, textStatus) {
       $("#gQuickEditPane").html(data);
       $("#gQuickEditPane div").click(function() {
-	quickedit($(this).attr("quickedit_link"), img);
+        quickedit(quick_edit, $(this), img);
       });
     }
   );
 };
 
-var quickedit = function(url, img) {
+var quickedit = function(quick_edit, pane, img) {
+  img.css("opacity", "0.2");
+  quick_edit.addClass("gLoadingLarge");
   $.ajax({
     type: "GET",
-    url: url,
+    url: pane.attr("quickedit_link"),
     dataType: "json",
     success: function(data) {
+      img.css("opacity", "1");
       img.attr("width", data.width);
       img.attr("height", data.height);
       img.attr("src", data.src);
       var pos = img.position();
+      quick_edit.removeClass("gLoadingLarge");
       $("#gQuickEditPane").css({
 	"position": "absolute",
 	"top": pos.top,
