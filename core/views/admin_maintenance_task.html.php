db4164520aa71b0990adb48b43dcf4668c06e810
Bharat Mediratta <bharat@menalto.com>
2009-01-06 08:59:09 +0000

    Add pause and done buttons to the progressbar

--- core/views/admin_maintenance_task.html.php
+++ core/views/admin_maintenance_task.html.php
@@ -1,5 +1,4 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
-<script src="<?= url::file("lib/jquery.js") ?>" type="text/javascript"></script>
 <script type="text/javascript">
   update = function() {
     $.ajax({
@@ -8,16 +7,26 @@
       success: function(data) {
         $("#gStatus").html("" + data.task.status);
         $("#gPercentComplete").html("" + data.task.percent_complete);
-        if (!data.task.done) {
+        if (data.task.done) {
+          $("#gPauseButton").hide();
+          $("#gDoneButton").show();
+        } else {
           setTimeout(update, 100);
         }
       }
     });
   }
   update();
+  dismiss = function() {
+    window.location.reload();
+  }
 </script>
 <div id="gProgressBar">
   status: <span id="gStatus"></span>
   <br/>
   percent_complete: <span id="gPercentComplete"></span>
+  <div>
+    <button id="gPauseButton" onclick="dismiss()"><?= _("Pause") ?></button>
+    <button id="gDoneButton" style="display: none" onclick="dismiss()"><?= _("Done") ?></button>
+  </div>
 </div>
