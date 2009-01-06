cb3109784e073cc02216e189eb9f17818c078f30
Bharat Mediratta <bharat@menalto.com>
2009-01-06 10:10:54 +0000

    Wrap permissions in a <fieldset> so that we get a label for the dialog

--- core/views/permissions_form.html.php
+++ core/views/permissions_form.html.php
@@ -1,47 +1,50 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
 <form method="post" action="<?= url::site("permissions/edit/$item->id") ?>">
   <?= access::csrf_form_field() ?>
+  <fieldset>
+    <legend><?= _("Edit Permissions") ?> </legend>
 
-  <table>
-    <tr>
-      <th> </th>
-      <? foreach ($groups as $group): ?>
-      <th> <?= $group->name ?> </th>
-      <? endforeach ?>
-    </tr>
+    <table>
+      <tr>
+        <th> </th>
+        <? foreach ($groups as $group): ?>
+        <th> <?= $group->name ?> </th>
+        <? endforeach ?>
+      </tr>
 
-    <? foreach ($permissions as $permission): ?>
-    <tr>
-      <td> <?= _($permission->display_name) ?> </td>
-      <? foreach ($groups as $group): ?>
-      <td>
-        <? $intent = access::group_intent($group, $permission->name, $item) ?>
-        <? $allowed = access::group_can($group, $permission->name, $item) ?>
-        <? $lock = access::locked_by($group, $permission->name, $item) ?>
+      <? foreach ($permissions as $permission): ?>
+      <tr>
+        <td> <?= _($permission->display_name) ?> </td>
+        <? foreach ($groups as $group): ?>
+        <td>
+          <? $intent = access::group_intent($group, $permission->name, $item) ?>
+          <? $allowed = access::group_can($group, $permission->name, $item) ?>
+          <? $lock = access::locked_by($group, $permission->name, $item) ?>
 
-        <? if ($lock): ?>
-          <?= _("denied and locked by") ?> <a href="javascript:show(<?= $lock->id ?>)"><?= _("parent") ?></a>
-        <? else: ?>
-          <? if ($intent === null): ?>
-            <? if ($allowed): ?>
-              <a href="javascript:set('deny',<?= $group->id ?>,<?= $permission->id ?>,<?= $item->id ?>)">allowed by parent</a>
-            <? else: ?>
-              <a href="javascript:set('deny',<?= $group->id ?>,<?= $permission->id ?>,<?= $item->id ?>)">denied by parent</a>
-            <? endif ?>
-          </a>
-          <? elseif ($intent === access::DENY): ?>
-            <a href="javascript:set('allow',<?= $group->id ?>,<?= $permission->id ?>,<?= $item->id ?>)">denied</a>
-          <? elseif ($intent === access::ALLOW): ?>
-            <? if ($item->id == 1): ?>
-            <a href="javascript:set('deny',<?= $group->id ?>,<?= $permission->id ?>,<?= $item->id ?>)">allowed</a>
-            <? else: ?>
-            <a href="javascript:set('reset',<?= $group->id ?>,<?= $permission->id ?>,<?= $item->id ?>)">allowed</a>
+          <? if ($lock): ?>
+            <?= _("denied and locked by") ?> <a href="javascript:show(<?= $lock->id ?>)"><?= _("parent") ?></a>
+          <? else: ?>
+            <? if ($intent === null): ?>
+              <? if ($allowed): ?>
+                <a href="javascript:set('deny',<?= $group->id ?>,<?= $permission->id ?>,<?= $item->id ?>)">allowed by parent</a>
+              <? else: ?>
+                <a href="javascript:set('deny',<?= $group->id ?>,<?= $permission->id ?>,<?= $item->id ?>)">denied by parent</a>
+              <? endif ?>
+            </a>
+            <? elseif ($intent === access::DENY): ?>
+              <a href="javascript:set('allow',<?= $group->id ?>,<?= $permission->id ?>,<?= $item->id ?>)">denied</a>
+            <? elseif ($intent === access::ALLOW): ?>
+              <? if ($item->id == 1): ?>
+              <a href="javascript:set('deny',<?= $group->id ?>,<?= $permission->id ?>,<?= $item->id ?>)">allowed</a>
+              <? else: ?>
+              <a href="javascript:set('reset',<?= $group->id ?>,<?= $permission->id ?>,<?= $item->id ?>)">allowed</a>
+              <? endif ?>
             <? endif ?>
           <? endif ?>
-        <? endif ?>
-      </td>
+        </td>
+        <? endforeach ?>
+      </tr>
       <? endforeach ?>
-    </tr>
-    <? endforeach ?>
-  </table>
+    </table>
+  <fieldset>
 </form>
