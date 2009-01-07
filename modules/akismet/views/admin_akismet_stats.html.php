8bcc3de28df6451c17c2be232595f9023c98e4b3
Bharat Mediratta <bharat@menalto.com>
2009-01-07 08:20:22 +0000

    Listen for comment creation, and run it through Akismet.  Make the akismet stats page take up as much room as possible.

--- modules/akismet/views/admin_akismet_stats.html.php
+++ modules/akismet/views/admin_akismet_stats.html.php
@@ -1,5 +1,6 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
 <iframe width="100%"
+        height="100%"
         style="border: 0px"
         src="http://<?= $api_key ?>.web.akismet.com/1.0/user-stats.php?blog=<?= urlencode($blog_url) ?>">
 </iframe>
