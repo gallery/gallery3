a0f3c09a07f2673f0ce2fac67c13df607d72061e
Bharat Mediratta <bharat@menalto.com>
2009-01-09 04:43:34 +0000

    Eliminate unused avatar code (for now)

--- modules/comment/views/comment.html.php
+++ modules/comment/views/comment.html.php
@@ -1,11 +1,8 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
 <li id="gComment-<?= $comment->id; ?>">
-  <? $avatar = $theme->url("images/avatar.jpg") ?>
-  <? //if ($user->avatar($comment->author)): ?>
-    <? //$avatar = $theme->url("images/avatar.jpg") ?>
-  <? //endif ?>
   <p class="gAuthor">
-    <a href="#"><img src="<?= $avatar ?>" class="gAvatar" alt="<?= $comment->author ?>" /></a>
+    <a href="#"><img src="<?= $theme->url("images/avatar.jpg") ?>"
+                     class="gAvatar" alt="<?= $comment->author ?>" /></a>
     <?= t("on ") . date("Y-M-d H:i:s", $comment->created) ?>
     <a href="#"><?= $comment->author ?></a> <?= t("said") ?>
   </p>
--- modules/comment/views/comments.html.php
+++ modules/comment/views/comments.html.php
@@ -3,12 +3,8 @@
 <ul id="gComments">
   <? foreach ($comments as $comment): ?>
   <li id="gComment-<?= $comment->id; ?>">
-    <? $avatar = $theme->url("images/avatar.jpg") ?>
-    <? //if ($user->avatar($comment->author)): ?>
-      <? //$avatar = $theme->url("images/avatar.jpg") ?>
-    <? //endif ?>
     <p class="gAuthor">
-      <a href="#"><img src="<?= $avatar ?>" class="gAvatar" alt="<?= $comment->author ?>" /></a>
+      <a href="#"><img src="<?= $theme->url("images/avatar.jpg") ?>" class="gAvatar" alt="<?= $comment->author ?>" /></a>
       <?= t("on ") . date("Y-M-d H:i:s", $comment->created) ?>
       <a href="#"><?= $comment->author ?></a> <?= t("said") ?>
     </p>
