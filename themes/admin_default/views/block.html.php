5fdcdc0e682f3ae42652bddea274e824e2a51974
Chad Kieffer <chad@2tbsp.com>
2009-01-12 03:49:19 +0000

    Applied jquery ui dialog titlebar to dashboard blocks to add a close button. It's not semantically "nice" but it works for now.

--- themes/admin_default/views/block.html.php
+++ themes/admin_default/views/block.html.php
@@ -1,6 +1,11 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
-<div id="<?= $id ?>" class="gBlock">
-  <h2><?= $title ?></h2>
+<div id="<?= $id ?>" class="gBlock ui-widget-dialog">
+  <div class="ui-dialog-titlebar ui-widget-header">
+    <a href="#" class="ui-dialog-titlebar-close">
+      <span class="ui-icon ui-icon-closethick">close</span>
+    </a>
+    <?= $title ?>
+  </div>
   <div class="gBlockContent">
     <?= $content ?>
   </div>
