3e5ef37ee8817232556ad15ba48c1bd940101e25
Chad Kieffer <chad@2tbsp.com>
2009-01-16 00:55:36 +0000

    Make admin dashboard blocks draggable. Updated order and position not yet saved.

--- themes/admin_default/js/ui.init.js
+++ themes/admin_default/js/ui.init.js
@@ -29,4 +29,25 @@ $(document).ready(function(){
         $("#gMessage").append("<span class='gError'>" + body.message + "</span>");
       }
   }});
+  
+  // Sortable dashboard blocks
+  $("#gContent").sortable({
+    connectWith: ["#gSidebar"],
+    cursor: "move",
+    opacity: 0.7,
+    placeholder: "gBlock",
+    update: function(){
+        console.log($(this).sortable("serialize"));
+      }
+  });
+  $("#gSidebar").sortable({
+    connectWith: ["#gContent"],
+    containment: "document",
+    cursor: "move",
+    opacity: 0.7,
+    placeholder: "gBlock",
+    update: function(){
+        console.log($(this).sortable("serialize"));
+      }
+  });  
 });
