6d99c29172a6b8a5e3ceb9df27b5fcb56337a0b2
Jakob Hilden <jhild@umich.edu>
2009-01-20 22:16:25 +0000

    totally incomplete version of user admin UI.

--- modules/user/views/admin_users.html.php
+++ modules/user/views/admin_users.html.php
@@ -1,22 +1,106 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
+<style>
+	.gButtonLink {
+		border-width: 1px;
+		border-style: solid;
+		border-color: #ececec #c8c8c8 #c8c8c8 #ececec;
+		background-image: url('/gallery3/themes/admin_default/images/backg-buttonlink.png');
+		padding: .2em .3em .2em .3em;
+		font-weight: bold;
+	}
+	.gButtonLink:hover {
+		border-color: #c8c8c8 #ececec #ececec #c8c8c8;
+	}
+	.gBlock h2 a {
+		font-size: .7em;
+		float: right;
+		position: relative;
+		top: -1.69em;
+	}
+	
+	.gUserAdminList li {
+		padding: .4em .4em .3em .4em;
+		position: relative;
+	}
+	.gUserAdminList li img {
+		width: 20px
+		height: 20px;
+		cursor: move;
+	}
+	.gFirstRow {
+		border-bottom: 1px solid grey;
+		padding-bottom: .5em;
+		padding-left: 30px !important;
+	}
+	.gOddRow {
+		background-color: #f1f1f1;
+	}
+	.gActions {
+		position: absolute;
+		left: 400px;
+	}
+	.gActions a {
+		margin-right: 40px;
+	}
+	.gUserEdit {
+		display: none;
+	}
+</style>
+
 <div class="gBlock">
-  <h2><?= t("User Administration") ?></h2>
+  <h2>
+  	<?= t("User Admin") ?>
+		<a class="gButtonLink" href="#" title="<?= t("Create a new user") ?>">+ <?= t("Add user") ?></a>
+	</h2>
+	
   <div class="gBlockContent">
-    <p><?= t("These are the users in your system") ?></p>
-    <ul>
+    <ul class="gUserAdminList">
+    	<li class="gFirstRow">
+    		<strong>Username</strong>
+				(Full name)
+				<span class="understate">last login</span>
+    	</li>
+			
       <? foreach ($users as $i => $user): ?>
-      <li>
-        <?= $user->name ?>
-        <?= ($user->last_login == 0) ? "" : "(" . date("M j, Y", $user->last_login) . ")" ?>
-        <a href="users/edit_form/<?= $user->id ?>" class="gDialogLink"><?= t("edit") ?></a>
-        <? if (!(user::active()->id == $user->id || user::guest()->id == $user->id)): ?>
-        <a href="users/delete_form/<?= $user->id ?>" class="gDialogLink">
-          <?= t("delete") ?></a>
-        <? endif ?>
-      </li>
+	      <li class="<?= ($i % 2 == 0) ? "gEvenRow" : "gOddRow" ?>">
+	      	<img src="<?= $theme->url("images/avatar.jpg") ?>"
+						title="<?= t("Drag user onto group below to add as a new member") ?>"
+						width="20" height="20" />
+	        <strong><?= $user->name ?></strong>
+					(<?= $user->full_name ?>)
+	        <span class="understate">
+	        	<?= ($user->last_login == 0) ? "" : date("M j, Y", $user->last_login) ?>
+					</span>
+					
+					<span class="gActions">
+						<a href="#" onclick="$('gUserEdit-<?= $user->id ?>').slideDown('slow');"><?= t("edit") ?></a>
+		        <!--<a href="users/edit_form/<?= $user->id ?>" class="gDialogLink"><?= t("edit") ?></a>-->
+		        <? if (!(user::active()->id == $user->id || user::guest()->id == $user->id)): ?>
+		        	<a href="users/delete_form/<?= $user->id ?>" class="gDialogLink"><?= t("delete") ?></a>
+		        <? endif ?>
+					</span>
+	      </li>
+				
+				<li id="gUserEdit-<?= $user->id ?>" class="gUserEdit">
+					<form>
+						<fieldset>
+							<label>Username</label>
+							<input type="text" />
+							<label>Full name</label>
+							<input type="text" />
+							<label>Email</label>
+							<input type="text" />
+							...
+							<input type="submit" value="Save changes" />
+							<a href="#">cancel</a>
+						</fieldset>
+					</form>
+				</li>
       <? endforeach ?>
-      <li><a href="users/add_form" class="gDialogLink" title="<?= t("Add user") ?>">
-        <?= t("Add user") ?></a></li>
     </ul>
+		<br />
+		<a href="users/add_form" class="gDialogLink gButtonLink" title="<?= t("Create a new user") ?>">
+  		+ <?= t("Add a new user") ?>
+		</a>
   </div>
 </div>
