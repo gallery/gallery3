be1f9d6892bb766ccbc8b09b99c39eab373463d7
Bharat Mediratta <bharat@menalto.com>
2009-01-20 08:25:21 +0000

    fix indentation

--- core/views/quick_pane.html.php
+++ core/views/quick_pane.html.php
@@ -1,6 +1,6 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
 <a class="edit gDialogLink" href="<?= url::site("quick/form_edit/$item->id") ?>"
-      title="<?= t("Edit this item's metadata") ?>">
+  title="<?= t("Edit this item's metadata") ?>">
   <span>
     <?= t("Edit this item") ?>
   </span>
@@ -8,13 +8,13 @@
 
 <? if ($item->type == "photo" && graphics::can("rotate")): ?>
 <a class="clockwise" href="<?= url::site("quick/rotate/$item->id/cw?csrf=" . access::csrf_token()) ?>"
-      title="<?= t("Rotate 90 degrees clockwise") ?>">
+  title="<?= t("Rotate 90 degrees clockwise") ?>">
   <span>
     <?= t("Rotate 90 degrees clockwise") ?>
   </span>
 </a>
 <a class="counter-clockwise" href="<?= url::site("quick/rotate/$item->id/ccw?csrf=" . access::csrf_token()) ?>"
-      title="<?= t("Rotate 90 degrees counter clockwise") ?>">
+  title="<?= t("Rotate 90 degrees counter clockwise") ?>">
   <span>
     <?= t("Rotate 90 degrees counter clockwise") ?>
   </span>
@@ -22,7 +22,7 @@
 <? endif ?>
 
 <a class="move" href="<?= url::site("quick/form_edit/$item->id") ?>"
-      title="<?= t("Move this item to another album") ?>">
+  title="<?= t("Move this item to another album") ?>">
   <span>
     <?= t("Move this item to another album") ?>
   </span>
@@ -30,7 +30,7 @@
 
 <? if ($item->type == "photo"): ?>
 <a class="cover" href="#"
-      title="<?= t("Select as album cover") ?>">
+   title="<?= t("Select as album cover") ?>">
   <span>
     <?= t("Select as album cover") ?>
   </span>
@@ -38,14 +38,14 @@
 <? endif ?>
 
 <a class="delete" href="#"
-      title="<?= t("Delete this item") ?>">
+   title="<?= t("Delete this item") ?>">
   <span>
     <?= t("Delete this item") ?>
   </span>
 </a>
 
 <a class="options" href="#"
-      title="<?= t("Additional options") ?>">
+   title="<?= t("Additional options") ?>">
   <span>
     <?= t("Additional options") ?>
   </span>
