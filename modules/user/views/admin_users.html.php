9048a5afcaa7399082fe350e6d75bcdf012fcbc2
Bharat Mediratta <bharat@menalto.com>
2009-01-27 21:54:22 +0000

    Clean up indentation

--- modules/user/views/admin_users.html.php
+++ modules/user/views/admin_users.html.php
@@ -1,29 +1,29 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
 <div class="gBlock">
-	<a href="<?= url::site("admin/users/add_form") ?>"
-      class="gDialogLink gButtonLink right"
-      title="<?= t("Create a new user") ?>">
-  	+ <?= t("Add a new user") ?>
-	</a>
+  <a href="<?= url::site("admin/users/add_form") ?>"
+     class="gDialogLink gButtonLink right"
+     title="<?= t("Create a new user") ?>">
+    + <?= t("Add a new user") ?>
+  </a>
 
   <h2>
-  	<?= t("User Admin") ?>
-	</h2>
-	
+    <?= t("User Admin") ?>
+  </h2>
+
   <div class="gBlockContent">
     <ul id="gUserAdminList">
       <li class="gHeaderRow">
-    	  <strong><?= t("Username") ?></strong> <?= t("(Full name)") ?>
-	      <span class="understate"><?= t("last login") ?></span>
+    	<strong><?= t("Username") ?></strong> <?= t("(Full name)") ?>
+	<span class="understate"><?= t("last login") ?></span>
       </li>
 
       <? foreach ($users as $i => $user): ?>
       <li class="<?= text::alternate("gOddRow", "gEvenRow") ?>">
         <img src="<?= $theme->url("images/avatar.jpg") ?>"
-            title="<?= t("Drag user onto group below to add as a new member") ?>"
-            alt="<?= $user->name ?>"
-            width="20"
-            height="20" />
+             title="<?= t("Drag user onto group below to add as a new member") ?>"
+             alt="<?= $user->name ?>"
+             width="20"
+             height="20" />
         <strong><?= $user->name ?></strong>
         (<?= $user->full_name ?>)
         <span class="understate">
@@ -31,13 +31,13 @@
         </span>
         <span class="gActions">
           <a href="users/edit_form/<?= $user->id ?>" class="gPanelLink"><?= t("edit") ?></a>
-        <? if (user::active()->id != $user->id && !$user->guest): ?>
+          <? if (user::active()->id != $user->id && !$user->guest): ?>
           <a href="users/delete_form/<?= $user->id ?>" class="gDialogLink"><?= t("delete") ?></a>
-        <? else: ?>
-          <span class="inactive" title="<?= t("This user can't be deleted") ?>">
+          <? else: ?>
+          <span class="inactive" title="<?= t("This user cannot be deleted") ?>">
             <?= t("delete") ?>
           </span>
-        <? endif ?>
+          <? endif ?>
         </span>
       </li>
       <? endforeach ?>
@@ -49,36 +49,35 @@
         + <?= t("Add a new user") ?>
       </a>
     </p>
-    
   </div>
 </div>
 
 <div id="gGroupAdmin" class="gBlock">
   <a href="groups/add_form"
-      class="gDialogLink gButtonLink right" 
-      title="<?= t("Create a new group") ?>">
-  	+ <?= t("Add a new group") ?>
-	</a>
+     class="gDialogLink gButtonLink right"
+     title="<?= t("Create a new group") ?>">
+    + <?= t("Add a new group") ?>
+  </a>
 
   <h2>
-  	<?= t("Group Admin") ?>
-	</h2>
+    <?= t("Group Admin") ?>
+  </h2>
 
   <div class="gBlockContent">
-  	<ul>
-  		<? foreach ($groups as $i => $group): ?>
-			<li class="gGroup">
-				<strong><?= $group->name?></strong><br />
-				<ul>
-					<? foreach ($group->users as $i => $user): ?>
-					<li class="gUser">
-						<?= $user->name ?>
-						<a href="groups/remove_users/<?= $user->id ?>">X</a>
-					</li>
-					<? endforeach ?>
-				</ul>
-			</li>
-			<? endforeach ?>
-		</ul>
+    <ul>
+      <? foreach ($groups as $i => $group): ?>
+      <li class="gGroup">
+	<strong><?= $group->name?></strong><br />
+	<ul>
+	  <? foreach ($group->users as $i => $user): ?>
+	  <li class="gUser">
+	    <?= $user->name ?>
+	    <a href="groups/remove_users/<?= $user->id ?>">X</a>
+	  </li>
+	  <? endforeach ?>
+	</ul>
+      </li>
+      <? endforeach ?>
+    </ul>
   </div>
 </div>
