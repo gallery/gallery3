6a8daaef5e2df4a98e1f47d4f4107c00c03d9498
Bharat Mediratta <bharat@menalto.com>
2009-02-05 19:21:29 +0000

    Don't try to show the recaptcha if we don't have a public key

--- modules/recaptcha/helpers/recaptcha_event.php
+++ modules/recaptcha/helpers/recaptcha_event.php
@@ -17,9 +17,10 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
-
 class recaptcha_event_Core {
   static function comment_add_form($form) {
-    $form->add_comment->recaptcha("recaptcha")->label("")->id("gRecaptcha");
+    if (module::get_var("recaptcha", "public_key")) {
+      $form->add_comment->recaptcha("recaptcha")->label("")->id("gRecaptcha");
+    }
   }
 }
