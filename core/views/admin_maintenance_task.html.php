af944f94334139d6b49b9344795652510a62bae0
Bharat Mediratta <bharat@menalto.com>
2009-02-09 05:28:30 +0000

    Use jquery ui's progressbar

--- core/views/admin_maintenance_task.html.php
+++ core/views/admin_maintenance_task.html.php
@@ -5,8 +5,8 @@
       url: "<?= url::site("admin/maintenance/run/$task->id?csrf=$csrf") ?>",
       dataType: "json",
       success: function(data) {
+        $("#gProgressBar").progressbar("value", data.task.percent_complete);
         $("#gStatus").html("" + data.task.status);
-        $("#gPercentComplete").html("" + data.task.percent_complete);
         if (data.task.done) {
           $("#gPauseButton").hide();
           $("#gDoneButton").show();
@@ -16,15 +16,15 @@
       }
     });
   }
+  $("#gProgressBar").progressbar({value: 0});
   update();
   dismiss = function() {
     window.location.reload();
   }
 </script>
-<div id="gProgressBar">
-  status: <span id="gStatus"></span>
-  <br/>
-  percent_complete: <span id="gPercentComplete"></span>
+<div id="gProgress">
+  <div id="gProgressBar"></div>
+  <div id="gStatus"></div>
   <div>
     <button id="gPauseButton" onclick="dismiss()"><?= t("Pause") ?></button>
     <button id="gDoneButton" style="display: none" onclick="dismiss()"><?= t("Done") ?></button>
