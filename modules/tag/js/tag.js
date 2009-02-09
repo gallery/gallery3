9f4fdcd8d50a9b4bd4bf2bcdd66a705b8b458df9
Bharat Mediratta <bharat@menalto.com>
2009-02-09 00:30:13 +0000

    Just reset the tag form after submission; no need to replace it with one from the backend because it's going to be the same form anyway.

--- modules/tag/js/tag.js
+++ modules/tag/js/tag.js
@@ -6,16 +6,12 @@ function ajaxify_tag_form() {
   $("#gTag form").ajaxForm({
     dataType: "json",
     success: function(data) {
-      if (data.form) {
-        $("#gTag form").replaceWith(data.form);
-        ajaxify_tag_form();
-      }
       if (data.result == "success") {
         $.get($("#gTagCloud").attr("src"), function(data, textStatus) {
 	      $("#gTagCloud").html(data);
 	    });
       }
-      $("#gTag form").clearForm();
+      $("#gTag form").resetForm();
     }
   });
 }
