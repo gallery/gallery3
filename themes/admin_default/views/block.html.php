b15e0c75291ee68a8e54c0dcda7985cad6b44ece
Chad Kieffer <chad@2tbsp.com>
2009-02-09 00:28:15 +0000

    Move close X back over to the right of the titlebar.

--- themes/admin_default/views/block.html.php
+++ themes/admin_default/views/block.html.php
@@ -1,9 +1,9 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
-<div block_id="<?= $id ?>" id="<?= $css_id ?>" class="gBlock ui-widget-dialog">
-  <div class="ui-dialog-titlebar ui-widget-header">
+<div block_id="<?= $id ?>" id="<?= $css_id ?>" class="gBlock ui-widget">
+  <div class="ui-dialog-titlebar ui-widget-header ui-helper-clearfix ui-icon-right">
     <? if ($css_id != "gBlockAdder"): ?>
     <a href="<?= url::site("admin/dashboard/remove_block/$id?csrf=" . access::csrf_token()) ?>"
-       class="ui-dialog-titlebar-close">
+       class="ui-dialog-titlebar-close ui-corner-all">
       <span class="ui-icon ui-icon-closethick">remove</span>
     </a>
     <? endif ?>
