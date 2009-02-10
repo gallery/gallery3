079a518334f0dfb2a67b402ac8680456964e1b04
Bharat Mediratta <bharat@menalto.com>
2009-02-10 05:00:29 +0000

    convert ' to " to match our HTML standards.

--- core/views/l10n_client.html.php
+++ core/views/l10n_client.html.php
@@ -1,26 +1,27 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
-
-<div id='l10n-client' class='hidden'>
-  <div class='labels'>
-    <span class='toggle'><?= t('Translate Text') ?></span>
-    <div class='label strings'><h2><?= t('Page Text') ?></h2></div>
-    <div class='label source'><h2><?= t('Source') ?></div>
-    <div class='label translation'><h2><?= t('Translation to %language',
-                                             array('%language' => 'TODO')) ?></h2></div>
+<div id="l10n-client" class="hidden">
+  <div class="labels">
+    <span class="toggle"><?= t("Translate Text") ?></span>
+    <div class="label strings"><h2><?= t("Page Text") ?></h2></div>
+    <div class="label source"><h2><?= t("Source") ?></div>
+    <div class="label translation"><h2><?= t("Translation to %language",
+                                             array("%language" => "TODO")) ?></h2></div>
   </div>
-  <div id='l10n-client-string-select'>
-    <ul class='string-list'>
+  <div id="l10n-client-string-select">
+    <ul class="string-list">
       <? foreach ($string_list as $string): ?>
-      <li class='<?= $string["translation"] === ''  ? "untranslated" : "translated" ?>'><?= $string["source"] ?></li>
+      <li class="<?= $string["translation"] === ""  ? "untranslated" : "translated" ?>">
+        <?= $string["source"] ?>
+      </li>
       <? endforeach; ?>
     </ul>
     <?= $l10n_search_form ?>
   </div>
-  <div id='l10n-client-string-editor'>
-    <div class='source'>
-      <div class='source-text'></div>
+  <div id="l10n-client-string-editor">
+    <div class="source">
+      <div class="source-text"></div>
     </div>
-    <div class='translation'>
+    <div class="translation">
       <?= $l10n_form ?>
     </div>
   </div>
