afa1805f15e7f54d54390db6e26011001e04601d
Bharat Mediratta <bharat@menalto.com>
2009-02-10 08:32:33 +0000

    Remove redundant "Gallery"

--- core/views/admin_languages.html.php
+++ core/views/admin_languages.html.php
@@ -1,6 +1,6 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
 <div id="gLanguages">
-  <h1> <?= t("Gallery Language Settings") ?> </h1>
+  <h1> <?= t("Language Settings") ?> </h1>
 
   <?= $form ?>
 </div>
