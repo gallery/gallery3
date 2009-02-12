6adf661ca7927a1cf8dc0c1e87c2739bd8762a7f
Tim Almdal <tnalmdal@shaw.ca>
2009-02-12 01:12:33 +0000

    Change the size of the text and and some odd/even coloring.  Hopefully some one will be so disgusted with these colors, they will change them :-).

--- modules/exif/views/exif_dialog.html.php
+++ modules/exif/views/exif_dialog.html.php
@@ -1,29 +1,33 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
+<style>
+  #gExifData {font-size: .85em;}
+  .gOdd {background: #BDD2FF;}
+  .gEven {background: #DFEFFC;}
+</style>
 <h1 style="display: none;"><?= t("Photo Detail") ?></h1>
 <div id="gExifData">
-    <table>
-      <tbody>
-        <? for ($i = 0; $i < count($details); $i++): ?>
-          <tr>
-             <td class="gEven">
-               <?= $details[$i]["caption"] ?>
-             </td>
-             <td class="gOdd">
-               <?= $details[$i]["value"] ?>
-             </td>
-             <? if (!empty($details[++$i])): ?>
-               <td class="gEven">
-                 <?= $details[$i]["caption"] ?>
-               </td>
-               <td class="gOdd">
-                 <?= $details[$i]["value"] ?>
-               </td>
-             <? else: ?>
-               <td class="gEven"></td><td class="gOdd"></td>
-             <? endif ?>
-             </td>
-          </tr>
-        <? endfor ?>
-      </tbody>
-    </table>
+  <table class="gMetadata" >
+    <tbody>
+      <? for ($i = 0; $i < count($details); $i++): ?>
+      <tr>
+         <td class="gEven">
+         <?= $details[$i]["caption"] ?>
+         </td>
+         <td class="gOdd">
+         <?= $details[$i]["value"] ?>
+         </td>
+         <? if (!empty($details[++$i])): ?>
+           <td class="gEven">
+           <?= $details[$i]["caption"] ?>
+           </td>
+           <td class="gOdd">
+           <?= $details[$i]["value"] ?>
+           </td>
+         <? else: ?>
+           <td class="gEven"></td><td class="gOdd"></td>
+         <? endif ?>
+       </tr>
+       <? endfor ?>
+    </tbody>
+  </table>
 </div>
