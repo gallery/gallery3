dcc42328b12839b43ca52058eeab1106e094e8e1
Bharat Mediratta <bharat@menalto.com>
2009-02-19 01:50:38 +0000

    Fix indentation.

--- core/helpers/photo.php
+++ core/helpers/photo.php
@@ -97,7 +97,8 @@ class photo_Core {
 
   static function get_add_form($parent) {
     $form = new Forge("albums/{$parent->id}", "", "post", array("id" => "gAddPhotoForm"));
-    $group = $form->group("add_photo")->label(t("Add Photo to %album_title", array("album_title" =>$parent->title)));
+    $group = $form->group("add_photo")->label(
+      t("Add Photo to %album_title", array("album_title" =>$parent->title)));
     $group->input("name")->label(t("Name"));
     $group->input("title")->label(t("Title"));
     $group->textarea("description")->label(t("Description"));
