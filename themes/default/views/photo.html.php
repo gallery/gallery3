1aa21cb2ab03766f7347ed2de371335961b7d5d6
Chad Kieffer <chad@2tbsp.com>
2009-02-22 08:20:08 +0000

    Need to have next/previous in place in a disabled state to maintain pager layout.

--- themes/default/views/photo.html.php
+++ themes/default/views/photo.html.php
@@ -7,12 +7,18 @@
       <? if ($previous_item): ?>
       <a href="<?= $previous_item->url() ?>" class="gButtonLink ui-icon-left ui-state-default ui-corner-all">
       <span class="ui-icon ui-icon-triangle-1-w"></span><?= t("previous") ?></a>
+      <? else: ?>
+      <a class="gButtonLink ui-icon-left ui-state-disabled ui-corner-all">
+      <span class="ui-icon ui-icon-triangle-1-w"></span><?= t("previous") ?></a>
       <? endif; ?>
     </li>
     <li class="gInfo"><?= t("%position of %total", array("position" => $position, "total" => $sibling_count)) ?></li>
     <li class="txtright">
       <? if ($next_item): ?>
       <a href="<?= $next_item->url() ?>" class="gButtonLink ui-icon-right ui-state-default ui-corner-all">
+      <span class="ui-icon ui-icon-triangle-1-e"></span><?= t("next") ?></a>
+      <? else: ?>
+      <a class="gButtonLink ui-icon-right ui-state-disabled ui-corner-all">
       <span class="ui-icon ui-icon-triangle-1-e"></span><?= t("next") ?></a>
       <? endif ?>
     </li>
