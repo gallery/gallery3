ca5bba09644631c7bd040c6cb0a1af678ae06d1d
Bharat Mediratta <bharat@menalto.com>
2009-02-23 00:25:51 +0000

    Replace the word "item" in display text with photo/album/movie as appropriate.  We shouldn't be saying "item" to the user.

--- core/views/quick_pane.html.php
+++ core/views/quick_pane.html.php
@@ -1,8 +1,15 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
+<? if ($item->type == "photo"): ?>
+<? $title = t("Edit this photo") ?>
+<? elseif ($item->type == "movie"): ?>
+<? $title = t("Edit this movie") ?>
+<? elseif ($item->type == "album"): ?>
+<? $title = t("Edit this album") ?>
+<? endif ?>
 <a class="edit gDialogLink" href="<?= url::site("quick/form_edit/$item->id") ?>"
-  title="<?= t("Edit this item's metadata") ?>">
+  title="<?= $title ?>">
   <span>
-    <?= t("Edit this item") ?>
+    <?= $title ?>
   </span>
 </a>
 
@@ -22,25 +29,46 @@
 </a>
 <? endif ?>
 
+<? if ($item->type == "photo"): ?>
+<? $title = t("Move this photo to another album") ?>
+<? elseif ($item->type == "movie"): ?>
+<? $title = t("Move this movie to another album") ?>
+<? elseif ($item->type == "album"): ?>
+<? $title = t("Move this album to another album") ?>
+<? endif ?>
 <a class="move gDialogLink" href="<?= url::site("move/browse/$item->id") ?>"
-  title="<?= t("Move this item to another album") ?>">
+  title="<?= $title ?>">
   <span>
-    <?= t("Move this item to another album") ?>
+    <?= $title ?>
   </span>
 </a>
 
 <? if (access::can("edit", $item->parent())): ?>
+<? if ($item->type == "photo"): ?>
+<? $title = t("Choose this photo as the album cover") ?>
+<? elseif ($item->type == "movie"): ?>
+<? $title = t("Choose this movie as the album cover") ?>
+<? elseif ($item->type == "album"): ?>
+<? $title = t("Choose this album as the album cover") ?>
+<? endif ?>
 <a class="cover" href="<?= url::site("quick/make_album_cover/$item->id?csrf=" . access::csrf_token()) ?>"
-   title="<?= t("Select as album cover") ?>">
+   title="<?= $title ?>">
   <span>
-    <?= t("Select as album cover") ?>
+    <?= $title ?>
   </span>
 </a>
 
+<? if ($item->type == "photo"): ?>
+<? $title = t("Delete this photo") ?>
+<? elseif ($item->type == "movie"): ?>
+<? $title = t("Delete this movie") ?>
+<? elseif ($item->type == "album"): ?>
+<? $title = t("Delete this album") ?>
+<? endif ?>
 <a class="delete" href="<?= url::site("quick/delete/$item->id?csrf=" . access::csrf_token()) ?>"
-   title="<?= t("Delete this item") ?>">
+   title="<?= $title ?>">
   <span>
-    <?= t("Delete this item") ?>
+    <?= $title ?>
   </span>
 </a>
 <? endif ?>
