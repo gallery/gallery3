47756db8569400d5d23dbb1c30dd40f00c7beaf1
Bharat Mediratta <bharat@menalto.com>
2009-03-11 02:39:52 +0000

    Get rid of stray 'type' argument to task::get_definitions()

--- core/controllers/admin_maintenance.php
+++ core/controllers/admin_maintenance.php
@@ -40,7 +40,7 @@ class Admin_Maintenance_Controller extends Admin_Controller {
 
     $view = new Admin_View("admin.html");
     $view->content = new View("admin_maintenance.html");
-    $view->content->task_definitions = task::get_definitions("admin");
+    $view->content->task_definitions = task::get_definitions();
     $view->content->running_tasks = ORM::factory("task")
       ->where("done", 0)->orderby("updated", "DESC")->find_all();
     $view->content->finished_tasks = ORM::factory("task")
