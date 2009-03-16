6aadb0bc77702bdfd37356d398fe2d9444835b3b
Bharat Mediratta <bharat@menalto.com>
2009-03-16 09:01:50 +0000

    Fix indentation

--- modules/user/views/admin_users_group.html.php
+++ modules/user/views/admin_users_group.html.php
@@ -2,12 +2,12 @@
 <strong><?= $group->name ?></strong>
 <? if (!$group->special): ?>
 <a href="<?= url::site("admin/users/delete_group_form/$group->id") ?>"
-    title="<?= t("Delete " . $group->name) ?>" 
-    class="gDialogLink gButtonLink ui-state-default ui-corner-all">
+  title="<?= t("Delete " . $group->name) ?>"
+  class="gDialogLink gButtonLink ui-state-default ui-corner-all">
   <span class="ui-icon ui-icon-trash"><?= t("delete") ?></span></a>
 <? else: ?>
 <a title="<?= t("This group cannot be deleted") ?>"
-    class="gDialogLink gButtonLink ui-state-disabled ui-corner-all ui-icon-left">
+   class="gDialogLink gButtonLink ui-state-disabled ui-corner-all ui-icon-left">
   <span class="ui-icon ui-icon-trash"><?= t("delete") ?></span></a>
 <? endif ?>
 <ul>
@@ -15,8 +15,8 @@
   <li class="gUser">
     <?= $user->name ?>
     <? if (!$group->special): ?>
-    <a href="javascript:remove_user(<?= $user->id ?>, <?= $group->id ?>)" 
-        class="gButtonLink ui-state-default ui-corner-all ui-icon-left">
+    <a href="javascript:remove_user(<?= $user->id ?>, <?= $group->id ?>)"
+       class="gButtonLink ui-state-default ui-corner-all ui-icon-left">
       <span class="ui-icon ui-icon-closethick">Remove <?= $user->name ?> from <?= $group->name ?></span></a>
     <? endif ?>
   </li>
