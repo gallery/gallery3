6fae077b52cc44dee15b2befd1862ea65da50730
Andy Staudacher <andy.st@gmail.com>
2009-04-17 02:04:25 +0000

    Fix i18n for "Logged in as" string

--- modules/user/views/login.html.php
+++ modules/user/views/login.html.php
@@ -5,9 +5,13 @@
       title="<?= t("Login to Gallery") ?>"
       id="gLoginLink"><?= t("Login") ?></a></li>
   <? else: ?>
-  <li class="first"><?= t("Logged in as ") ?><a href="<?= url::site("form/edit/users/{$user->id}") ?>"
-      title="<?= t("Edit Your Profile") ?>"
-      id="gUserProfileLink" class="gDialogLink"><?= $user->full_name ?></a></li>
+     <li class="first"><?=
+        t('Logged in as %name',
+          array('name' =>
+                '<a href="' . url::site("form/edit/users/{$user->id}") .
+                '" title="' . t("Edit Your Profile") .
+                '" id="gUserProfileLink" class="gDialogLink">' .
+                $user->full_name . '</a>')) ?></li>
   <li><a href="<?= url::site("logout?continue=" . url::current(true)) ?>"
       id="gLogoutLink"><?= t("Logout") ?></a></li>
   <? endif; ?>
