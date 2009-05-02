4b790f5b76dd1258cdd571a59a45681bfc1f9dab
Bharat Mediratta <bharat@menalto.com>
2009-05-02 20:49:09 +0000

    Revert r20668; if we call our helper 'gallery' then it interferes with g2_import since it uses the Gallery2 embed interface which loads up G2's Gallery class as well.  Grumble.

--- core/helpers/core.php
+++ core/helpers/core.php
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
-class gallery_Core {
+class core_Core {
   static function maintenance_mode() {
     $maintenance_mode = Kohana::config("core.maintenance_mode", false, false);
 
--- core/hooks/init_gallery.php
+++ core/hooks/init_gallery.php
@@ -27,7 +27,7 @@ Event::add("system.post_routing", array("theme", "load_themes"));
 Event::add("system.ready", array("module", "load_modules"));
 Event::add("system.post_routing", array("url", "parse_url"));
 Event::add("system.shutdown", array("module", "shutdown"));
-Event::add("system.post_routing", array("gallery", "maintenance_mode"));
+Event::add("system.post_routing", array("core", "maintenance_mode"));
 
 // Override the cookie if we have a session id in the URL.
 // @todo This should probably be an event callback
