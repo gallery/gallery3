3b52b31c5451bfb4edd92d3f4b0093e51ec2b027
Bharat Mediratta <bharat@menalto.com>
2009-05-12 23:46:03 +0000

    Use the module_change event to figure out if the rss module is inactive.  If it is, we need to alert the admin user.

--- modules/slideshow/helpers/slideshow_event.php
+++ modules/slideshow/helpers/slideshow_event.php
@@ -17,16 +17,16 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
-class slideshow_installer {
-  static function install() {
-    $version = module::get_version("slideshow");
-    if ($version == 0) {
-      module::set_version("slideshow", 1);
+class slideshow_event_Core {
+  static function module_change($changes) {
+    if (!module::is_installed("rss") || in_array("rss", $changes->uninstall)) {
+      site_status::warning(
+        t("The Slideshow module requires the RSS module.  " .
+          "<a href=\"%url\">Install the RSS module now</a>",
+          array("url" => url::site("admin/modules"))),
+        "slideshow_needs_rss");
+    } else {
+      site_status::clear("slideshow_needs_rss");
     }
-  }
-
-  static function uninstall() {
-    module::delete("slideshow");
-    site_status::clear("slideshow_needs_rss");
   }
 }
