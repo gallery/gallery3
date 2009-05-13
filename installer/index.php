5495037a3d999adb7af48d1f65683f42a3104a89
Bharat Mediratta <bharat@menalto.com>
2009-05-13 20:04:58 +0000

    Gee it's May already.  Update copyright to 2009.

--- installer/index.php
+++ installer/index.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Gallery - a web based photo album viewer and editor
- * Copyright (C) 2000-2008 Bharat Mediratta
+ * Copyright (C) 2000-2009 Bharat Mediratta
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
