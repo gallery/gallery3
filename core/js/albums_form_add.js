3fdaf7b60e7f7b8ec7635827b74ef25681d739d4
Bharat Mediratta <bharat@menalto.com>
2009-05-14 22:27:08 +0000

    Convert spaces into underscores when generating the album name from the title

--- core/js/albums_form_add.js
+++ core/js/albums_form_add.js
@@ -1,10 +1,10 @@
 $("#gAddAlbumForm input[name=title]").change(
   function() {
     $("#gAddAlbumForm input[name=name]").attr(
-      "value", $("#gAddAlbumForm input[name=title]").attr("value"));
+      "value", $("#gAddAlbumForm input[name=title]").attr("value").replace(/\s+/g, "_"));
   });
 $("#gAddAlbumForm input[name=title]").keyup(
   function() {
     $("#gAddAlbumForm input[name=name]").attr(
-      "value", $("#gAddAlbumForm input[name=title]").attr("value"));
+      "value", $("#gAddAlbumForm input[name=title]").attr("value").replace(/\s+/g, "_"));
   });
