245c840405520ceb943466c70f997fccf268a7ed
Bharat Mediratta <bharat@menalto.com>
2009-05-16 22:31:02 +0000

    Silently drop trailing periods in the album name so that we don't have to throw up an error message (since we don't accept them in album names).

--- core/js/albums_form_add.js
+++ core/js/albums_form_add.js
@@ -1,10 +1,12 @@
 $("#gAddAlbumForm input[name=title]").change(
   function() {
     $("#gAddAlbumForm input[name=name]").attr(
-      "value", $("#gAddAlbumForm input[name=title]").attr("value").replace(/\s+/g, "_"));
+      "value", $("#gAddAlbumForm input[name=title]").attr("value").
+        replace(/\s+/g, "_").replace(/\.+$/, ""));
   });
 $("#gAddAlbumForm input[name=title]").keyup(
   function() {
     $("#gAddAlbumForm input[name=name]").attr(
-      "value", $("#gAddAlbumForm input[name=title]").attr("value").replace(/\s+/g, "_"));
+      "value", $("#gAddAlbumForm input[name=title]").attr("value").
+        replace(/\s+/g, "_").replace(/\.+$/, ""));
   });
