a1ad048ef24337f12540297d095352be3f6d4884
Bharat Mediratta <bharat@menalto.com>
2009-05-18 04:00:20 +0000

    Update preamble to avoid extra opening php block

--- modules/exif/lib/exif.php
+++ modules/exif/lib/exif.php
@@ -1,5 +1,4 @@
-<?php defined("SYSPATH") or die("No direct script access."); ?>
-<?php
+<?php defined("SYSPATH") or die("No direct script access.");
 /*
 	Exifer 1.6
 	Extracts EXIF information from digital photos.
