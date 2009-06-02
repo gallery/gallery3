0f987880e67c667dc1e2c47455fc9ee0859fcba7
Tim Almdal <tnalmdal@shaw.ca>
2009-06-02 12:08:47 -0700

    Fix for ticket #320

--- modules/gallery/views/move_tree.html.php
+++ modules/gallery/views/move_tree.html.php
@@ -1,5 +1,5 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
-<?= $parent->thumb_tag(array(), 25); ?>
+<?= $parent->thumb_img(array(), 25); ?>
 <? if (!access::can("edit", $parent) || $source->is_descendant($parent)): ?>
 <a href="javascript:load_tree('<?= $parent->id ?>',1)"> <?= p::clean($parent->title) ?> <?= t("(locked)") ?> </a>
 <? else: ?>
@@ -8,7 +8,7 @@
 <ul id="tree_<?= $parent->id ?>">
   <? foreach ($children as $child): ?>
   <li id="node_<?= $child->id ?>" class="node">
-    <?= $child->thumb_tag(array(), 25); ?>
+    <?= $child->thumb_img(array(), 25); ?>
     <? if (!access::can("edit", $child) || $source->is_descendant($child)): ?>
     <a href="javascript:load_tree('<?= $child->id ?>',1)"> <?= p::clean($child->title) ?> <?= t("(locked)") ?></a>
     <? else: ?>
