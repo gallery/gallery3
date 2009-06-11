cf687fe15248b4d14f5fefc6eddd91f5dcafe3c2
Bharat Mediratta <bharat@menalto.com>
2009-06-12 03:50:50 +0800

    Rename theme_details to theme_options.  These changes got left out of 68fd196d66e2d21f571ff3b5a673f18cd129abf9 leaving us in a temporarily broken state.
    Signed-off-by: Marc Poulhiès <dkm@kataplop.net>

--- modules/gallery/views/admin_theme_options.html.php
+++ modules/gallery/views/admin_theme_options.html.php
@@ -1,5 +1,5 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
-<div id="gAdminThemeDetails">
+<div id="gAdminThemeOptions">
   <h1> <?= t("Theme Options") ?> </h1>
 
   <?= $form ?>
