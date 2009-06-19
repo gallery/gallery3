cdb4052b5245315cf6619dfeb3488fd17ebc54e0
Bharat Mediratta <bharat@menalto.com>
2009-06-19 09:58:36 -0700

    Add an id to the exif link anchor.

--- modules/exif/views/exif_sidebar.html.php
+++ modules/exif/views/exif_sidebar.html.php
@@ -1,5 +1,5 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
-<a href="<?= url::site("exif/show/{$item->id}") ?>" title="<?= t("Photo Details") ?>"
+<a id="gExifDataLink" href="<?= url::site("exif/show/{$item->id}") ?>" title="<?= t("Photo Details") ?>"
   class="gDialogLink gButtonLink ui-icon-left ui-state-default ui-corner-all">
   <span class="ui-icon ui-icon-info"></span>
   <?= t("View more information") ?>
