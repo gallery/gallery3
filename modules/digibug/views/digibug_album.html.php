3824f2b52fed7bbbd3e1c91eafff9df21be6c765
Tim Almdal <tnalmdal@shaw.ca>
2009-06-23 05:26:44 -0700

    Extract the html for the Print button on the album page into a seperate view

---
+++ modules/digibug/views/digibug_album.html.php
@@ -0,0 +1,8 @@
+<?php defined("SYSPATH") or die("No direct script access.") ?>
+<div class="gDigibugPrintButton">
+  <a class="gButtonLink ui-corner-all ui-state-default ui-icon-left"
+     href="<?= url::site("digibug/print_photo/$id?csrf={$csrf}&return=$return") ?>"
+     title="<?= $title ?>">
+    <span class="ui-icon ui-icon-print"><?= $title ?></span>
+  </a>
+</div>
