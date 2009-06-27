bbba0a67c22436aa166b1a882f35ddc093faae7c
Bharat Mediratta <bharat@menalto.com>
2009-06-27 14:29:24 -0700

    Fix formatting.

--- modules/digibug/views/digibug_form.html.php
+++ modules/digibug/views/digibug_form.html.php
@@ -1,16 +1,16 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
 <html>
-<head>
-  <?= html::script("lib/jquery.js") ?>
-  <script type="text/javascript">
-    $("body form").ready(function() {
-      $("body form").submit();
-    });
-  </script>
-</head>
-<body>
-  <?= form::open("http://www.digibug.com/dapi/order.php") ?>
-  <?= form::hidden($order_parms) ?>
-  <?= form::close() ?>
-</body>
+  <head>
+    <?= html::script("lib/jquery.js") ?>
+    <script type="text/javascript">
+      $("body form").ready(function() {
+        $("body form").submit();
+      });
+    </script>
+  </head>
+  <body>
+    <?= form::open("http://www.digibug.com/dapi/order.php") ?>
+    <?= form::hidden($order_parms) ?>
+    <?= form::close() ?>
+  </body>
 </html>
