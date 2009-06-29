006b63030a364677143799c7ce41eabb10c86eee
Tim Almdal <tnalmdal@shaw.ca>
2009-06-29 15:38:55 -0700

    Combine the Combined::javascript and Combined::css into a single  method implemented by the magic method __call. The first parameter is the content type for text/xxxx and the 2nd parameter is the key of the combined file.

--- modules/gallery/controllers/combined.php
+++ modules/gallery/controllers/combined.php
@@ -18,7 +18,11 @@
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
 class Combined_Controller extends Controller {
-  public function javascript($key) {
+  public function __call($type, $key) {
+    if (empty($key)) {
+      Kohana::show_404();
+    }
+    $key = $key[0];
     if (preg_match('/[^0-9a-f]/', $key)) {
       // The key can't contain non-hex, so just terminate early
       Kohana::show_404();
@@ -51,7 +55,7 @@ class Combined_Controller extends Controller {
       header("Cache-Control: public");
     }
 
-    header("Content-Type: text/javascript; charset=UTF-8");
+    header("Content-Type: text/$type; charset=UTF-8");
     header("Expires: Tue, 19 Jan 2038 00:00:00 GMT");
     header("Last-Modified: " . gmdate("D, d M Y H:i:s T", time()));
 
@@ -59,45 +63,5 @@ class Combined_Controller extends Controller {
     print $content;
   }
 
-  public function css($key) {
-    if (preg_match('/[^0-9a-f]/', $key)) {
-      // The key can't contain non-hex, so just terminate early
-      Kohana::show_404();
-    }
-
-    // We don't need to save the session for this request
-    Session::abort_save();
-
-    // Our data is immutable, so if they already have a copy then it needs no updating.
-    if (!empty($_SERVER["HTTP_IF_MODIFIED_SINCE"])) {
-      header('HTTP/1.0 304 Not Modified');
-      return;
-    }
-
-    $cache = Cache::instance();
-    if (strpos($_SERVER["HTTP_ACCEPT_ENCODING"], "gzip") !== false ) {
-      $content = $cache->get("{$key}_gz");
-    }
-
-    if (empty($content)) {
-      $content = $cache->get($key);
-    }
-
-    if (empty($content)) {
-      Kohana::show_404();
-    }
-
-    if (strpos($_SERVER["HTTP_ACCEPT_ENCODING"], "gzip") !== false) {
-      header("Content-Encoding: gzip");
-      header("Cache-Control: public");
-    }
-
-    header("Content-Type: text/css; charset=UTF-8");
-    header("Expires: Tue, 19 Jan 2038 00:00:00 GMT");
-    header("Last-Modified: " . gmdate("D, d M Y H:i:s T", time()));
-
-    Kohana::close_buffers(false);
-    print $content;
-  }
 }
 
