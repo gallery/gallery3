d4e976cc53b32122c2047d9b697a9d5155e66937
Bharat Mediratta <bharat@menalto.com>
2009-07-02 05:06:17 -0700

    Remove empty <script> block.

--- modules/server_add/views/server_add_tree.html.php
+++ modules/server_add/views/server_add_tree.html.php
@@ -1,6 +1,4 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
-<script type="text/javascript">
-</script>
 <ul id="<?= $tree_id ?>" class="gCheckboxTree">
   <? foreach ($data as $file => $file_info): ?>
   <li class="<?= empty($file_info["is_dir"]) ? "gFile" : "gDirectory gCollapsed ui-icon-left" ?>">
