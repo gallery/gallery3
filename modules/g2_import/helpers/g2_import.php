1475b772bd256238e7e5e8c0985daede9e18a16f
Tim Almdal <tnalmdal@shaw.ca>
2009-07-10 08:18:58 +0800

    Change the tag importer to convert spaces to a dot in multi word tags.
    Signed-off-by: Tim Almdal <tnalmdal@shaw.ca>

--- modules/g2_import/helpers/g2_import.php
+++ modules/g2_import/helpers/g2_import.php
@@ -623,8 +623,10 @@ class g2_import_Core {
     }
 
     $tags = "";
+    // Multiword tags have the space changed to dots.s
     foreach ($tag_names as $tag_name) {
-      $tags .= (strlen($tags) ? ", " : "") . tag::add($g3_item, $tag_name);
+      $tags .= (strlen($tags) ? ", " : "") .
+        tag::add($g3_item, preg_replace('/\s\s+/', '.', $tag_name));
     }
 
     // Tag operations are idempotent so we don't need to map them.  Which is good because we don't
