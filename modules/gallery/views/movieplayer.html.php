653badc2e1b9753f29bbb1d9fec8098716a4a2f4
hiwilson <wilsonpscheung@gmail.com>
2009-07-21 09:44:51 +0800

    set wmode='transparent' other than flowplayer.js (cherry picked from commit 79a46658196a98a3972983cc422d1ee544b09d4a)

--- modules/gallery/views/movieplayer.html.php
+++ modules/gallery/views/movieplayer.html.php
@@ -1,7 +1,8 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
 <?= html::anchor($item->file_url(true), "", $attrs) ?>
 <script>
-  flowplayer("<?= $attrs["id"] ?>", "<?= url::abs_file("lib/flowplayer.swf") ?>", {
+  flowplayer("<?= $attrs["id"] ?>", {src: "<?= url::abs_file("lib/flowplayer.swf") ?>",
+      wmode: "transparent"}, {
     plugins: {
       h264streaming: {
         url: "<?= url::abs_file("lib/flowplayer.h264streaming.swf") ?>"
