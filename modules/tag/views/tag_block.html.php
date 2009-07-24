fa1d32e6466d8c6ffe77d163e2da9c71688a3c61
Tim Almdal <tnalmdal@shaw.ca>
2009-07-24 11:24:43 -0700

    Partial implementation of ticket #80. Provide auto complete and suggestions on the tag add form in the tag sidebar block.  Updated the xss golden file as well.  Still to do figure out how toget it into the edit popup dialog

--- modules/tag/views/tag_block.html.php
+++ modules/tag/views/tag_block.html.php
@@ -1,4 +1,14 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
+<script>
+  $("#gAddTagForm").ready(function() {
+    var url = $("#gTagCloud").attr("title") + "/autocomplete";
+    $("#gAddTagForm input:text").autocomplete(
+      url, {
+        max: 30,
+        formatResult: formatTagAutoCompleteResult}
+    );
+  });
+</script>
 <div id="gTagCloud" title="<?= url::site("tags") ?>">
   <?= $cloud ?>
 </div>
