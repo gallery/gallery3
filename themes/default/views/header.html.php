a7af9bb456fa840d0be2f83145a642e202be3325
Chad Kieffer <ckieffer@gmail.com>
2009-07-26 16:02:53 -0600

    Missed committing the gBanner addition to the view template.

--- themes/default/views/header.html.php
+++ themes/default/views/header.html.php
@@ -1,18 +1,18 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
-<?= $theme->header_top() ?>
-<? if ($header_text = module::get_var("gallery", "header_text")): ?>
-<?= $header_text ?>
-<? else: ?>
-<a id="gLogo" href="<?= url::site("albums/1") ?>" title="<?= t("go back to the Gallery home") ?>">
-  <img width="107" height="48" alt="<?= t("Gallery logo: Your photos on your web site") ?>" src="<?= $theme->url("images/logo.png") ?>" />
-</a>
-<? endif ?>
-
-<div id="gSiteMenu" style="display: none">
-<?= $theme->site_menu() ?>
+<div id="gBanner">
+  <?= $theme->header_top() ?>
+  <? if ($header_text = module::get_var("gallery", "header_text")): ?>
+  <?= $header_text ?>
+  <? else: ?>
+  <a id="gLogo" href="<?= url::site("albums/1") ?>" title="<?= t("go back to the Gallery home") ?>">
+    <img width="107" height="48" alt="<?= t("Gallery logo: Your photos on your web site") ?>" src="<?= $theme->url("images/logo.png") ?>" />
+  </a>
+  <? endif ?>
+  <div id="gSiteMenu">
+  <?= $theme->site_menu() ?>
+  </div>
+  <?= $theme->header_bottom() ?>
 </div>
-
-<?= $theme->header_bottom() ?>
 
 <? if (!empty($parents)): ?>
 <ul class="gBreadcrumbs">
