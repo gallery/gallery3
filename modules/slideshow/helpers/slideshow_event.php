980ee6b4b263481251c92da6f7382bc7235fb11c
Bharat Mediratta <bharat@menalto.com>
2009-08-27 14:54:10 -0700

    Revert "delete the simple slideshow"
    This reverts commit 940edac81c854227d1a25f482cb38c96b6937bb2.
    
    Revert "Add window resizing support. Just can't seem to get loading icon to show up"
    
    This reverts commit afd85945d7028cefd703c3f6bbebdf92f8e0477a.
    
    Revert "Resolve issues with show the wrong album when slideshow is invoked from a photo page.  In addition, implement the slideshow based on tags."
    
    This reverts commit 152caf5195e3bbcdb353707fed5683ed07927f47.
    
    Revert "Initial commit of a basic slideshow"
    
    This reverts commit 5943761df282bfdfe68453de5c1682030006cd11.

---
+++ modules/slideshow/helpers/slideshow_event.php
@@ -0,0 +1,62 @@
+<?php defined("SYSPATH") or die("No direct script access.");
+/**
+ * Gallery - a web based photo album viewer and editor
+ * Copyright (C) 2000-2009 Bharat Mediratta
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+class slideshow_event_Core {
+  static function module_change($changes) {
+    if (!module::is_active("rss") || in_array("rss", $changes->deactivate)) {
+      site_status::warning(
+        t("The Slideshow module requires the RSS module.  " .
+          "<a href=\"%url\">Activate the RSS module now</a>",
+          array("url" => url::site("admin/modules"))),
+        "slideshow_needs_rss");
+    } else {
+      site_status::clear("slideshow_needs_rss");
+    }
+  }
+
+  static function album_menu($menu, $theme) {
+    $menu
+      ->append(Menu::factory("link")
+               ->id("slideshow")
+               ->label(t("View slideshow"))
+               ->url("javascript:PicLensLite.start(" .
+                     "{maxScale:0,feedUrl:PicLensLite.indexFeeds()[0].url})")
+               ->css_id("gSlideshowLink"));
+  }
+
+  static function photo_menu($menu, $theme) {
+    $menu
+      ->append(Menu::factory("link")
+               ->id("slideshow")
+               ->label(t("View slideshow"))
+               ->url("javascript:PicLensLite.start(" .
+                     "{maxScale:0,feedUrl:PicLensLite.indexFeeds()[0].url})")
+               ->css_id("gSlideshowLink"));
+  }
+
+  static function tag_menu($menu, $theme) {
+    $menu
+      ->append(Menu::factory("link")
+               ->id("slideshow")
+               ->label(t("View slideshow"))
+               ->url("javascript:PicLensLite.start(" .
+                     "{maxScale:0,feedUrl:PicLensLite.indexFeeds()[0].url})")
+               ->css_id("gSlideshowLink"));
+  }
+}
