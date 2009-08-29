d85a8b20bbe0a5be0a03da70354169d41f418d41
Bharat Mediratta <bharat@menalto.com>
2009-08-29 11:48:49 -0700

    Rename $comment_model to $comments.

--- modules/comment/helpers/comment_rss.php
+++ modules/comment/helpers/comment_rss.php
@@ -33,16 +33,16 @@ class comment_rss_Core {
       return;
     }
 
-    $comment_model = ORM::factory("comment")
+    $comments = ORM::factory("comment")
       ->viewable()
       ->where("state", "published")
       ->orderby("created", "DESC");
 
     if ($feed_id == "item") {
-      $comment_model->where("item_id", $id);
+      $comments->where("item_id", $id);
     }
 
-    $comments = $comment_model->find_all($limit, $offset);
+    $comments = $comments->find_all($limit, $offset);
     $feed->view = "comment.mrss";
     $feed->children = array();
     foreach ($comments as $comment) {
@@ -59,7 +59,7 @@ class comment_rss_Core {
         ArrayObject::ARRAY_AS_PROPS);
     }
 
-    $feed->max_pages = ceil($comment_model->count_all() / $limit);
+    $feed->max_pages = ceil($comments->count_all() / $limit);
     $feed->title = htmlspecialchars(t("Recent Comments"));
     $feed->uri = url::abs_site("albums/" . (empty($id) ? "1" : $id));
     $feed->description = t("Recent Comments");
