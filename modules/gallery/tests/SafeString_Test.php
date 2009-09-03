1405e8ed9e2003d1b06853bae4ce3413de1910ce
Bharat Mediratta <bharat@menalto.com>
2009-09-03 11:29:57 -0700

    Fix tests for new purifier API.

--- modules/gallery/tests/SafeString_Test.php
+++ modules/gallery/tests/SafeString_Test.php
@@ -91,16 +91,18 @@ class SafeString_Test extends Unit_Test_Case {
 
   public function purify_test() {
     $safe_string = SafeString::purify("hello <p  >world</p>");
-    $expected =
-      module::is_active("htmlpurifier") ? "hello <p>world</p>" : "hello &lt;p  &gt;world&lt;/p&gt;";
+    $expected = method_exists("purifier", "purify")
+      ? "hello <p>world</p>"
+      : "hello &lt;p  &gt;world&lt;/p&gt;";
     $this->assert_equal($expected, $safe_string);
   }
 
   public function purify_twice_test() {
     $safe_string = SafeString::purify("hello <p  >world</p>");
     $safe_string_2 = SafeString::purify($safe_string);
-    $expected =
-      module::is_active("htmlpurifier") ? "hello <p>world</p>" : "hello &lt;p  &gt;world&lt;/p&gt;";
+    $expected = method_exists("purifier", "purify")
+      ? "hello <p>world</p>"
+      : "hello &lt;p  &gt;world&lt;/p&gt;";
     $this->assert_equal($expected, $safe_string_2);
   }
 
