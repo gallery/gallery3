047196b23c5fb12b3c517892d8016679a0ea8799
Andy Staudacher <andy.st@gmail.com>
2009-09-05 17:39:49 -0700

    Add missing view for language selection

--- modules/user/views/user_languages_block.html.php
+++ modules/user/views/user_languages_block.html.php
@@ -1,2 +1,19 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
-<?= form::dropdown("", $installed_locales, $selected) ?>
+<?= form::dropdown("gSelectSessionLocale", $installed_locales, $selected) ?>
+<script type="text/javascript">
+  $("#gSelectSessionLocale").click(function() {
+    var old_locale_preference = <?= html::js_string($selected) ?>;
+    var locale = $(this).val();
+    if (old_locale_preference == locale) {
+      return;
+    }
+
+    var expires = -1;
+    if (locale) {
+      expires = 365;
+    }
+    $.cookie("g_locale", locale, {"expires": expires, "path": "/"});
+    window.location.reload(true);
+  });
+</script>
+
