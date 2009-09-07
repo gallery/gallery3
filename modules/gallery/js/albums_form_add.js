6bc2235b277c23c7c56ec5244406e283ca641d60
Bharat Mediratta <bharat@menalto.com>
2009-09-07 16:29:51 -0700

    Populate the slug field from the title as well.

--- modules/gallery/js/albums_form_add.js
+++ modules/gallery/js/albums_form_add.js
@@ -2,11 +2,17 @@ $("#gAddAlbumForm input[name=title]").change(
   function() {
     $("#gAddAlbumForm input[name=name]").attr(
       "value", $("#gAddAlbumForm input[name=title]").attr("value").
-        replace(/\s+/g, "_").replace(/\.+$/, ""));
+        replace(/[\s\/]+/g, "-").replace(/\.+$/, ""));
+    $("#gAddAlbumForm input[name=slug]").attr(
+      "value", $("#gAddAlbumForm input[name=title]").attr("value").
+        replace(/[^A-Za-z0-9-_]+/g, "-"));
   });
 $("#gAddAlbumForm input[name=title]").keyup(
   function() {
     $("#gAddAlbumForm input[name=name]").attr(
       "value", $("#gAddAlbumForm input[name=title]").attr("value").
-        replace(/\s+/g, "_").replace(/\.+$/, ""));
+        replace(/[\s\/]+/g, "-").replace(/\.+$/, ""));
+    $("#gAddAlbumForm input[name=slug]").attr(
+      "value", $("#gAddAlbumForm input[name=title]").attr("value").
+        replace(/[^A-Za-z0-9-_]+/g, "-"));
   });
