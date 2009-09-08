7889ae1085963eaf40afbb72c044da7630a5af63
Bharat Mediratta <bharat@menalto.com>
2009-09-07 21:33:00 -0700

    Fix a bug where we were not properly decoding the path in the fallback code in get_item_from_uri() by using relative_url() instead of relative_path().

--- modules/gallery/helpers/MY_url.php
+++ modules/gallery/helpers/MY_url.php
@@ -63,7 +63,8 @@ class url extends url_Core {
   static function get_item_from_uri($uri) {
     $current_uri = html_entity_decode($uri, ENT_QUOTES);
     // In most cases, we'll have an exact match in the relative_url_cache item field.
-    // but failing that, walk down the tree until we find it.
+    // but failing that, walk down the tree until we find it.  The fallback code will fix caches
+    // as it goes, so it'll never be run frequently.
     $item = ORM::factory("item")->where("relative_url_cache", $current_uri)->find();
     if (!$item->loaded) {
       $count = count(Router::$segments);
@@ -71,7 +72,7 @@ class url extends url_Core {
                ->where("slug", html_entity_decode(Router::$segments[$count - 1], ENT_QUOTES))
                ->where("level", $count + 1)
                ->find_all() as $match) {
-        if ($match->relative_path() == $current_uri) {
+        if ($match->relative_url() == $current_uri) {
           $item = $match;
         }
       }
