f69dee8841d2285a7068d349053bd429f4bb9b40
Bharat Mediratta <bharat@menalto.com>
2009-09-08 19:58:27 -0700

    Trim leading and trailing hyphens from the slug.

--- modules/gallery/js/albums_form_add.js
+++ modules/gallery/js/albums_form_add.js
@@ -1,18 +1,23 @@
 $("#gAddAlbumForm input[name=title]").change(
   function() {
     $("#gAddAlbumForm input[name=name]").attr(
-      "value", $("#gAddAlbumForm input[name=title]").attr("value").
-        replace(/[\s\/]+/g, "-").replace(/\.+$/, ""));
+      "value", $("#gAddAlbumForm input[name=title]").attr("value")
+        .replace(/[\s\/]+/g, "-").replace(/\.+$/, ""));
     $("#gAddAlbumForm input[name=slug]").attr(
-      "value", $("#gAddAlbumForm input[name=title]").attr("value").
-        replace(/[^A-Za-z0-9-_]+/g, "-"));
+      "value", $("#gAddAlbumForm input[name=title]").attr("value")
+        .replace(/[^A-Za-z0-9-_]+/g, "-")
+	.replace(/^-+/, "")
+	.replace(/-+$/, ""));
   });
 $("#gAddAlbumForm input[name=title]").keyup(
   function() {
     $("#gAddAlbumForm input[name=name]").attr(
-      "value", $("#gAddAlbumForm input[name=title]").attr("value").
-        replace(/[\s\/]+/g, "-").replace(/\.+$/, ""));
+      "value", $("#gAddAlbumForm input[name=title]").attr("value")
+        .replace(/[\s\/]+/g, "-")
+	.replace(/\.+$/, ""));
     $("#gAddAlbumForm input[name=slug]").attr(
-      "value", $("#gAddAlbumForm input[name=title]").attr("value").
-        replace(/[^A-Za-z0-9-_]+/g, "-"));
+      "value", $("#gAddAlbumForm input[name=title]").attr("value")
+        .replace(/[^A-Za-z0-9-_]+/g, "-")
+	.replace(/^-+/, "")
+	.replace(/-+$/, ""));
   });
