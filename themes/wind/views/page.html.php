ef35f0900170c9c375d5c1c87fc79c613ee96c51
Chad Kieffer <ckieffer@gmail.com>
2009-09-26 14:28:43 -0600

    Force IE 8 into IE 8 compatiblility mode

--- themes/wind/views/page.html.php
+++ themes/wind/views/page.html.php
@@ -4,6 +4,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=8" />
     <title>
       <? if ($page_title): ?>
         <?= $page_title ?>
