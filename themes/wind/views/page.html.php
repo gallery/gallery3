484dd7abaa03009c66edad50ce60dd872a80ae92
Chad Kieffer <ckieffer@gmail.com>
2009-09-26 15:50:16 -0600

    Remove IE 8 compatibility mode, it's not really needed after the latest context menu update

--- themes/wind/views/page.html.php
+++ themes/wind/views/page.html.php
@@ -4,7 +4,6 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=8" />
     <title>
       <? if ($page_title): ?>
         <?= $page_title ?>
