49505f28bb68e84b926d7907a33c7f814e78eb3c
Chad Kieffer <ckieffer@gmail.com>
2009-10-05 22:27:30 -0600

    Refer to sidebar elements as blocks, rather than panels.

--- themes/wind/views/no_sidebar.html.php
+++ themes/wind/views/no_sidebar.html.php
@@ -1,5 +1,5 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
 <div class="g-warning">
-  <?= t("No active sidebar panels. <a href=\"%url\">Add panels</a>",
+  <?= t("No active sidebar blocks. <a href=\"%url\">Add blocks</a>",
           array("url" => html::mark_clean(url::site("admin/sidebar")))) ?>
 </div>
