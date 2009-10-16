ed00914baaa04af37051a02c7a1ff38ca7ce6c5c
Chad Kieffer <ckieffer@gmail.com>
2009-10-15 23:38:39 -0600

    Restored g-buttonset class to the view menu, for now.

--- themes/wind/views/sidebar.html.php
+++ themes/wind/views/sidebar.html.php
@@ -1,6 +1,6 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
 <?= $theme->sidebar_top() ?>
-<div id="g-view-menu" class="g-clearfix">
+<div id="g-view-menu" class="g-buttonset g-clearfix">
   <? if ($page_type == "album"):?>
     <?= $theme->album_menu() ?>
   <? elseif ($page_type == "photo") : ?>
