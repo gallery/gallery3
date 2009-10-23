b3e175006cc3be2c1793797e726095520ba16d9c
Tim Almdal <tnalmdal@shaw.ca>
2009-10-23 07:48:07 -0700

    Move the filters that the File_Structure_Test use into a seperate file so other tests. can use them.  Add a No_Direct_ORM_Access_Test to find all places that the user or group table are accessed outside of the user module.  This test will fail at this point, as it in preparation for the IdentityProvider refactor.

--- modules/gallery/tests/File_Structure_Test.php
+++ modules/gallery/tests/File_Structure_Test.php
@@ -17,6 +17,8 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
+require_once(MODPATH . "gallery/tests/Gallery_Filters.php");
+
 class File_Structure_Test extends Unit_Test_Case {
   public function no_trailing_closing_php_tag_test() {
     $dir = new GalleryCodeFilterIterator(
@@ -259,35 +261,5 @@ class File_Structure_Test extends Unit_Test_Case {
     if ($errors) {
       $this->assert_true(false, $errors);
     }
-  }
-}
-
-class PhpCodeFilterIterator extends FilterIterator {
-  public function accept() {
-    $path_name = $this->getInnerIterator()->getPathName();
-    return substr($path_name, -4) == ".php";
-  }
-}
-
-class GalleryCodeFilterIterator extends FilterIterator {
-  public function accept() {
-    // Skip anything that we didn"t write
-    $path_name = $this->getInnerIterator()->getPathName();
-    return !(
-      strpos($path_name, ".svn") ||
-      strpos($path_name, DOCROOT . "test") !== false ||
-      strpos($path_name, DOCROOT . "var") !== false ||
-      strpos($path_name, MODPATH . "forge") !== false ||
-      strpos($path_name, MODPATH . "gallery/views/kohana_error_page.php") !== false ||
-      strpos($path_name, MODPATH . "gallery/views/kohana_profiler.php") !== false ||
-      strpos($path_name, MODPATH . "gallery_unit_test/views/kohana_error_page.php") !== false ||
-      strpos($path_name, MODPATH . "gallery_unit_test/views/kohana_unit_test_cli.php") !== false ||
-      strpos($path_name, MODPATH . "unit_test") !== false ||
-      strpos($path_name, MODPATH . "exif/lib") !== false ||
-      strpos($path_name, MODPATH . "user/lib/PasswordHash") !== false ||
-      strpos($path_name, DOCROOT . "lib/swfupload") !== false ||
-      strpos($path_name, SYSPATH) !== false ||
-      strpos($path_name, MODPATH . "gallery/libraries/HTMLPurifier") !== false ||
-      substr($path_name, -1, 1) == "~");
   }
 }
---
+++ modules/user/tests/No_Direct_ORM_Access_Test.php
@@ -0,0 +1,77 @@
+<?php defined("SYSPATH") or die("No direct script access.");
+/**
+ * Gallery - a web based photo album viewer and editor
+ * Copyright (C) 2000-2009 Bharat Mediratta
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+require_once(MODPATH . "gallery/tests/Gallery_Filters.php");
+
+class No_Direct_ORM_Access_Test extends Unit_Test_Case {
+  public function no_access_to_users_table_test() {
+    $dir = new UserModuleFilterIterator(
+      new PhpCodeFilterIterator(
+        new GalleryCodeFilterIterator(
+          new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator(DOCROOT)))));
+    $errors = array();
+    foreach ($dir as $file) {
+      //if (basename(dirname($file)) == "helpers") {
+      $file_as_string = file_get_contents($file);
+      if (preg_match("/ORM::factory\\(\"user\"/", $file_as_string)) {
+        foreach (split("\n", $file_as_string) as $l => $line) {
+          if (preg_match('/ORM::factory\\(\"user\"/', $line)) {
+            $errors[] = "$file($l) => $line";
+          }
+        }
+      }
+      $file_as_string = null;
+    }
+    if ($errors) {
+      $this->assert_false(true, "Direct access to the users table found:\n" . join("\n", $errors));
+    }
+  }
+
+  public function no_access_to_groups_table_test() {
+    $dir = new UserModuleFilterIterator(
+      new PhpCodeFilterIterator(
+        new GalleryCodeFilterIterator(
+          new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator(DOCROOT)))));
+    $errors = array();
+    foreach ($dir as $file) {
+      $file_as_string = file_get_contents($file);
+      if (preg_match("/ORM::factory\\(\"group\"/", $file_as_string)) {
+        foreach (split("\n", $file_as_string) as $l => $line) {
+          if (preg_match('/ORM::factory\\(\"group\"/', $line)) {
+            $errors[] = "$file($l) => $line";
+          }
+        }
+      }
+      $file_as_string = null;
+    }
+    if ($errors) {
+      $this->assert_false(true, "Direct access to the groups table found:\n" . join("\n", $errors));
+    }
+  }
+
+}
+
+class UserModuleFilterIterator extends FilterIterator {
+  public function accept() {
+    $path_name = $this->getInnerIterator()->getPathName();
+    return strpos($path_name, "/modules/user") === false;
+  }
+}
