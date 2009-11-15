967b17204b4e038fa2181473e80fcba143d30970
Bharat Mediratta <bharat@menalto.com>
2009-11-14 20:28:20 -0800

    Moved night_wind to contrib

--- themes/night_wind/views/no_sidebar.html.php
+++
@@ -1,6 +0,0 @@
-<?php defined("SYSPATH") or die("No direct script access.") ?>
-<ul class="g-message-block">
-  <li class="g-warning"><?= t("No active sidebar blocks.<br/>
-      <a href=\"%url\">Add blocks</a>",
-          array("url" => html::mark_clean(url::site("admin/sidebar")))) ?></li>
-</ul>
