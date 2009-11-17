401c9c21e1d5b0d88853e895491ee38f9fce38c7
Chad Kieffer <ckieffer@gmail.com>
2009-11-17 15:16:40 -0700

    White-space fix and quoting update.

--- modules/gallery/views/in_place_edit.html.php
+++ modules/gallery/views/in_place_edit.html.php
@@ -1,8 +1,8 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
 <?= form::open($action, array("method" => "post", "id" => "g-in-place-edit-form", "class" => "g-short-form"), $hidden) ?>
   <ul>
-    <li <? if (!empty($errors["input"])): ?> class="g-error"<? endif ?>>
-      <?= form::input("input", $form["input"], " class='textbox'") ?>
+    <li<? if (!empty($errors["input"])): ?> class="g-error"<? endif ?>>
+      <?= form::input("input", $form["input"], " class=\"textbox\"") ?>
     </li>
     <li>
       <?= form::submit(array("class" => "submit ui-state-default"), t("Save")) ?>
