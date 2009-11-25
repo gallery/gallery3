e201536d82d6ed49b7c4832f367a01029de05dd6
Bharat Mediratta <bharat@menalto.com>
2009-11-24 19:25:14 -0800

    K24 has separate log sinks, this configures the file based log sink

---
+++ modules/gallery/config/log_file.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Message logging directory.
+ */
+$config['log_directory'] = VARPATH.'logs';
+
+/**
+ * Permissions of the log file
+ */
+$config['posix_permissions'] = 0644;
