6b8a52d3287a8ead8be26070b354e672e9e1c0fd
Bharat Mediratta <bharat@menalto.com>
2010-01-04 21:37:51 -0800

    Fix the logout link to send you back to the current url.  The old approach depended on having an $item, which is not the case on all pages (eg: tag pages).  Also, check the CSRF in the logout controller, else you can use the logout link as a blind forwarder.

--- modules/gallery/controllers/logout.php
+++ modules/gallery/controllers/logout.php
@@ -19,6 +19,7 @@
  */
 class Logout_Controller extends Controller {
   public function index() {
+    access::verify_csrf();
     auth::logout();
     if ($continue_url = Input::instance()->get("continue")) {
       $item = url::get_item_from_uri($continue_url);
