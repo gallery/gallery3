2b283f4d011a7a67f306e11e590b499242a0774b
Tim Almdal <tnalmdal@shaw.ca>
2010-01-11 13:26:10 -0800

    Remove the translation submit button from translation search form.  Fixes ticket #975

--- modules/gallery/controllers/l10n_client.php
+++ modules/gallery/controllers/l10n_client.php
@@ -102,10 +102,9 @@ class L10n_Client_Controller extends Controller {
   }
 
   private static function _l10n_client_search_form() {
-    $form = new Forge("l10n_client/search", "", "post", array("id" => "g-l10n-search-form"));
+    $form = new Forge("#", "", "post", array("id" => "g-l10n-search-form"));
     $group = $form->group("l10n_search");
     $group->input("l10n-search")->id("g-l10n-search");
-    $group->submit("l10n-search-filter-clear")->value(t("X"));
 
     return $form;
   }
