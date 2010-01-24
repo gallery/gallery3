7c06e21ec443a46bd78bc9e99d8284283ff85c59
Tim Almdal <tnalmdal@shaw.ca>
2010-01-24 15:27:33 -0800

    Refactor creating the user profile page content into the the event module.  The show_user_profile is used to provide content to the user profile page.  Add the list of the users comments to the profile page.

---
+++ modules/comment/views/user_profile_comments.html.php
@@ -0,0 +1,20 @@
+<?php defined("SYSPATH") or die("No direct script access.") ?>
+<div id="g-comment-detail">
+<ul>
+  <? foreach ($comments as $comment): ?>
+  <li id="g-comment-<?= $comment->id ?>">
+    <p class="g-author">
+      <?= t('on %date for %title ',
+            array("date" => date("Y-M-d H:i:s", $comment->created),
+                  "title" => $comment->item()->title)); ?>
+      <a href="<?= $comment->item()->url() ?>">
+        <?= $comment->item()->thumb_img(array(), 50) ?>
+      </a>
+    </p>
+    <div>
+      <?= nl2br(html::purify($comment->text)) ?>
+    </div>
+  </li>
+  <? endforeach ?>
+</ul>
+</div>
