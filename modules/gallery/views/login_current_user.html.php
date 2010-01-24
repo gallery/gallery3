c39437a1929a3383e66f6bc8d6fc1a367de23f27
Tim Almdal <tnalmdal@shaw.ca>
2010-01-23 23:52:29 -0800

    Change the hover text to just display 'Your profile'.

--- modules/gallery/views/login_current_user.html.php
+++ modules/gallery/views/login_current_user.html.php
@@ -1,8 +1,7 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
 <li>
   <? $name = $menu->label->for_html() ?>
-  <? $hover_text = Identity::is_writable() ? t("Edit your profile")->for_html_attr() :
-                                             t("Display your profile")->for_html_attr() ?>
+  <? $hover_text = t("Your profile")->for_html_attr() ?>
   <?= t("Logged in as %name", array("name" => html::mark_clean(
         "<a href='$menu->url' title='$hover_text' id='$menu->id'>{$name}</a>"))) ?>
 </li>
