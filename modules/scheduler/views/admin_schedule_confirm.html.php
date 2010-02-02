d5a231225d3b4f1fe6a08f350b93b6410b2d1914
Tim Almdal <tnalmdal@shaw.ca>
2010-02-02 14:37:26 -0800

    Move the scheduler module to the -contrib repository for this release.

--- modules/scheduler/views/admin_schedule_confirm.html.php
+++
@@ -1,4 +0,0 @@
-<?php defined("SYSPATH") or die("No direct script access.") ?>
-<h1 style="display:none"><?= t("Confirm task removal") ?></h1>
-<p class="g-warning"><?= t("Really remove the task: %name", array("name" => $name)) ?></p>
-<?= $form ?>
