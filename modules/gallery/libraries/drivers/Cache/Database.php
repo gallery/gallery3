6591ea25771f2ab31fea4bf3b7a6fd76c586e098
Andy Staudacher <andy.st@gmail.com>
2010-02-21 23:48:23 -0800

    Fix delete() function of DB based Cache driver. It expected a scalar key / tag value, but it was always an array of keys / tags. (compare to system/libraries/Cache.php and the File.php driver)

--- modules/gallery/libraries/drivers/Cache/Database.php
+++ modules/gallery/libraries/drivers/Cache/Database.php
@@ -153,15 +153,17 @@ class Cache_Database_Driver extends Cache_Driver {
    * @param  bool    delete a tag
    * @return bool
    */
-  public function delete($id, $tag=false) {
+  public function delete($keys, $is_tag=false) {
     $db = db::build()
       ->delete("caches");
-    if ($id === true) {
+    if ($keys === true) {
       // Delete all caches
-    } else if ($tag === true) {
-      $db->where("tags", "LIKE", "%<$id>%");
+    } else if ($is_tag === true) {
+      foreach ($keys as $tag) {
+        $db->where("tags", "LIKE", "%<$tag>%");
+      }
     } else {
-      $db->where("key", "=", $id);
+      $db->where("key", "IN", $keys);
     }
 
     $status = $db->execute();
