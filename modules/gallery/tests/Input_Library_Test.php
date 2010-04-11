c9edb943f99704a649612afa6ae7825b2e6bc44e
Bharat Mediratta <bharat@menalto.com>
2010-04-10 17:01:48 -0700

    Fix a bug introduced when we patched the "disallowed global data" security check.  Not sure why the patch worked for some and not for others, but this should resolve it either way.  Fixes ticket #1123.

--- modules/gallery/tests/Input_Library_Test.php
+++ modules/gallery/tests/Input_Library_Test.php
@@ -17,15 +17,9 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
-class Input extends Input_Core {
-  /**
-   * Modified form of Input::clean_input_keys() that replaces malformed values
-   * instead of dying on bad input.
-   *
-   * @param   string  string to clean
-   * @return  string
-   */
-  public function clean_input_keys($str) {
-    return preg_replace('#[^a-zA-Z0-9:_.-]+#', '_', $str);
+class Input_Library_Test extends Gallery_Unit_Test_Case {
+  function clean_input_keys_test() {
+    $input = Input::instance();
+    $this->assert_same("foo_bar", $input->clean_input_keys("foo|bar"));
   }
 }
