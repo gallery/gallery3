66bea24482be05ccfbea76bda2c9b70c93c34408
Bharat Mediratta <bharat@menalto.com>
2010-06-20 11:59:46 -0700

    Use text::limit_chars() to keep individual elements in breadcrumbs from going over 15 chars.  Fixes ticket #1108.

--- themes/wind/views/page.html.php
+++ themes/wind/views/page.html.php
@@ -107,12 +107,14 @@
                  level you're on the right page. -->
             <a href="<?= $parent->url($parent == $theme->item()->parent() ?
                      "show={$theme->item()->id}" : null) ?>">
-              <?= html::purify($parent->title) ?>
+              <?= text::limit_chars(html::purify($parent->title), 15) ?>
             </a>
           </li>
           <? $i++ ?>
           <? endforeach ?>
-          <li class="g-active<? if ($i == 0) print " g-first" ?>"><?= html::purify($theme->item()->title) ?></li>
+          <li class="g-active<? if ($i == 0) print " g-first" ?>">
+            <?= text::limit_chars(html::purify($theme->item()->title), 15) ?>
+          </li>
         </ul>
         <? endif ?>
       </div>
