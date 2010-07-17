a8c629409bbc78791c3ae7f5f16dac0c90db214d
Chad Kieffer <ckieffer@gmail.com>
2010-07-17 15:49:47 -0600

    Fix margin: 0 auto; IE issue in the upgrader view by adding a doctype to the template. Minor code refactoring. Also seems to fix #1188.

--- modules/gallery/views/upgrader.html.php
+++ modules/gallery/views/upgrader.html.php
@@ -1,4 +1,6 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+          "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
   <head>
     <title><?= t("Gallery 3 upgrader") ?></title>
