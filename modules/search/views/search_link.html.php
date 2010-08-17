8c2ed0d681364837ab51b35d7aeb895b8adfa470
Chad Kieffer <ckieffer@gmail.com>
2010-08-16 22:39:54 -0600

    Added text and submit classes to search form input and submit buttons, respectively. Fixed the width of the search form in IE. Button height's off in IE 8 compatbility mode.

--- modules/search/views/search_link.html.php
+++ modules/search/views/search_link.html.php
@@ -3,10 +3,10 @@
   <ul>
     <li>
       <label for="g-search"><?= t("Search the gallery") ?></label>
-      <input type="text" name="q" id="g-search"/>
+      <input type="text" name="q" id="g-search" class="text" />
     </li>
     <li>
-      <input type="submit" value="<?= t("Go")->for_html_attr() ?>" />
+      <input type="submit" value="<?= t("Go")->for_html_attr() ?>" class="submit" />
     </li>
   </ul>
 </form>
