gallery/gallery3

commit b6fa33faf789749f4de3f4eadf8832748372c980

Create a setLocation javascript callback which is called everytime with the full url of the current album(non-REST). When the dialog is closed the window location is set to this value and the page gets loaded appropriately.

modules/organize/views/organize_dialog.html.php
@@ -19,11 +19,15 @@
 <script type="text/javascript">
   $("#g-dialog").bind("dialogclose", function(event, ui) {
     // @todo do a call to organize/closing to end the batch
-    window.location.reload();
+    window.location = $(this).data("reload.location");
   });
 
   function closeOrganizeDialog() {
     $("#g-dialog").dialog("close");
+  }
+
+  function setLocation(url) {
+    $("#g-dialog").data("reload.location", url);
   }
 
   function setTitle(title) {

commit cc4a54a3d65d5a6772cbf2d9670a0af0d6d52542

Fix bug introduced in b6fa33faf789749f4de3f4eadf8832748372c980.  Don't try to use $(this).data("reload.location") if it hasn't been set, which can happen if Flash isn't available.  Fixes #1362

modules/organize/views/organize_dialog.html.php
@@ -19,7 +19,11 @@
 <script type="text/javascript">
   $("#g-dialog").bind("dialogclose", function(event, ui) {
     // @todo do a call to organize/closing to end the batch
-    window.location = $(this).data("reload.location");
+    if ($(this).data("reload.location")) {
+      window.location = $(this).data("reload.location");
+    } else {
+      window.location.reload();
+    }
   });
 
   function closeOrganizeDialog() {