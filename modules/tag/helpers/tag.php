gallery/gallery3

commit 3d952f41c8d90b5c217616fb060697f93fb9db07

Fix a bug in the way that we add tags that causes it to be really slow when adding a tag to lots of items.  Tag_Model::save() would call item_related_update for every tag related to an item upon save which is an O(N!) operation.  Fixes ticket #1412.

modules/tag/helpers/tag.php
@@ -126,6 +126,7 @@ class tag_Core {
       ->delete("items_tags")
       ->where("item_id", "=", $item->id)
       ->execute();
+    module::event("item_related_update", $item);
   }
 
   /**

modules/tag/models/tag.php
@@ -57,8 +57,7 @@ class Tag_Model_Core extends ORM {
 
   /**
    * Overload ORM::save() to trigger an item_related_update event for all items that are related
-   * to this tag.  Since items can be added or removed as part of the save, we need to trigger an
-   * event for the union of all related items before and after the save.
+   * to this tag.
    */
   public function save() {
     $related_item_ids = array();
@@ -70,20 +69,16 @@ class Tag_Model_Core extends ORM {
       $related_item_ids[$row->item_id] = 1;
     }
 
-    $result = parent::save();
-
-    foreach (db::build()
-             ->select("item_id")
-             ->from("items_tags")
-             ->where("tag_id", "=", $this->id)
-             ->execute() as $row) {
-      $related_item_ids[$row->item_id] = 1;
+    if (isset($this->changed_relations["items"])) {
+      $changed = array_merge(
+        array_diff($this->changed_relations["items"], $this->object_relations["items"]),
+        array_diff($this->object_relations["items"], $this->changed_relations["items"]));
     }
 
-    if ($related_item_ids) {
-      foreach (ORM::factory("item")
-               ->where("id", "IN", array_keys($related_item_ids))
-               ->find_all() as $item) {
+    $result = parent::save();
+
+    if (!empty($changed)) {
+      foreach (ORM::factory("item")->where("id", "IN", $changed)->find_all() as $item) {
         module::event("item_related_update", $item);
       }
     }
@@ -93,7 +88,7 @@ class Tag_Model_Core extends ORM {
 
   /**
    * Overload ORM::delete() to trigger an item_related_update event for all items that are
-   * related to this tag.
+   * related to this tag, and delete all items_tags relationships.
    */
   public function delete($ignored_id=null) {
     $related_item_ids = array();
@@ -105,6 +100,7 @@ class Tag_Model_Core extends ORM {
       $related_item_ids[$row->item_id] = 1;
     }
 
+    db::build()->delete("items_tags")->where("tag_id", "=", $this->id)->execute();
     $result = parent::delete();
 
     if ($related_item_ids) {

modules/tag/controllers/admin_tags.php
@@ -52,7 +52,6 @@ class Admin_Tags_Controller extends Admin_Controller {
     $form = tag::get_delete_form($tag);
     if ($form->validate()) {
       $name = $tag->name;
-      db::build()->delete("items_tags")->where("tag_id", "=", $tag->id)->execute();
       $tag->delete();
       message::success(t("Deleted tag %tag_name", array("tag_name" => $name)));
       log::success("tags", t("Deleted tag %tag_name", array("tag_name" => $name)));

commit 7efa9a5b87eaa8fe86972376af8c58dc29c44392

Fix two issues with item deletion:
1) We're compacting tags on every deletion which is slow.  Since we delete
   albums in batch, we should just do one tag compaction at the end.  Fixes
   #1487.

2) Issue introduced in 3d952f41c8d90b5c217616fb060697f93fb9db07 where
   we trigger an item_related_update in tag::clear_all().  Since
   tag::clear_all() is called when we delete an item, this causes
   the search module to attempt to index a deleted item.  Move that
   triggering upstream.

modules/tag/helpers/tag.php
@@ -126,7 +126,6 @@ class tag_Core {
       ->delete("items_tags")
       ->where("item_id", "=", $item->id)
       ->execute();
-    module::event("item_related_update", $item);
   }
 
   /**

modules/tag/helpers/tag_event.php
@@ -62,6 +62,12 @@ class tag_event_Core {
 
   static function item_deleted($item) {
     tag::clear_all($item);
+    if (!batch::in_progress()) {
+      tag::compact();
+    }
+  }
+
+  static function batch_complete() {
     tag::compact();
   }
 
@@ -88,6 +94,7 @@ class tag_event_Core {
         tag::add($item, trim($tag_name));
       }
     }
+    module::event("item_related_update", $item);
     tag::compact();
   }
 
@@ -109,7 +116,7 @@ class tag_event_Core {
     if (!isset($group->uploadify)) {
       return;
     }
-    
+
     $group = $form->add_photos;
     $group->input("tags")
       ->label(t("Add tags to all uploaded files"))
@@ -132,7 +139,7 @@ class tag_event_Core {
     if (!isset($group->uploadify)) {
       return;
     }
-    
+
     foreach (explode(",", $form->add_photos->tags->value) as $tag_name) {
       $tag_name = trim($tag_name);
       if ($tag_name) {