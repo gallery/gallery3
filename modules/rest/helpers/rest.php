554ca2e683297489251d621e2c959a6a467a6f9c
Bharat Mediratta <bharat@menalto.com>
2010-11-22 22:01:11 -0800

    Take into account the core.url_suffix configuration setting when parsing REST urls.  This fixes the problem that setting the url suffix breaks REST.  Fixes #1500.

--- modules/rest/helpers/rest.php
+++ modules/rest/helpers/rest.php
@@ -122,7 +122,12 @@ class rest_Core {
    * @return mixed  the corresponding object (usually a model of some kind)
    */
   static function resolve($url) {
-    $relative_url = substr($url, strlen(url::abs_site("rest")));
+    if ($suffix = Kohana::config('core.url_suffix')) {
+      $relative_url = substr($url, strlen(url::abs_site("rest")) - strlen($suffix));
+    } else {
+      $relative_url = substr($url, strlen(url::abs_site("rest")));
+    }
+
     $path = parse_url($relative_url, PHP_URL_PATH);
     $components = explode("/", $path, 3);
 
