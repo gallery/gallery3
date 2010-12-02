b217ed77c70e97e04c69dc186ecedf1a4d2b5fe7
Bharat Mediratta <bharat@menalto.com>
2010-12-02 08:32:59 -0800

    Duh, set the image_block.image_count variable in the installer!  This issue was introduced in cdf0f7f765a5bd5e0d5619d345bdc6aba069c555 which was a follow-on for 3c523bcac6e902973c5891c02842254eaecd33a4 which fixes #1499.

--- modules/image_block/helpers/image_block_installer.php
+++ modules/image_block/helpers/image_block_installer.php
@@ -20,6 +20,7 @@
 class image_block_installer {
 
   static function install() {
+    module::set_var("image_block", "image_count", "1");
     module::set_version("image_block", $version = 2);
   }
 
