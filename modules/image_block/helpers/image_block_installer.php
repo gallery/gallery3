gallery/gallery3

commit cdf0f7f765a5bd5e0d5619d345bdc6aba069c555

In 3c523bcac6e902973c5891c02842254eaecd33a4 (fix for #1499) I left out the install() function which should set the image_block module to version 2.  Fix that here.

modules/image_block/helpers/image_block_installer.php
@@ -19,6 +19,10 @@
  */
 class image_block_installer {
 
+  static function install() {
+    module::set_version("image_block", $version = 2);
+  }
+
   static function upgrade($version) {
     $db = Database::instance();
     if ($version == 1) {

commit c3ef8921260db8e39b6d2a7b4708e3d19f35f8b5

Duh, set the image_block.image_count variable in the installer!  This issue was introduced in cdf0f7f765a5bd5e0d5619d345bdc6aba069c555 which was a follow-on for 3c523bcac6e902973c5891c02842254eaecd33a4 which fixes #1499.

modules/image_block/helpers/image_block_installer.php
@@ -20,6 +20,7 @@
 class image_block_installer {
 
   static function install() {
+    module::set_var("image_block", "image_count", "1");
     module::set_version("image_block", $version = 2);
   }
 