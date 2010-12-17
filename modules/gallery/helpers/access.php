43edb598e70ec7c91b612cebf99b7685e9f08318
Kriss Andsten <kriss@sverok.se>
2010-12-17 03:52:32 +0100

    Fetch permissions for non-albumbs by parent rather than by item, allowing the result to be cached.

--- modules/gallery/helpers/access.php
+++ modules/gallery/helpers/access.php
@@ -98,9 +98,17 @@ class access_Core {
     if ($user->admin) {
       return true;
     }
-
+    
+    /*
+      We do this for cache reasons - if you check n photos in an album, it makes more sense
+      to check the album permissions once and let the cache deal with that, rather than check
+      every item individually and generate cache misses.
+    */
+    $id = ($item->type == 'album') ? $item->id : $item->parent_id;
+    
     $resource = $perm_name == "view" ?
-      $item : model_cache::get("access_cache", $item->id, "item_id");
+      $item : model_cache::get("access_cache", $id, "item_id");
+      
     foreach ($user->groups() as $group) {
       if ($resource->__get("{$perm_name}_{$group->id}") === self::ALLOW) {
         return true;
@@ -136,8 +144,16 @@ class access_Core {
    * @return boolean
    */
   static function group_can($group, $perm_name, $item) {
+    /*
+      We do this for cache reasons - if you check n photos in an album, it makes more sense
+      to check the album permissions once and let the cache deal with that, rather than check
+      every item individually and generate cache misses.
+    */
+    $id = ($item->type == 'album') ? $item->id : $item->parent_id;
+    
     $resource = $perm_name == "view" ?
-      $item : model_cache::get("access_cache", $item->id, "item_id");
+      $item : model_cache::get("access_cache", $id, "item_id");
+    
     return $resource->__get("{$perm_name}_{$group->id}") === self::ALLOW;
   }
 
