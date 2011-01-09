92f66058d30a04681a01b886bdc652a70652cbfa
Bharat Mediratta <bharat@menalto.com>
2011-01-08 16:58:52 -0800

    Revert "Warn admins after login if their PHP install has the"
    This reverts commit 612ddd7050889974fc1f7e449e715b4c1129c0bb.

--- modules/gallery/helpers/gallery_event.php
+++ modules/gallery/helpers/gallery_event.php
@@ -178,10 +178,6 @@ class gallery_event_Core {
     }
     Session::instance()->set("active_auth_timestamp", time());
     auth::clear_failed_attempts($user);
-
-    if ($user->admin && ini_get("session.use_trans_sid")) {
-      message::info(t("PHP is configured with <a href=\"url\">session.use_trans_sid</a> enabled which will cause random logouts.  Please disable this setting.", array("url" => "http://www.php.net/manual/en/session.configuration.php#ini.session.use-trans-sid")));
-    }
   }
 
   static function user_auth_failed($name) {
