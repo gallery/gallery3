gallery/gallery3

commit 65ff2470a505fbc18211093ac131c29b1e820c3e

Use eeldivad's fallback code from http://gallery.menalto.com/node/96806#comment-358404 instead of relying solely on preg_quote.  Fixes #1460.

modules/server_add/controllers/server_add.php
@@ -167,8 +167,11 @@ class Server_Add_Controller extends Admin_Controller {
             $entry->delete();
           }
 
-          $path = preg_replace("/(\*|\?|\[)/", "[$1]", $entry->path);
-          foreach (glob("$path/*") as $child_path) {
+          $child_paths = glob(preg_quote($entry->path) . "/*");
+          if (!$child_paths) {
+            $child_paths = glob("$path/*");
+          }
+          foreach ($child_paths as $child_path) {
             if (!is_dir($child_path)) {
               $ext = strtolower(pathinfo($child_path, PATHINFO_EXTENSION));
               if (!in_array($ext, array("gif", "jpeg", "jpg", "png", "flv", "mp4", "m4v")) ||
@@ -189,8 +192,8 @@ class Server_Add_Controller extends Admin_Controller {
           // We've processed this entry, mark it as done.
           $entry->checked = 1;
           $entry->save();
+          $dirs_scanned++;
         }
-        $dirs_scanned++;
       }
 
       // We have no idea how long this can take because we have no idea how deep the tree

commit 0126a0385d5a763b4920bc9ffe63ed9ec03d64f0

Fix a bug introduced in 65ff2470a505fbc18211093ac131c29b1e820c3e, clearly I didn't test this enough.  Further fix for #1460.

modules/server_add/controllers/server_add.php
@@ -169,7 +169,7 @@ class Server_Add_Controller extends Admin_Controller {
 
           $child_paths = glob(preg_quote($entry->path) . "/*");
           if (!$child_paths) {
-            $child_paths = glob("$path/*");
+            $child_paths = glob("{$entry->path}/*");
           }
           foreach ($child_paths as $child_path) {
             if (!is_dir($child_path)) {