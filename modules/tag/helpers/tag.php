9dd91b55b03923ef3058fb965b0c927d101a396f
Tim Almdal <tnalmdal@shaw.ca>
2011-04-23 07:42:28 -0700

    Insure that the tag count to display is always greater than 1.  Refixes #1649.

--- modules/tag/helpers/tag.php
+++ modules/tag/helpers/tag.php
@@ -48,6 +48,7 @@ class tag_Core {
    * @return ORM_Iterator of Tag_Model in descending tag count order
    */
   static function popular_tags($count) {
+    $count = $count >= 1 ? $count : 30;
     return ORM::factory("tag")
       ->order_by("count", "DESC")
       ->limit($count)
