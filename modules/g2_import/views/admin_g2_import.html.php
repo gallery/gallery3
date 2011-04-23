bb23e28035293174f935345178f32638c09d0421
Chad Kieffer <ckieffer@gmail.com>
2011-04-23 10:28:39 -0400

    Remove enumeration unordered list class, it's no longer needed.

--- modules/g2_import/views/admin_g2_import.html.php
+++ modules/g2_import/views/admin_g2_import.html.php
@@ -36,7 +36,7 @@
     </div>
     <div id="g-admin-g2-import-import">
       <? if (isset($g2_version)): ?>
-      <ul class="enumeration">
+      <ul>
         <li>
           <?= t("Gallery version %version detected", array("version" => $g2_version)) ?>
         </li>
