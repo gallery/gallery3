80af9f0f17ce5ac10ceefc9d3d0736b3d4c1c2aa
Bharat Mediratta <bharat@menalto.com>
2011-04-23 07:05:15 -0700

    Inline admin JS into admin_server_add.html.php.  Fixes #1686.

--- modules/server_add/js/admin.js
+++
@@ -1,8 +0,0 @@
-/**
- * Set up autocomplete on the server path list
- *
- */
-$("document").ready(function() {
-  $("#g-path").autocomplete(
-    base_url.replace("__ARGS__", "admin/server_add/autocomplete"), {max: 256});
-});
