gallery/gallery3

commit f2477703faa7cd05ff1aa16da3ecef7b666bef40

Fix against ticket #1581 - based on https://github.com/Joe7/gallery3/commit/0d36b97aca3b5ed53ea3ea398ce28b0f6198e577

modules/g2_import/helpers/g2_import.php
@@ -499,6 +499,9 @@ class g2_import_Core {
   static function set_album_highlight(&$queue) {
     // Dequeue the current album and enqueue its children
     list($g2_album_id, $children) = each($queue);
+    if (empty($children)) {
+      return;
+    }
     unset($queue[$g2_album_id]);
     foreach ($children as $key => $value) {
       $queue[$key] = $value;

commit ea2127c3b2ddef5709e02b4804c074bd1f942159

Fix a bug introduced in f2477703faa7cd05ff1aa16da3ecef7b666bef40 which was a fix for #1581 that caused us to skip adding highlights for any albums that had no sub-albums.

modules/g2_import/helpers/g2_import.php
@@ -518,12 +518,11 @@ class g2_import_Core {
   static function set_album_highlight(&$queue) {
     // Dequeue the current album and enqueue its children
     list($g2_album_id, $children) = each($queue);
-    if (empty($children)) {
-      return;
-    }
     unset($queue[$g2_album_id]);
-    foreach ($children as $key => $value) {
-      $queue[$key] = $value;
+    if (!empty($children)) {
+      foreach ($children as $key => $value) {
+        $queue[$key] = $value;
+      }
     }
 
     $messages = array();