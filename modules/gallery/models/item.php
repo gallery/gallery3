gallery/gallery3

commit 526859d9605d137ebe053ecbd80f46ca6a331194

Do simple transliteration when converting filenames to slugs, but check to see if the transliteration module is available and use a more complex transliteration if possible.  Fixes #1668.

modules/gallery/models/item.php
@@ -336,9 +336,7 @@ class Item_Model_Core extends ORM_MPTT {
 
         // Make an url friendly slug from the name, if necessary
         if (empty($this->slug)) {
-          $tmp = pathinfo($this->name, PATHINFO_FILENAME);
-          $tmp = preg_replace("/[^A-Za-z0-9-_]+/", "-", $tmp);
-          $this->slug = trim($tmp, "-");
+          $this->slug = item::convert_filename_to_slug($this->name);
 
           // If the filename is all invalid characters, then the slug may be empty here.  Pick a
           // random value.

modules/gallery/helpers/item.php
@@ -152,8 +152,18 @@ class item_Core {
    * @param string $filename
    */
   static function convert_filename_to_slug($filename) {
-    $result = pathinfo($filename, PATHINFO_FILENAME);
+    $result = str_replace("&", "-and-", $filename);
+    $result = str_replace(" ", "-", $result);
+
+    // It's not easy to extend the text helper since it's called by the Input class which is
+    // referenced in hooks/init_gallery, so it's
+    if (class_exists("transliterate")) {
+      $result = transliterate::utf8_to_ascii($result);
+    } else {
+      $result = text::transliterate_to_ascii($result);
+    }
     $result = preg_replace("/[^A-Za-z0-9-_]+/", "-", $result);
+    $result = preg_replace("/-+/", "-", $result);
     return trim($result, "-");
   }
 

modules/gallery/tests/Item_Helper_Test.php
@@ -49,6 +49,10 @@ class Item_Helper_Test extends Gallery_Unit_Test_Case {
   public function convert_filename_to_slug_test() {
     $this->assert_equal("foo", item::convert_filename_to_slug("{[foo]}"));
     $this->assert_equal("foo-bar", item::convert_filename_to_slug("{[foo!@#!$@#^$@($!(@bar]}"));
+    $this->assert_equal("english-text", item::convert_filename_to_slug("english text"));
+    $this->assert_equal("new-line", item::convert_filename_to_slug("new \n line"));
+    $this->assert_equal("foo-and-bar", item::convert_filename_to_slug("foo&bar"));
+    $this->assert_equal("special", item::convert_filename_to_slug("šṗëçîąļ"));
   }
 
   public function move_test() {

commit 91c83874e518b9106a4d54d8c3bbe28e4c055123

Fix a bug introduced in 526859d9605d137ebe053ecbd80f46ca6a331194 where we changed the behavior of the slug code and included the file extension, not just the name.  This broke Item_Model_Test::move_photo_with_conflicting_target_gets_uniqified_test so yay for unit tests!  #1668

modules/gallery/models/item.php
@@ -336,7 +336,7 @@ class Item_Model_Core extends ORM_MPTT {
 
         // Make an url friendly slug from the name, if necessary
         if (empty($this->slug)) {
-          $this->slug = item::convert_filename_to_slug($this->name);
+          $this->slug = item::convert_filename_to_slug(pathinfo($this->name, PATHINFO_FILENAME));
 
           // If the filename is all invalid characters, then the slug may be empty here.  Pick a
           // random value.