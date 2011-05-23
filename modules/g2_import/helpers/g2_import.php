784ebe75321304fe3f83cddaf3cb1030410fb5ed
Bharat Mediratta <bharat@menalto.com>
2011-05-22 21:24:27 -0700

    Leave the "updated" field alone when importing comments so that if Akismet marks them as spam, we don't immediately flush them out of the database on the next visit to Admin > Content > Comments.
    Also warn the user about Akismet, and fix up the G2 import code to
    reimport deleted comments.

--- modules/g2_import/helpers/g2_import.php
+++ modules/g2_import/helpers/g2_import.php
@@ -908,9 +908,14 @@ class g2_import_Core {
                array("id" => $g2_comment_id, "exception" => (string)$e));
     }
 
-    if (self::map($g2_comment->getId())) {
-      // Already imported
-      return;
+    if ($id = self::map($g2_comment->getId())) {
+      if (ORM::factory("comment", $id)->loaded()) {
+        // Already imported and still exists
+        return;
+      }
+      // This comment was already imported, but now it no longer exists.  Import it again, per
+      // ticket #1736.
+      self::clear_map($g2_comment_id);
     }
 
     $item_id = self::map($g2_comment->getParentId());
@@ -948,10 +953,11 @@ class g2_import_Core {
     self::set_map($g2_comment->getId(), $comment->id, "comment");
 
     // Backdate the creation date.  We can't do this at creation time because
-    // Comment_Model::save() will override it.
+    // Comment_Model::save() will override it.  Leave the updated date alone
+    // so that if the comments get marked as spam, they don't immediately get
+    // flushed (see ticket #1736)
     db::update("comments")
       ->set("created", $g2_comment->getDate())
-      ->set("updated", $g2_comment->getDate())
       ->where("id", "=", $comment->id)
       ->execute();
   }
@@ -1304,6 +1310,16 @@ class g2_import_Core {
     $g2_map->g2_url = $g2_url;
     $g2_map->save();
     self::$map[$g2_id] = $g3_id;
+  }
+
+  /**
+   * Remove all map entries associated with the given Gallery 2 id.
+   */
+  static function clear_map($g2_id) {
+    db::build()
+      ->delete("g2_maps")
+      ->where("g2_id", "=", $g2_id)
+      ->execute();
   }
 
   static function log($msg) {
