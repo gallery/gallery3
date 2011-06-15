ce1e3e6a6444bbe7c3c3c73e1f49ad28eec5bd84
Bharat Mediratta <bharat@menalto.com>
2011-06-14 20:03:20 -0700

    Mark this branch as 3.0.3 and christen the release "Hotlanta"

--- modules/gallery/helpers/gallery.php
+++ modules/gallery/helpers/gallery.php
@@ -18,8 +18,8 @@
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
 class gallery_Core {
-  const VERSION = "3.0.2";
-  const CODE_NAME = "Coollanta";
+  const VERSION = "3.0.3";
+  const CODE_NAME = "Hotlanta";
   const RELEASE_CHANNEL = "git";
   const RELEASE_BRANCH = "3.0.x";
 
