gallery/gallery3

commit dc21cf36b606048dc24532407d39bc8f5b4211fa

Change the $thumb_proportions variable to be a theme callback which takes an item as an argument.  This will let us figure out proportions in the case where a module has defined custom thumbnail sizes.

themes/wind/views/page.html.php
@@ -23,7 +23,7 @@
           type="image/x-icon" />
 
     <? if ($theme->page_type == "collection"): ?>
-      <? if ($thumb_proportion != 1): ?>
+    <? if (($thumb_proportion = $theme->thumb_proportion($theme->item())) != 1): ?>
         <? $new_width = round($thumb_proportion * 213) ?>
         <? $new_height = round($thumb_proportion * 240) ?>
         <style type="text/css">

modules/gallery/libraries/Theme_View.php
@@ -42,9 +42,6 @@ class Theme_View_Core extends Gallery_View {
                             "page_type" => $page_type,
                             "page_subtype" => $page_subtype,
                             "page_title" => null));
-    if ($page_type == "collection") {
-      $this->set_global("thumb_proportion", $this->thumb_proportion());
-    }
 
     if (module::get_var("gallery", "maintenance_mode", 0)) {
       if (identity::active_user()->admin) {
@@ -58,10 +55,17 @@ class Theme_View_Core extends Gallery_View {
    * Proportion of the current thumb_size's to default
    * @return int
    */
-  public function thumb_proportion() {
-    // @TODO change the 200 to a theme supplied value when and if we come up with an
-    // API to allow the theme to set defaults.
-    return module::get_var("gallery", "thumb_size", 200) / 200;
+  public function thumb_proportion($item) {
+    // By default we have a globally fixed thumbnail size In core code, we just return a fixed
+    // proportion based on the global thumbnail size, but since modules can override that, we
+    // return the actual proportions when we have them.
+    if ($item->has_thumb()) {
+      return max($item->thumb_width, $item->thumb_height) / 200;
+    } else {
+      // @TODO change the 200 to a theme supplied value when and if we come up with an
+      // API to allow the theme to set defaults.
+      return module::get_var("gallery", "thumb_size", 200) / 200;
+    }
   }
 
   public function item() {

commit 4cb49b9152e82d0ab53b1038c7a7d7203c8bf97d

Fix a bug introduced in dc21cf36b606048dc24532407d39bc8f5b4211fa where we're basing the thumb proportions on the album's thumbnail, but this will be messed up in the case of the root album which may not have a properly update thumbnail, so its proportions may not be representative of the album's contents.  So instead, base the thumb proportions on the first child of the album.

themes/wind/views/page.html.php
@@ -24,7 +24,7 @@
     <link rel="apple-touch-icon-precomposed"
           href="<?= url::file(module::get_var("gallery", "apple_touch_icon_url")) ?>" />
     <? if ($theme->page_type == "collection"): ?>
-    <? if (($thumb_proportion = $theme->thumb_proportion($theme->item())) != 1): ?>
+    <? if (($thumb_proportion = $theme->thumb_proportion($children[0])) != 1): ?>
         <? $new_width = round($thumb_proportion * 213) ?>
         <? $new_height = round($thumb_proportion * 240) ?>
         <style type="text/css">