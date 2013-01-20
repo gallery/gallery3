3a71813f35857faf1f8a6a68cc8dd3e8d6982a7e
Bharat Mediratta <bharat@menalto.com>
2013-01-20 00:24:08 -0500

    Change the logic for finding controllers to use glob() but drop anything that we can find is git-ignore'd.

--- modules/gallery/tests/Controller_Auth_Test.php
+++ modules/gallery/tests/Controller_Auth_Test.php
@@ -20,8 +20,9 @@
 class Controller_Auth_Test extends Gallery_Unit_Test_Case {
   public function find_missing_auth_test() {
     $found = array();
-    $controllers = explode("\n", `git ls-files '*/*/controllers/*.php'`);
-    $feeds = explode("\n", `git ls-files '*/*/helpers/*_rss.php'`);
+    $git_ignores = explode("\n", `git ls-files -o -i --exclude-standard`);
+    $controllers = array_diff(glob("*/*/controllers/*.php"), $git_ignores);
+    $feeds = array_diff(glob("*/*/helpers/*_rss.php"), $git_ignores);
     foreach (array_merge($controllers, $feeds) as $controller) {
       if (preg_match("{modules/(gallery_)?unit_test/}", $controller)) {
         continue;
