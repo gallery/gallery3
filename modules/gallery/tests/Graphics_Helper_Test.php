cf077425953a6a492dea97eaf1517e7d08c9648f
shadlaws <shad@shadlaws.com>
2013-01-30 01:07:36 +0100

    #1968 - Improve album cover generation/removal/etc. - Added stanza to Item_Model::save that handles when cover id is null. - Added logic to graphics::generate to copy/convert album cover thumbs from their item thumbs to ensure they're always jpg, and eliminate the possibility that we copy/convert a dirty thumb. - Redirected other places in code where we want to do one of the above two things to use these two functions instead (gallery_event::item_updated_data_file, item::make_album_cover, item::remove_album_cover). - Improved validation in Item_Model so only albums can have covers and all covers must be non-albums. - Added unit tests to Graphics_Helper_Test.

--- modules/gallery/tests/Graphics_Helper_Test.php
+++ modules/gallery/tests/Graphics_Helper_Test.php
@@ -44,6 +44,39 @@ class Graphics_Helper_Test extends Gallery_Unit_Test_Case {
     $this->assert_equal(0, $movie->thumb_dirty);
   }
 
+  public function generate_album_cover_test() {
+    $album = test::random_album();
+    $photo = test::random_unique_photo($album);
+    $album->reload();
+    // Check that the image was copied directly from item thumb
+    $this->assert_equal(file_get_contents($photo->thumb_path()),
+                        file_get_contents($album->thumb_path()));
+    // Check that the items table got updated
+    $this->assert_equal(array(200, 150), array($album->thumb_width, $album->thumb_height));
+    // Check that the image is not marked dirty
+    $this->assert_equal(0, $album->thumb_dirty);
+  }
+
+  public function generate_album_cover_from_png_test() {
+    $input_file = MODPATH . "gallery/tests/test.jpg";
+    $output_file = TMPPATH . test::random_name() . ".png";
+    gallery_graphics::resize($input_file, $output_file, null, null);
+
+    $album = test::random_album();
+    $photo = test::random_photo_unsaved($album);
+    $photo->set_data_file($output_file);
+    $photo->name = "album_cover_from_png.png";
+    $photo->save();
+    $album->reload();
+    // Check that the image was correctly resized and converted to jpg
+    $this->assert_equal(array(200, 150, "image/jpeg", "jpg"),
+                        photo::get_file_metadata($album->thumb_path()));
+    // Check that the items table got updated
+    $this->assert_equal(array(200, 150), array($album->thumb_width, $album->thumb_height));
+    // Check that the image is not marked dirty
+    $this->assert_equal(0, $album->thumb_dirty);
+  }
+
   public function generate_bad_photo_test() {
     $photo = test::random_photo();
     // At this point, the photo is valid and has a valid resize and thumb.  Make it garble.
@@ -85,5 +118,30 @@ class Graphics_Helper_Test extends Gallery_Unit_Test_Case {
     $this->assert_equal(array(200, 200), array($movie->thumb_width, $movie->thumb_height));
     // Check that the image is *not* marked as dirty
     $this->assert_equal(0, $movie->thumb_dirty);
+  }
+
+  public function generate_album_cover_from_bad_photo_test() {
+    $album = test::random_album();
+    $photo = test::random_photo($album);
+    $album->reload();
+    // At this point, the photo is valid and has a valid resize and thumb.  Make it garble.
+    file_put_contents($photo->file_path(), test::lorem_ipsum(200));
+    // Regenerate album from garbled photo.
+    $photo->thumb_dirty = 1;
+    $photo->save();
+    $album->thumb_dirty = 1;
+    try {
+      graphics::generate($album);
+      $this->assert_true(false, "Shouldn't get here");
+    } catch (Exception $e) {
+      // Exception expected
+    }
+    // Check that the image got replaced with a missing image placeholder
+    $this->assert_same(file_get_contents(MODPATH . "gallery/images/missing_photo.jpg"),
+                       file_get_contents($album->thumb_path()));
+    // Check that the items table got updated with new metadata
+    $this->assert_equal(array(200, 200), array($album->thumb_width, $album->thumb_height));
+    // Check that the images are marked as dirty
+    $this->assert_equal(1, $album->thumb_dirty);
   }
 }
