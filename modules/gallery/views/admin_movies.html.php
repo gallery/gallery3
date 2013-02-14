f212f6a794c4aff96446b99e4824a9e2c8cfb259
shadlaws <shad@shadlaws.com>
2013-02-14 23:42:20 +0100

    #2003 - Add admin/movies screen. Added admin/movies screen analogous to the admin/graphics screen so the user can: - see how FFmpeg is configured (path and version, similar to toolkits in admin/graphics) - get some instructions on how to install FFmpeg if not found - change the movie_allow_uploads setting - ask Gallery to rebuild their movie thumbs Specifics: - admin_movies, admin_movies.html (new) - new Movies admin screen - ffmpeg.png (new) - logo for admin screen - movie::get_ffmpeg_version (new) - return version number and date of FFmpeg - form_uploadify.html - change admin message if movie uploads are disabled - gallery_event::admin_menu - added Movies link to Settings - xss_data.txt - updated golden file for unit tests

---
+++ modules/gallery/views/admin_movies.html.php
@@ -0,0 +1,44 @@
+<?php defined("SYSPATH") or die("No direct script access.") ?>
+<div id="g-movies-admin" class="g-block ui-helper-clearfix">
+  <h1> <?= t("Movies settings") ?> </h1>
+  <p>
+    <?= t("Gallery comes with everything it needs to upload and play movies.") ?>
+    <?= t("However, it needs the FFmpeg toolkit to extract thumbnails and size information from them.") ?>
+  </p>
+  <p>
+    <?= t("Although popular, FFmpeg is not installed on all Linux systems.") ?>
+    <?= t("To use FFmpeg without fully installing it, download a pre-compiled, <b>static build</b> of FFmpeg from one of the links <a href=\"%url\">here</a>.", array("url" => "http://ffmpeg.org/download.html")) ?>
+    <?= t("Then, put the \"ffmpeg\" file in Gallery's \"bin\" directory (e.g. \"/gallery/bin\"), where Gallery will auto-detect it.") ?>
+  </p>
+  <p>
+    <?= t("Movies will work without FFmpeg, but their thumbnails will be placeholders.") ?>
+  </p>
+  <p>
+    <?= t("Can't get FFmpeg configured on your system? <a href=\"%url\">We can help!</a>",
+          array("url" => "http://codex.galleryproject.org/Gallery3:FAQ#Why_does_it_say_I.27m_missing_ffmpeg.3F")) ?>
+  </p>
+
+  <div class="g-available">
+    <h2> <?= t("Current configuration") ?> </h2>
+    <div id="g-ffmpeg" class="g-block">
+      <img class="logo" width="284" height="70" src="<?= url::file("modules/gallery/images/ffmpeg.png"); ?>" alt="<? t("Visit the FFmpeg project site") ?>" />
+      <p>
+        <?= t("FFmpeg is a cross-platform standalone audio/video program.") ?><br/>
+        <?= t("Please refer to the <a href=\"%url\">FFmpeg website</a> for more information.", array("url" => "http://ffmpeg.org")) ?>
+      </p>
+      <div class="g-module-status g-info">
+        <? if ($ffmpeg_dir): ?>
+          <? if ($ffmpeg_version): ?>
+            <p><?= t("FFmpeg version %version was found in %dir", array("version" => $ffmpeg_version, "dir" => $ffmpeg_dir)) ?></p>
+          <? else: ?>
+            <p><?= t("FFmpeg (of unknown version) was found in %dir", array("dir" => $ffmpeg_dir)) ?></p>
+          <? endif ?>
+        <? else: ?>
+          <p><?= t("We could not locate FFmpeg on your system.") ?></p>
+        <? endif ?>
+      </div>
+    </div>
+  </div>
+
+  <?= $form ?>
+</div>
