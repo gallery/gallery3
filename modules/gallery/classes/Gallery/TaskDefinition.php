aabe8527c2774c940147494eb83c7d5be91a3324
shadlaws <shad@shadlaws.com>
2013-03-17 23:50:31 +0100

    Fix class declarations for just-moved libraries (except Menu - it needs extra work).

--- modules/gallery/classes/Gallery/TaskDefinition.php
+++ modules/gallery/classes/Gallery/TaskDefinition.php
@@ -17,8 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
-
-class Task_Definition_Core {
+class Gallery_TaskDefinition {
   public $callback;
   public $description;
   public $name;
