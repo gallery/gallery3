b8d79767fd79128ca8d074e729221744ba2a3fc5
shadlaws <shad@shadlaws.com>
2013-03-18 10:05:37 +0100

    Fix class declarations for View-related classes.

--- modules/gallery/classes/Gallery/View/Gallery.php
+++ modules/gallery/classes/Gallery/View/Gallery.php
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
-class Gallery_View extends Kohana_View {
+class Gallery_View_Gallery extends View {
   protected $theme_name = null;
   protected $combine_queue = array();
 
