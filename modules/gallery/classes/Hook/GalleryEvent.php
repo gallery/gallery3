b906bb47656d908f93807a5caf183b687aa0b2d3
shadlaws <shad@shadlaws.com>
2013-03-24 23:48:44 +0100

    Add class extensions for just-moved helpers.

---
+++ modules/gallery/classes/Hook/GalleryEvent.php
@@ -0,0 +1,3 @@
+<?php defined("SYSPATH") or die("No direct script access.");
+
+class Hook_GalleryEvent extends Gallery_Hook_GalleryEvent {}
---
+++ modules/gallery/classes/Hook/Rest/Item.php
@@ -0,0 +1,3 @@
+<?php defined("SYSPATH") or die("No direct script access.");
+
+class Hook_Rest_Item extends Gallery_Hook_Rest_Item {}
