266aeeb299edf9b87be2ca6657adb7fd01e69ebc
shadlaws <shad@shadlaws.com>
2013-03-26 12:05:49 +0100

    Fix Gallery_Database class definition and transparent extension file. - transparent extension file didn't have abstract - main file didn't have Kohana_

--- modules/gallery/classes/Database.php
+++ modules/gallery/classes/Database.php
@@ -1,3 +1,3 @@
 <?php defined("SYSPATH") or die("No direct script access.");
 
-class Database extends Gallery_Database {}
+abstract class Database extends Gallery_Database {}
--- modules/gallery/classes/Gallery/Database.php
+++ modules/gallery/classes/Gallery/Database.php
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
-abstract class Gallery_Database extends Database {
+abstract class Gallery_Database extends Kohana_Database {
   protected $_table_names;
 
   /**
