a4fdd8e31c030278ef7290403f73113ac90b71c2
Bharat Mediratta <bharat@menalto.com>
2013-03-26 21:00:07 -0400

    Rip out the hand-rolled global data support since it's back in K3. Simplify render().  Not sure if that works yet.

--- modules/gallery/classes/Gallery/View.php
+++ modules/gallery/classes/Gallery/View.php
@@ -18,40 +18,6 @@
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
 class Gallery_View extends Kohana_View {
-  static $global_data = array();
-
-  /**
-   * Reimplement Kohana 2.3's View::set_global() functionality.
-   */
-  public function set_global($key, $value = NULL) {
-    if (is_array($key)) {
-      foreach ($key as $key2 => $value) {
-        View::$global_data[$key2] = $value;
-      }
-    } else {
-      View::$global_data[$key] = $value;
-    }
-  }
-
-  public function is_set($key=null) {
-    return parent::is_set($key) ? true : array_key_exists($key, View::$global_data);
-  }
-
-  /**
-   * Completely replace Kohana_View::__get() so that local data trumps global data, trumps members.
-   * This simulates the Kohana 2.3 behavior.
-   */
-  public function &__get($key) {
-    if (isset($this->kohana_local_data[$key])) {
-      return $this->kohana_local_data[$key];
-    } else if (isset(View::$global_data[$key])) {
-      return View::$global_data[$key];
-    } else if (isset($this->$key)) {
-      return $this->$key;
-    } else {
-      throw new Kohana_Exception('Undefined view variable: :var', array(':var' => $key));
-    }
-  }
 
   /**
    * Override Kohana_View::__construct so that we can set the csrf value into all views.
@@ -64,22 +30,13 @@ class Gallery_View extends Kohana_View {
   }
 
   /**
-   * Override Kohana_View::render so that we trap errors stemming from bad PHP includes and show a
+   * Override View::render so that we trap errors stemming from bad PHP includes and show a
    * visible stack trace to help developers.
    *
-   * @see Kohana_View::render
+   * @see View::render
    */
-  public function render($print=false, $renderer=false, $modifier=false) {
-    try {
-      $this->kohana_local_data = array_merge(View::$global_data, $this->kohana_local_data);
-      return parent::render($print, $renderer, $modifier);
-    } catch (ORM_Validation_Exception $e) {
-      Log::add("error", $e->getMessage() . "\n" . $e->getTraceAsString());
-      Log::add("error", "Validation errors: " . print_r($e->validation->errors(), 1));
-      return "";
-    } catch (Exception $e) {
-      Log::add("error", $e->getMessage() . "\n" . $e->getTraceAsString());
-      return "";
-    }
+  public function render($file=null) {
+    $this->_data = array_merge(View::$_global_data, $this->_data);
+    return parent::render($file);
   }
 }
