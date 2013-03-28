ad517f0b42ebe44e589af99917096230404d4255
shadlaws <shad@shadlaws.com>
2013-03-28 18:36:51 +0100

    Add the organize module transparent-extension-enabling files.

---
+++ modules/organize/classes/Controller/Organize.php
@@ -0,0 +1,3 @@
+<?php defined("SYSPATH") or die("No direct script access.");
+
+class Controller_Organize extends Organize_Controller_Organize {}
---
+++ modules/organize/classes/Hook/OrganizeInstaller.php
@@ -0,0 +1,3 @@
+<?php defined("SYSPATH") or die("No direct script access.");
+
+class Hook_OrganizeInstaller extends Organize_Hook_OrganizeInstaller {}
