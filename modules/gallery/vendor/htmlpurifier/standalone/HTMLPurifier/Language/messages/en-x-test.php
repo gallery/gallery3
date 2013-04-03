5bcdaeb3560991696a072746558c28aa1498e0d0
shadlaws <shad@shadlaws.com>
2013-04-03 10:02:03 +0200

    Add HTMLPurifier Standalone v4.5.0 to modules/gallery/vendor.

---
+++ modules/gallery/vendor/htmlpurifier/standalone/HTMLPurifier/Language/messages/en-x-test.php
@@ -0,0 +1,11 @@
+<?php defined("SYSPATH") or die("No direct script access.");
+
+// private language message file for unit testing purposes
+
+$fallback = 'en';
+
+$messages = array(
+    'HTMLPurifier' => 'HTML Purifier X'
+);
+
+// vim: et sw=4 sts=4
