99899d13361248a02d32a2375ba57072e1239c16
Bharat Mediratta <bharat@menalto.com>
2013-04-22 15:58:30 -0400

    Implement IdentityProvider_GroupDefinition - we expect that in Access.

--- modules/user/classes/Group.php
+++ modules/user/classes/Group.php
@@ -1,3 +1,3 @@
 <?php defined("SYSPATH") or die("No direct script access.");
 
-class Group extends User_Group {}
+abstract class Group extends User_Group implements IdentityProvider_GroupDefinition {}
