d7866a62f7fab4adfbbaa61121b7ddcc8c4ced70
Bharat Mediratta <bharat@menalto.com>
2013-04-22 21:16:12 -0400

    It looks like this was not necessary.
    --
    
    Revert "Implement IdentityProvider_GroupDefinition - we expect that in Access."
    
    This reverts commit 99899d13361248a02d32a2375ba57072e1239c16.

--- modules/user/classes/Group.php
+++ modules/user/classes/Group.php
@@ -1,3 +1,3 @@
 <?php defined("SYSPATH") or die("No direct script access.");
 
-abstract class Group extends User_Group implements IdentityProvider_GroupDefinition {}
+class Group extends User_Group {}
