22fc8abcb8dd9f94289637e0fc2b8f516ac56dcb
Bharat Mediratta <bharat@menalto.com>
2013-04-26 16:06:00 -0400

    Rename the test class.

--- modules/akismet/tests/Akismet_Test.php
+++ modules/akismet/tests/Akismet_Test.php
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
-class Akismet_Helper_Test extends Unittest_Testcase {
+class Akismet_Test extends Unittest_Testcase {
   protected $_client_ip;
   protected $_user_agent;
   protected $_save;
