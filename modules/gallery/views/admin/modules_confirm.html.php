09b08470f5d961fe0f9b8733c912299024439d55
shadlaws <shad@shadlaws.com>
2013-05-01 09:24:54 +0200

    Fix formatting in admin/modules_confirm view.

--- modules/gallery/views/admin/modules_confirm.html.php
+++ modules/gallery/views/admin/modules_confirm.html.php
@@ -3,15 +3,14 @@
   <p>
     <?= t("The following issue(s) have been identified:") ?>
   </p>
-
   <div id="g-admin-modules-messages" class="g-block-content">
-   <ul>
-     <? foreach (array("error" => "g-error", "warn" => "g-warning") as $type => $css_class): ?>
-     <? foreach ($messages[$type] as $message): ?>
-     <li class="<?= $css_class ?>" style="padding-bottom: 0"><?= $message ?></li>
-     <? endforeach ?>
-     <? endforeach ?>
-   </ul>
+  <ul>
+    <? foreach (array("error" => "g-error", "warn" => "g-warning") as $type => $css_class): ?>
+    <? foreach ($messages[$type] as $message): ?>
+    <li class="<?= $css_class ?>" style="padding-bottom: 0"><?= $message ?></li>
+    <? endforeach ?>
+    <? endforeach ?>
+  </ul>
     <form method="post" action="<?= URL::site("admin/modules/save") ?>">
       <?= Access::csrf_form_field() ?>
       <? foreach ($modules as $module): ?>
