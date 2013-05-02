gallery/gallery3

commit db1b52f059faf6973a32d35a46da7bf05e00de7f

Clean up ORM calls: "->limit(1)->find_all()->current()" is just "->find()" @see ORM::_load_result() and ORM::find().

modules/gallery/classes/Gallery/Hook/GalleryEvent.php
@@ -166,7 +166,7 @@ class Gallery_Hook_GalleryEvent {
         Session::instance()->set("batch_missing_album_cover", $batch_missing_album_cover);
       } else {
         // Choose the first viewable child as the new cover.
-        if ($child = $parent->children->viewable()->limit(1)->find_all()->current()) {
+        if ($child = $parent->children->viewable()->find()) {
           Item::make_album_cover($child);
         }
       }
@@ -194,7 +194,7 @@ class Gallery_Hook_GalleryEvent {
     foreach (array_keys(Session::instance()->get("batch_missing_album_cover", array())) as $id) {
       $item = ORM::factory("Item", $id);
       if ($item->loaded() && !$item->album_cover_item_id) {
-        if ($child = $item->children->limit(1)->find_all()->current()) {
+        if ($child = $item->children->find()) {
           Item::make_album_cover($child);
         }
       }

modules/gallery/classes/Gallery/Access.php
@@ -184,7 +184,6 @@ class Gallery_Access {
       ->with("access_intent")
       ->where("access_intent.view_$group->id", "=", Access::DENY)
       ->order_by("level", "DESC")
-      ->limit(1)
       ->find();
 
     if ($lock->loaded()) {
@@ -529,7 +528,6 @@ class Gallery_Access {
         ->with("access_intent")
         ->where("access_intent.$field", "=", Access::DENY)
         ->order_by("left_ptr", "DESC")
-        ->limit(1)
         ->find();
       if ($tmp_item->loaded()) {
         $item = $tmp_item;
@@ -613,7 +611,6 @@ class Gallery_Access {
         ->where("right_ptr", ">", $item->right_ptr)
         ->where($field, "IS NOT", Access::UNKNOWN) // UNKNOWN is NULL so we have to use IS NOT
         ->order_by("left_ptr", "DESC")
-        ->limit(1)
         ->find();
       if ($tmp_item->loaded()) {
         $item = $tmp_item;

modules/gallery/classes/Gallery/Hook/GalleryTask.php
@@ -459,11 +459,12 @@ class Gallery_Hook_GalleryTask {
 
         // We want to leave the first one alone and update all conflicts to be random values.
         $fixed = 0;
-        $conflicts = ORM::factory("Item")
+        $conflict = ORM::factory("Item")
           ->where("parent_id", "=", $parent_id)
           ->where("slug", "=", $slug)
-          ->limit(1)->offset(1)->find_all();
-        if ($conflicts->count() && $conflict = $conflicts->current()) {
+          ->offset(1)
+          ->find();
+        if ($conflict->loaded()) {
           $task->log("Fixing conflicting slug for item id {$conflict->id}");
           DB::update("items")
             ->set(array("slug" => $slug . "-" . (string)rand(1000, 9999)))
@@ -506,11 +507,12 @@ class Gallery_Hook_GalleryTask {
 
         $fixed = 0;
         // We want to leave the first one alone and update all conflicts to be random values.
-        $conflicts = ORM::factory("Item")
+        $conflict = ORM::factory("Item")
           ->where("parent_id", "=", $parent_id)
           ->where("name", "=", $name)
-          ->limit(1)->offset(1)->find_all();
-        if ($conflicts->count() && $conflict = $conflicts->current()) {
+          ->offset(1)
+          ->find();
+        if ($conflict->loaded()) {
           $task->log("Fixing conflicting name for item id {$conflict->id}");
           if (!$conflict->is_album() && preg_match("/^(.*)(\.[^\.\/]*?)$/", $conflict->name, $matches)) {
             $item_base_name = $matches[1];
@@ -562,12 +564,13 @@ class Gallery_Hook_GalleryTask {
 
         $fixed = 0;
         // We want to leave the first one alone and update all conflicts to be random values.
-        $conflicts = ORM::factory("Item")
+        $conflict = ORM::factory("Item")
           ->where("parent_id", "=", $parent_id)
           ->where("name", "LIKE", "{$base_name_escaped}.%")
           ->where("type", "<>", "album")
-          ->limit(1)->offset(1)->find_all();
-        if ($conflicts->count() && $conflict = $conflicts->current()) {
+          ->offset(1)
+          ->find();
+        if ($conflict->loaded()) {
           $task->log("Fixing conflicting name for item id {$conflict->id}");
           if (preg_match("/^(.*)(\.[^\.\/]*?)$/", $conflict->name, $matches)) {
             $item_base_name = $matches[1]; // unlike $base_name, this always maintains capitalization

modules/gallery/tests/ORM_MPTT_Test.php
@@ -129,7 +129,7 @@ class ORM_MPTT_Test extends Unittest_Testcase {
     $inner2 = Test::random_album($outer);
 
     $this->assertEquals(
-      $inner2->id, $outer->children->limit(1)->offset(1)->find_all()->current()->id);
+      $inner2->id, $outer->children->offset(1)->find()->id);
   }
 
   public function test_children_count() {

commit ac9ab946ce1407255d6c412d936b567130d8af76

Fix up some issues introduced in db1b52f059faf6973a32d35a46da7bf05e00de7f
Database_Result::current() can return null if there are no results,
but ORM::find() always returns a Model so we need to use ORM::loaded()
in conditionals, not null checking.

modules/gallery/classes/Gallery/Hook/GalleryEvent.php
@@ -166,7 +166,8 @@ class Gallery_Hook_GalleryEvent {
         Session::instance()->set("batch_missing_album_cover", $batch_missing_album_cover);
       } else {
         // Choose the first viewable child as the new cover.
-        if ($child = $parent->children->viewable()->find()) {
+        $child = $parent->children->viewable()->find();
+        if ($child->loaded()) {
           Item::make_album_cover($child);
         }
       }
@@ -194,7 +195,8 @@ class Gallery_Hook_GalleryEvent {
     foreach (array_keys(Session::instance()->get("batch_missing_album_cover", array())) as $id) {
       $item = ORM::factory("Item", $id);
       if ($item->loaded() && !$item->album_cover_item_id) {
-        if ($child = $item->children->find()) {
+        $child = $item->children->find();
+        if ($child->loaded()) {
           Item::make_album_cover($child);
         }
       }