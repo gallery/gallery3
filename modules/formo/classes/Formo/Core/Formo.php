9f05319b9fc9b1439abb4e1f63dc4a91780f2628
shadlaws <shad@shadlaws.com>
2013-05-03 15:28:41 +0200

    Update Formo - fix a bug to make 'template_ext' config work with render_opt().

--- modules/formo/classes/Formo/Core/Formo.php
+++ modules/formo/classes/Formo/Core/Formo.php
@@ -1125,6 +1125,12 @@ class Formo_Core_Formo extends Formo_Innards {
 		{
 			$template = $this->config('template_dir').$template;
 
+			// If template_ext is defined and template doesn't already have that extension, add it
+			$template_ext = $this->config('template_ext');
+			if ($template_ext AND pathinfo($template, PATHINFO_EXTENSION) != $template_ext) {
+				$template .= ".$template_ext";
+			}
+
 			$view = View::factory($template)
 				->set('field', $this)
 				->set('opts', $this->get('opts', array()));
