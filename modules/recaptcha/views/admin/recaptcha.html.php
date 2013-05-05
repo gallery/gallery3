2d3df9fc24d4c77c0f2f099182db967d510b87ba
shadlaws <shad@shadlaws.com>
2013-05-05 22:01:48 +0200

    Convert Controller_Admin_Recaptcha to Formo.

--- modules/recaptcha/views/admin/recaptcha.html.php
+++ modules/recaptcha/views/admin/recaptcha.html.php
@@ -3,7 +3,7 @@
   <h1> <?= t("reCAPTCHA challenge filtering") ?> </h1>
   <p>
     <?= t("reCAPTCHA is a free CAPTCHA service that helps to digitize books, newspapers and old time radio shows.  In order to use it, you need to sign up for a <a href=\"%domain_url\">reCAPTCHA Public/Private Key pair</a>, which is also free.  Once registered, the challenge and response strings are evaluated at <a href=\"%recaptcha_url\">recaptcha.net</a> to determine if the form content has been entered by a bot.",
-          array("domain_url" => $form->get_key_url,
+          array("domain_url" => $get_key_url,
                 "recaptcha_url" => HTML::mark_clean("http://recaptcha.net"))) ?>
   </p>
 
