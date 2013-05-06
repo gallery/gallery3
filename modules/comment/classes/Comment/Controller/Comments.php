a8b4f822e85b5d28d35df56fddf0b80633461552
shadlaws <shad@shadlaws.com>
2013-05-06 13:30:12 +0200

    Convert Controller_Comments to Formo, clean up links in comment block.

--- modules/comment/classes/Comment/Controller/Comments.php
+++ modules/comment/classes/Comment/Controller/Comments.php
@@ -19,65 +19,91 @@
  */
 class Comment_Controller_Comments extends Controller {
   /**
-   * Add a new comment to the collection.
+   * Add a new comment.  This generates the form, validates it, adds the comment,
+   * and returns an ajaxified response.
    */
-  public function action_create() {
-    $id = $this->request->arg(0, "digit");
-    $item = ORM::factory("Item", $id);
-    Access::required("view", $item);
-    if (!Comment::can_comment()) {
-      Access::forbidden();
-    }
-
-    $form = Comment::get_add_form($item);
-    try {
-      $valid = $form->validate();
-      $comment = ORM::factory("Comment");
-      $comment->item_id = $id;
-      $comment->author_id = Identity::active_user()->id;
-      $comment->text = $form->add_comment->text->value;
-      $comment->guest_name = $form->add_comment->inputs["name"]->value;
-      $comment->guest_email = $form->add_comment->email->value;
-      $comment->guest_url = $form->add_comment->url->value;
-      $comment->validate();
-    } catch (ORM_Validation_Exception $e) {
-      // Translate ORM validation errors into form error messages
-      foreach ($e->errors() as $key => $error) {
-        switch ($key) {
-        case "guest_name":  $key = "name";  break;
-        case "guest_email": $key = "email"; break;
-        case "guest_url":   $key = "url";   break;
-        }
-        $form->add_comment->inputs[$key]->add_error($error[0], 1);
-      }
-      $valid = false;
-    }
-
-    if ($valid) {
-      $comment->save();
-      $view = new View_Theme("comment/comment.html", "other", "comment-fragment");
-      $view->comment = $comment;
-
-      $this->response->json(array("result" => "success",
-                                  "view" => (string)$view,
-                                  "form" => (string)Comment::get_add_form($item)));
-    } else {
-      $form = Comment::prefill_add_form($form);
-      $this->response->json(array("result" => "error", "form" => (string)$form));
-    }
-  }
-
-  /**
-   * Present a form for adding a new comment to this item or editing an existing comment.
-   */
-  public function action_form_add() {
+  public function action_add() {
     $item_id = $this->request->arg(0, "digit");
     $item = ORM::factory("Item", $item_id);
     Access::required("view", $item);
     if (!Comment::can_comment()) {
       Access::forbidden();
     }
+    $author = Identity::active_user();
 
-    $this->response->body(Comment::prefill_add_form(Comment::get_add_form($item)));
+    // Build the comment model.
+    $comment = ORM::factory("Comment");
+    $comment->author_id = $author->id;
+    $comment->item_id = $item->id;
+    $comment->state = "published";  // The module events can alter this default if desired.
+
+    // Build the form.
+    $form = Formo::form()
+      ->attr("id", "g-comment-form")
+      ->add("comment", "group");
+    $form->comment
+      ->set("label", t("Add comment"))
+      ->add("guest_name", "input")
+      ->add("guest_email", "input")
+      ->add("guest_url", "input")
+      ->add("text", "textarea")
+      ->add("submit", "input|submit", t("Add"));
+    $form->comment->guest_name
+      ->attr("id", "g-author")
+      ->set("label", t("Name"))
+      ->set("error_messages", array("not_empty" => t("You must enter a name for yourself")));
+    $form->comment->guest_email
+      ->attr("id", "g-email")
+      ->set("label", t("Email (hidden)"))
+      ->set("error_messages", array("not_empty" => t("You must enter a valid email address"),
+                                    "email"     => t("You must enter a valid email address")));
+    $form->comment->guest_url
+      ->attr("id", "g-url")
+      ->set("label", t("Website (hidden)"))
+      ->set("error_messages", array("url"       => t("You must enter a valid url")));
+    $form->comment->text
+      ->attr("id", "g-text")
+      ->set("label", t("Comment"))
+      ->set("error_messages", array("not_empty" => t("You must enter a comment")));
+    $form->comment->submit
+      ->add_class("ui-state-default ui-corner-all");  // @todo: do this in js, and add "Cancel".
+
+    // Link the ORM model and call the form events.
+    $form->comment->orm("link", array("model" => $comment));
+    //Module::event("comment_add_form", $form);      // @todo: make this work!
+    //Module::event("captcha_protect_form", $form);  // @todo: make this work!
+
+    // If we have a registered user, fill in their name, email, and url, and disable the fields.
+    if (!$author->guest) {
+      $form->comment->guest_name
+        ->attr("disabled", "disabled")
+        ->val($author->full_name);
+      $form->comment->guest_email
+        ->attr("disabled", "disabled")
+        ->val($author->email);
+      $form->comment->guest_url
+        ->attr("disabled", "disabled")
+        ->val($author->url);
+    }
+
+    // Load and validate the form.
+    if ($form->sent()) {
+      if ($form->load()->validate()) {
+        $comment->save();
+
+        $view = new View_Theme("comment/comment.html", "other", "comment-fragment");
+        $view->comment = $comment;
+
+        $this->response->json(array("result" => "success",
+                                    "view"   => (string)$view,
+                                    "form"   => (string)$form));
+      } else {
+        $this->response->json(array("result" => "error",
+                                    "form"   => (string)$form));
+      }
+      return;
+    }
+
+    $this->response->body($form);
   }
 }
