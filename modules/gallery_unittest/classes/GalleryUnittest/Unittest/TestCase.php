3c68b79cbe856a47b51716a07b07c3de2ddbd544
Bharat Mediratta <bharat@menalto.com>
2013-05-06 12:49:59 -0400

    Overload Unittest_TestCase to make sure that we only call teardown when setup was called - this should catch cases where we forget to call parent::setup in our tests's setup.  It doesn't (yet) catch the case where we forget to call teardown.

---
+++ modules/gallery_unittest/classes/GalleryUnittest/Unittest/TestCase.php
@@ -0,0 +1,32 @@
+<?php defined("SYSPATH") or die("No direct script access.");
+/**
+ * Gallery - a web based photo album viewer and editor
+ * Copyright (C) 2000-2013 Bharat Mediratta
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+class GalleryUnittest_Unittest_TestCase extends Kohana_Unittest_TestCase {
+  protected $_called_setup = null;
+
+  public function setUp() {
+    parent::setUp();
+    $this->_called_setup = true;
+  }
+
+  public function tearDown() {
+    $this->assertTrue($this->_called_setup);
+    parent::tearDown();
+  }
+}
---
+++ modules/gallery_unittest/classes/Unittest/TestCase.php
@@ -0,0 +1,3 @@
+<?php defined("SYSPATH") or die("No direct script access.");
+
+class Unittest_TestCase extends GalleryUnittest_Unittest_TestCase {}
