37a5b0b37811ad6f66046795a1dd6c1f8a7133f1
Bharat Mediratta <bharat@menalto.com>
2013-05-07 22:04:38 -0400

    Largely fix these tests.  Stop using a mock, we control the database configuration and we're calling compile() which is fast enough so it's useful to test the full API here and not reach into testing the database innards.
    The one exception is testing the prefix replacement, which is a new
    API that exists only in Gallery_Database and classes that inherit from
    it.  But we expect that we'll always be using a Gallery_Database so
    it's fair to test that API here as if it were part of the regular
    Database API.

--- modules/gallery/tests/Database_Test.php
+++ modules/gallery/tests/Database_Test.php
@@ -18,129 +18,122 @@
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
 class Database_Test extends Unittest_TestCase {
-  function setup() {
-    parent::setup();
-
-    $db_config = Kohana::$config->load("database");
-    $db_config["mock"] = $db_config["default"];
-    $db_config["mock"]["caching"] = false;
-    $db_config["mock"]["table_prefix"] = "g_";
-  }
-
   function test_simple_where() {
     $sql = DB::select("some_column")
       ->from("some_table")
       ->where("a", "=", 1)
       ->where("b", "=", 2)
-      ->compile("mock");
-    $sql = str_replace("\n", " ", $sql);
-    $this->assertSame("SELECT [some_column] FROM [some_table] WHERE [a] = [1] AND [b] = [2]", $sql);
+      ->compile();
+    $this->assertSame(
+      "SELECT `some_column` FROM `g3_some_table` WHERE `a` = 1 AND `b` = 2",
+      $sql);
   }
 
   function test_compound_where() {
     $sql = DB::select()
+      ->from("table")
       ->where("outer1", "=", 1)
       ->and_where_open()
       ->where("inner1", "=", 1)
       ->or_where("inner2", "=", 2)
       ->and_where_close()
       ->where("outer2", "=", 2)
-      ->compile("mock");
-    $sql = str_replace("\n", " ", $sql);
+      ->compile();
     $this->assertSame(
-      "SELECT [*] WHERE [outer1] = [1] AND ([inner1] = [1] OR [inner2] = [2]) AND [outer2] = [2]",
+      "SELECT * FROM `g3_table` WHERE `outer1` = 1 AND " .
+      "(`inner1` = 1 OR `inner2` = 2) AND `outer2` = 2",
       $sql);
   }
 
   function test_group_first() {
     $sql = DB::select()
+      ->from("table")
       ->and_where_open()
       ->where("inner1", "=", 1)
       ->or_where("inner2", "=", 2)
       ->and_where_close()
       ->where("outer1", "=", 1)
       ->where("outer2", "=", 2)
-      ->compile("mock");
-    $sql = str_replace("\n", " ", $sql);
+      ->compile();
     $this->assertSame(
-      "SELECT [*] WHERE ([inner1] = [1] OR [inner2] = [2]) AND [outer1] = [1] AND [outer2] = [2]",
+      "SELECT * FROM `g3_table` WHERE (`inner1` = 1 OR `inner2` = 2) " .
+      "AND `outer1` = 1 AND `outer2` = 2",
       $sql);
   }
 
   function test_where_array() {
     $sql = DB::select()
+      ->from("table")
       ->where("outer1", "=", 1)
       ->and_where_open()
       ->where("inner1", "=", 1)
       ->or_where("inner2", "=", 2)
       ->or_where("inner3", "=", 3)
       ->and_where_close()
-      ->compile("mock");
+      ->compile();
     $sql = str_replace("\n", " ", $sql);
     $this->assertSame(
-      "SELECT [*] WHERE [outer1] = [1] AND ([inner1] = [1] OR [inner2] = [2] OR [inner3] = [3])",
+      "SELECT * FROM `g3_table` WHERE `outer1` = 1 AND " .
+      "(`inner1` = 1 OR `inner2` = 2 OR `inner3` = 3)",
       $sql);
   }
 
   function test_notlike() {
     $sql = DB::select()
+      ->from("table")
       ->where("outer1", "=", 1)
       ->or_where_open()
       ->where("inner1", "NOT LIKE", "%1%")
       ->or_where_close()
-      ->compile("mock");
+      ->compile();
     $sql = str_replace("\n", " ", $sql);
     $this->assertSame(
-      "SELECT [*] WHERE [outer1] = [1] OR ([inner1] NOT LIKE [%1%])",
+      "SELECT * FROM `g3_table` WHERE `outer1` = 1 OR (`inner1` NOT LIKE '%1%')",
       $sql);
   }
 
   function test_prefix_replacement() {
-    $db = Database::instance("mock");
-    $converted = $db->add_table_prefixes("CREATE TABLE IF NOT EXISTS {test} (
-                   `id` int(9) NOT NULL auto_increment,
-                   `name` varchar(32) NOT NULL,
-                   PRIMARY KEY (`id`),
-                   UNIQUE KEY(`name`))
-                 ENGINE=InnoDB DEFAULT CHARSET=utf8");
-    $expected = "CREATE TABLE IF NOT EXISTS `g_test` (
-                   `id` int(9) NOT NULL auto_increment,
-                   `name` varchar(32) NOT NULL,
-                   PRIMARY KEY (`id`),
-                   UNIQUE KEY(`name`))
-                 ENGINE=InnoDB DEFAULT CHARSET=utf8";
-    $this->assertSame($expected, $converted);
+    $actual = Database::instance()->add_table_prefixes(
+      "CREATE TABLE IF NOT EXISTS {test} (
+         `id` int(9) NOT NULL auto_increment,
+         `name` varchar(32) NOT NULL,
+         PRIMARY KEY (`id`),
+         UNIQUE KEY(`name`))
+       ENGINE=InnoDB DEFAULT CHARSET=utf8");
+    $expected =
+      "CREATE TABLE IF NOT EXISTS `g3_test` (
+         `id` int(9) NOT NULL auto_increment,
+         `name` varchar(32) NOT NULL,
+         PRIMARY KEY (`id`),
+         UNIQUE KEY(`name`))
+       ENGINE=InnoDB DEFAULT CHARSET=utf8";
+    $this->assertSame($expected, $actual);
 
-    $sql = "UPDATE {test} SET `name` = '{test string}' " .
-        "WHERE `item_id` IN " .
-        "  (SELECT `id` FROM {test} " .
-        "  WHERE `left_ptr` >= 1 " .
-        "  AND `right_ptr` <= 6)";
-    $sql = $db->add_table_prefixes($sql);
-
-    $expected = "UPDATE `g_test` SET `name` = '{test string}' " .
-        "WHERE `item_id` IN " .
-        "  (SELECT `id` FROM `g_test` " .
-        "  WHERE `left_ptr` >= 1 " .
-        "  AND `right_ptr` <= 6)";
-
-    $this->assertSame($expected, $sql);
+    $actual = Database::instance()->add_table_prefixes(
+      "UPDATE {test} SET `name` = '{test string}'
+       WHERE `item_id` IN
+       (SELECT `id` FROM {test} WHERE `left_ptr` >= 1 AND `right_ptr` <= 6)");
+    $expected =
+      "UPDATE `g3_test` SET `name` = '{test string}'
+       WHERE `item_id` IN
+       (SELECT `id` FROM `g3_test` WHERE `left_ptr` >= 1 AND `right_ptr` <= 6)";
+    $this->assertSame($expected, $actual);
   }
 
   function test_prefix_replacement_for_rename_table() {
-    $db = Database::instance("mock");
     $this->assertSame(
-      "RENAME TABLE `g_test` TO `g_new_test`",
-      $db->add_table_prefixes("RENAME TABLE {test} TO {new_test}"));
+      "RENAME TABLE `g3_test` TO `g3_new_test`",
+      Database::instance()->add_table_prefixes("RENAME TABLE {test} TO {new_test}"));
   }
 
   function test_prefix_no_replacement() {
-    $sql = DB::update("test_tables")
+    $sql =
+    $this->assertSame(
+      "UPDATE `g3_test_table` SET `name` = 'Test Name' WHERE `1` = '1'",
+      DB::update("test_table")
       ->where("1", "=", "1")
       ->set(array("name" => "Test Name"))
-      ->compile("mock");
-    $sql = str_replace("\n", " ", $sql);
-    $this->assertSame("UPDATE [test_tables] SET [name] = [Test Name] WHERE [1] = [1]", $sql);
+      ->compile());
   }
 
   function test_escape_for_like() {
@@ -148,45 +141,4 @@ class Database_Test extends Unittest_TestCase {
     $this->assertSame('basic\_test', Database::escape_for_like("basic_test"));
     $this->assertSame('\\\100\%\_test/', Database::escape_for_like('\100%_test/'));
   }
-}
-
-class Database_Mock extends Gallery_Database {
-  public function connect() {
-  }
-
-  public function disconnect() {
-    return true;
-  }
-
-  public function set_charset($charset) {
-  }
-
-  public function query($type, $sql, $as_object=false, array $params=null) {
-    return array($type, $sql, $as_object, $params);
-  }
-
-  public function begin($mode=null) {
-    return true;
-  }
-
-  public function commit() {
-    return true;
-  }
-
-  public function rollback() {
-    return true;
-  }
-
-  public function list_tables($like=null) {
-    return array("@todo put something reasonable here");
-  }
-
-  public function list_columns($table, $like=null, $add_prefix=true) {
-    return array("@todo put something reasonable here");
-  }
-
-  public function escape($value) {
-    return "[escaped:$value]";
-  }
-
 }
