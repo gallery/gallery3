af7515af13862b662bce85f5ad0ee0450d07a3ed
shadlaws <shad@shadlaws.com>
2013-06-01 13:17:51 +0200

    Move Comment_Test tests to Comment_Model_Test where they belong. They test the Model_Comment class, not the Comment class.  Also, remove the saving/resetting of the IP address since it's no longer used anywhere.

--- modules/comment/tests/Comment_Model_Test.php
+++ modules/comment/tests/Comment_Model_Test.php
@@ -18,8 +18,35 @@
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
 class Comment_Model_Test extends Unittest_TestCase {
+  protected $_user_agent;
+  protected $_save;
+
+  public function setup() {
+    parent::setup();
+
+    $this->_user_agent = Request::$user_agent;  // Use this instead of user_agent() for exact reset.
+    $this->_save = $_SERVER;
+
+    $_SERVER["HTTP_ACCEPT"] = "HTTP_ACCEPT";
+    $_SERVER["HTTP_ACCEPT_CHARSET"] = "HTTP_ACCEPT_CHARSET";
+    $_SERVER["HTTP_ACCEPT_ENCODING"] = "HTTP_ACCEPT_ENCODING";
+    $_SERVER["HTTP_ACCEPT_LANGUAGE"] = "HTTP_ACCEPT_LANGUAGE";
+    $_SERVER["HTTP_CONNECTION"] = "HTTP_CONNECTION";
+    $_SERVER["HTTP_REFERER"] = "HTTP_REFERER";
+    $_SERVER["HTTP_USER_AGENT"] = "HTTP_USER_AGENT";
+    $_SERVER["QUERY_STRING"] = "QUERY_STRING";
+    $_SERVER["REMOTE_ADDR"] = "REMOTE_ADDR";
+    $_SERVER["REMOTE_HOST"] = "REMOTE_HOST";
+    $_SERVER["REMOTE_PORT"] = "REMOTE_PORT";
+    $_SERVER["SERVER_NAME"] = "SERVER_NAME";
+
+    Request::$user_agent = "HTTP_USER_AGENT";
+  }
+
   public function teardown() {
-    Identity::set_active_user(Identity::admin_user());
+    Request::$user_agent = $this->_user_agent;
+    $_SERVER = $this->_save;
+
     parent::teardown();
   }
 
@@ -77,5 +104,72 @@ class Comment_Model_Test extends Unittest_TestCase {
     Access::deny(Identity::everybody(), "view", $album);
     $this->assertFalse((bool)
       ORM::factory("Comment")->viewable()->where("comment.id", "=", $comment->id)->count_all());
+  }
+
+  public function test_create_comment_for_guest() {
+    $comment = ORM::factory("Comment");
+    $comment->item_id = Item::root()->id;
+    $comment->text = "text";
+    $comment->author_id = Identity::guest()->id;
+    $comment->guest_name = "name";
+    $comment->guest_email = "email@email.com";
+    $comment->guest_url = "http://url.com";
+    $comment->save();
+
+    $this->assertEquals("name", $comment->author_name());
+    $this->assertEquals("email@email.com", $comment->author_email());
+    $this->assertEquals("http://url.com", $comment->author_url());
+    $this->assertEquals("text", $comment->text);
+    $this->assertEquals(1, $comment->item_id);
+
+    $this->assertEquals("REMOTE_ADDR", $comment->server_remote_addr);
+    $this->assertEquals("HTTP_USER_AGENT", $comment->server_http_user_agent);
+    $this->assertEquals("HTTP_ACCEPT", $comment->server_http_accept);
+    $this->assertEquals("HTTP_ACCEPT_CHARSET", $comment->server_http_accept_charset);
+    $this->assertEquals("HTTP_ACCEPT_ENCODING", $comment->server_http_accept_encoding);
+    $this->assertEquals("HTTP_ACCEPT_LANGUAGE", $comment->server_http_accept_language);
+    $this->assertEquals("HTTP_CONNECTION", $comment->server_http_connection);
+    $this->assertEquals("HTTP_REFERER", $comment->server_http_referer);
+    $this->assertEquals("HTTP_USER_AGENT", $comment->server_http_user_agent);
+    $this->assertEquals("SERVER_NAME", $comment->server_name);
+    $this->assertEquals("QUERY_STRING", $comment->server_query_string);
+    $this->assertEquals("REMOTE_ADDR", $comment->server_remote_addr);
+    $this->assertEquals("REMOTE_HOST", $comment->server_remote_host);
+    $this->assertEquals("REMOTE_PORT", $comment->server_remote_port);
+
+    $this->assertTrue(!empty($comment->created));
+  }
+
+  public function test_create_comment_for_user() {
+    $admin = Identity::admin_user();
+
+    $comment = ORM::factory("Comment");
+    $comment->item_id = Item::root()->id;
+    $comment->text = "text";
+    $comment->author_id = $admin->id;
+    $comment->save();
+
+    $this->assertEquals($admin->full_name, $comment->author_name());
+    $this->assertEquals($admin->email, $comment->author_email());
+    $this->assertEquals($admin->url, $comment->author_url());
+    $this->assertEquals("text", $comment->text);
+    $this->assertEquals(1, $comment->item_id);
+
+    $this->assertEquals("REMOTE_ADDR", $comment->server_remote_addr);
+    $this->assertEquals("HTTP_USER_AGENT", $comment->server_http_user_agent);
+    $this->assertEquals("HTTP_ACCEPT", $comment->server_http_accept);
+    $this->assertEquals("HTTP_ACCEPT_CHARSET", $comment->server_http_accept_charset);
+    $this->assertEquals("HTTP_ACCEPT_ENCODING", $comment->server_http_accept_encoding);
+    $this->assertEquals("HTTP_ACCEPT_LANGUAGE", $comment->server_http_accept_language);
+    $this->assertEquals("HTTP_CONNECTION", $comment->server_http_connection);
+    $this->assertEquals("HTTP_REFERER", $comment->server_http_referer);
+    $this->assertEquals("HTTP_USER_AGENT", $comment->server_http_user_agent);
+    $this->assertEquals("SERVER_NAME", $comment->server_name);
+    $this->assertEquals("QUERY_STRING", $comment->server_query_string);
+    $this->assertEquals("REMOTE_ADDR", $comment->server_remote_addr);
+    $this->assertEquals("REMOTE_HOST", $comment->server_remote_host);
+    $this->assertEquals("REMOTE_PORT", $comment->server_remote_port);
+
+    $this->assertTrue(!empty($comment->created));
   }
 }
