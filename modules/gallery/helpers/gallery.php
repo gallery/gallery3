40eb5edd978af69fac69ad167b0741958c16fdf6
Bharat Mediratta <bharat@menalto.com>
2013-06-06 00:07:25 -0400

    I hereby dub 3.0.9 "Chartres"

--- modules/gallery/helpers/gallery.php
+++ modules/gallery/helpers/gallery.php
@@ -18,8 +18,8 @@
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
 class gallery_Core {
-  const VERSION = "3.0.8";
-  const CODE_NAME = "Seine";
+  const VERSION = "3.0.9";
+  const CODE_NAME = "Chartres";
   const RELEASE_CHANNEL = "git";
   const RELEASE_BRANCH = "3.0.x";
 
