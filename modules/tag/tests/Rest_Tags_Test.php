b345b2e7f572afe65480f56375f69d7319840744
shadlaws <shad@shadlaws.com>
2013-06-19 17:29:17 +0200

    Fix class names for renamed tests, and move Tag_Rest tests into Rest_Tags_Test.

--- modules/tag/tests/Rest_Tags_Test.php
+++ modules/tag/tests/Rest_Tags_Test.php
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
-class Tags_Rest_Test extends Unittest_TestCase {
+class Rest_Tags_Test extends Unittest_TestCase {
   public function setup() {
     parent::setup();
     try {
@@ -79,4 +79,84 @@ class Tags_Rest_Test extends Unittest_TestCase {
     Hook_Rest_Tags::post($request);
   }
 
+  public function test_tag_get() {
+    $this->markTestIncomplete("REST API is currently under re-construction...");
+
+    $tag = Tag::add(Item::root(), "tag1")->reload();
+
+    $request = new stdClass();
+    $request->url = RestAPI::url("tag", $tag);
+    $this->assertEquals(
+      array("url" => RestAPI::url("tag", $tag),
+            "entity" => $tag->as_array(),
+            "relationships" => array(
+              "items" => array(
+                "url" => RestAPI::url("tag_items", $tag),
+                "members" => array(
+                  RestAPI::url("tag_item", $tag, Item::root()))))),
+      Hook_Rest_Tag::get($request));
+  }
+
+  /**
+   * @expectedException HTTP_Exception_404
+   */
+  public function test_tag_get_with_invalid_url() {
+    $this->markTestIncomplete("REST API is currently under re-construction...");
+
+    $request = new stdClass();
+    $request->url = "bogus";
+    Hook_Rest_Tag::get($request);
+  }
+
+  public function test_tag_get_with_no_relationships() {
+    $this->markTestIncomplete("REST API is currently under re-construction...");
+
+    $tag = Test::random_tag();
+
+    $request = new stdClass();
+    $request->url = RestAPI::url("tag", $tag);
+    $this->assertEquals(
+      array("url" => RestAPI::url("tag", $tag),
+            "entity" => $tag->as_array(),
+            "relationships" => array(
+              "items" => array(
+                "url" => RestAPI::url("tag_items", $tag),
+                "members" => array()))),
+      Hook_Rest_Tag::get($request));
+  }
+
+  public function test_tag_put() {
+    $this->markTestIncomplete("REST API is currently under re-construction...");
+
+    $tag = Test::random_tag();
+    $request = new stdClass();
+    $request->url = RestAPI::url("tag", $tag);
+    $request->params = new stdClass();
+    $request->params->entity = new stdClass();
+    $request->params->entity->name = "new name";
+
+    Hook_Rest_Tag::put($request);
+    $this->assertEquals("new name", $tag->reload()->name);
+  }
+
+  public function test_tag_delete_tag() {
+    $this->markTestIncomplete("REST API is currently under re-construction...");
+
+    $tag = Test::random_tag();
+    $request = new stdClass();
+    $request->url = RestAPI::url("tag", $tag);
+    Hook_Rest_Tag::delete($request);
+
+    $this->assertFalse($tag->reload()->loaded());
+  }
+
+  public function test_tag_resolve() {
+    $this->markTestIncomplete("REST API is currently under re-construction...");
+
+    $tag = Test::random_tag();
+
+    $this->assertEquals(
+      $tag->as_array(),
+      RestAPI::resolve(RestAPI::url("tag", $tag))->as_array());
+  }
 }
