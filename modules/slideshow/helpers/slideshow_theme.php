2834d3a542a98c234b4004093582b2e4a5ee1d63
Stian Lund <stianlund@gmail.com>
2018-08-25 19:14:07 +0200

    Fix for Cooliris slideshow not working. Last available files downloaded from archive.org, and copied to lib/cooliris. slideshow_theme.php changed to load Cooliris from local dir.

--- modules/slideshow/helpers/slideshow_theme.php
+++ modules/slideshow/helpers/slideshow_theme.php
@@ -20,7 +20,7 @@
 class slideshow_theme_Core {
   static function page_bottom($theme) {
     $proto = request::protocol();
-    return "<script src=\"$proto://e.cooliris.com/slideshow/v/37732/go.js\" " .
+    return "<script src=\"lib/cooliris/go.js\" " .
       "type=\"text/javascript\"></script>";
   }
 }
