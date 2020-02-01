761de1b42f3c8751cb32d6d3f25db8a4ee67aa9f
bwdutton <brad-github@duttonbros.com>
2020-02-01 14:17:33 -0800

    get the CI tests fully working again

--- modules/gallery/tests/Xss_Security_Test.php
+++ modules/gallery/tests/Xss_Security_Test.php
@@ -29,6 +29,7 @@ class Xss_Security_Test extends Gallery_Unit_Test_Case {
         }
       }
 
+print "checking view $view\n";
       $frame  = null;
       $script_block = 0;
       $in_script_block = false;
