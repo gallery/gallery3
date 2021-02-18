faa69d24b92bc10a64b0875a33dd73a227809926
Brad Dutton <brad@duttonbrosllc.com>
2021-02-17 16:04:35 -0800

    use the standard video tag to render videos

--- modules/gallery/views/movieplayer.html.php
+++ modules/gallery/views/movieplayer.html.php
@@ -1,18 +1,4 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
 <div <?= html::attributes($div_attrs) ?>>
-  <video <?= html::attributes($video_attrs) ?>>
-    <source <?= html::attributes($source_attrs) ?>>
-  </video>
+  <video <?= html::attributes($source_attrs) ?> <?= html::attributes($video_attrs) ?>></video>
 </div>
-<script type="text/javascript">
-  $("#<?= $div_attrs["id"] ?> video").mediaelementplayer(
-    $.extend(true, {
-      defaultVideoWidth: <?= $width ?>,
-      defaultVideoHeight: <?= $height ?>,
-      startVolume: 1.0,
-      features: ["playpause", "progress", "current", "duration", "volume", "fullscreen"],
-      pluginPath: "<?= url::abs_file("lib/mediaelementjs/") ?>",
-      flashName: "flashmediaelement.swf.php"
-    }, <?= json_encode($player_options) ?>)
-  );
-</script>
--- modules/movie_resized/helpers/movie_resized_event.php
+++ modules/movie_resized/helpers/movie_resized_event.php
@@ -3,26 +3,12 @@
 class movie_resized_event_Core {
   static function movie_img ($movie_img, $obj) {
     $resize_file = $obj->resize_path().'.mp4';
-    $relative_file = $obj->relative_path();
-    $resize_url = $obj->resize_url(true);
-    #print pretty_backtrace();
 
     if (file_exists($resize_file)) {
-      // in case the resize is a different size
-      list ($width, $height, $mime_type, $ext, $duration) = movie::get_file_metadata($resize_file);
+      $resize_url = $obj->resize_url(true);
+      $relative_file = $obj->relative_path();
 
-      // copied this from modules/gallery/models/items.php
-      $view = new View("movieplayer.html");
-      $view->width = $width ? $width : $movie_img->width;
-      $view->height = $height ? $height : $movie_img->height;
-      $view->url = str_replace($relative_file, $relative_file.'.mp4', $resize_url);
-
-      // add some scalling logic
-      $view->width = $view->width/2;
-      $view->height = $view->height/2;
-
-      #$movie_img->view[] = $view->render();
-      $movie_img->view[] = "<video src=\"{$view->url}\" width=\"{$view->width}\" height=\"{$view->height}\" controls style=\"max-width: 100%\"></video>";
+      $movie_img->url = str_replace($relative_file, $relative_file.'.mp4', $resize_url);
     }
   }
 }
