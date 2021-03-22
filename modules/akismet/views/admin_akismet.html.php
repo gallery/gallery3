5fc15ee711c52046222759efd1731832e117120f
Stian Lund <stianlund@gmail.com>
2021-03-22 18:52:26 +0100

    Akismet stats URL and change to https

--- modules/akismet/views/admin_akismet.html.php
+++ modules/akismet/views/admin_akismet.html.php
@@ -3,8 +3,8 @@
   <h1> <?= t("Akismet spam filtering") ?> </h1>
   <p>
   <?= t("Akismet is a free, automated spam filtering service.  In order to use it, you need to sign up for a <a href=\"%api_key_url\">Wordpress.com API Key</a>, which is also free.  Your comments will be automatically relayed to <a href=\"%akismet_url\">Akismet.com</a> where they'll be scanned for spam.  Spam messages will be flagged accordingly and hidden from your vistors until you approve or delete them.",
-        array("api_key_url" => "http://wordpress.com/api-keys",
-              "akismet_url" => "http://akismet.com")) ?>
+        array("api_key_url" => "https://wordpress.com/api-keys",
+              "akismet_url" => "https://akismet.com")) ?>
   </p>
   <div class="g-block-content">
     <?php if ($valid_key): ?>
